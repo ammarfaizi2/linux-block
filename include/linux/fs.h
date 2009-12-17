60a71efa33ee20b4c9ab8adcb2e26789c7da5558
Ingo Molnar <mingo@elte.hu>
2009-12-17 09:58:30 +0100

    Revert "direct-io: cleanup blockdev_direct_IO locking"
    This reverts commit 5fe878ae7f82fbf0830dbfaee4c5ca18f3aee442.
    
    Causes crashes.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2264,11 +2264,9 @@ ssize_t __blockdev_direct_IO(int rw, struct kiocb *iocb, struct inode *inode,
 	int lock_type);
 
 enum {
-	/* need locking between buffered and direct access */
-	DIO_LOCKING	= 0x01,
-
-	/* filesystem does not support filling holes */
-	DIO_SKIP_HOLES	= 0x02,
+	DIO_LOCKING = 1, /* need locking between buffered and direct access */
+	DIO_NO_LOCKING,  /* bdev; no locking at all between buffered/direct */
+	DIO_OWN_LOCKING, /* filesystem locks buffered and direct internally */
 };
 
 static inline ssize_t blockdev_direct_IO(int rw, struct kiocb *iocb,
@@ -2277,8 +2275,7 @@ static inline ssize_t blockdev_direct_IO(int rw, struct kiocb *iocb,
 	dio_iodone_t end_io)
 {
 	return __blockdev_direct_IO(rw, iocb, inode, bdev, iov, offset,
-				    nr_segs, get_block, end_io,
-				    DIO_LOCKING | DIO_SKIP_HOLES);
+				nr_segs, get_block, end_io, DIO_LOCKING);
 }
 
 static inline ssize_t blockdev_direct_IO_no_locking(int rw, struct kiocb *iocb,
@@ -2287,7 +2284,16 @@ static inline ssize_t blockdev_direct_IO_no_locking(int rw, struct kiocb *iocb,
 	dio_iodone_t end_io)
 {
 	return __blockdev_direct_IO(rw, iocb, inode, bdev, iov, offset,
-				nr_segs, get_block, end_io, 0);
+				nr_segs, get_block, end_io, DIO_NO_LOCKING);
+}
+
+static inline ssize_t blockdev_direct_IO_own_locking(int rw, struct kiocb *iocb,
+	struct inode *inode, struct block_device *bdev, const struct iovec *iov,
+	loff_t offset, unsigned long nr_segs, get_block_t get_block,
+	dio_iodone_t end_io)
+{
+	return __blockdev_direct_IO(rw, iocb, inode, bdev, iov, offset,
+				nr_segs, get_block, end_io, DIO_OWN_LOCKING);
 }
 #endif
 
