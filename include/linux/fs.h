f739bae54ab94c851c055bd4b2ad6799b77c3b06
Valerie Aurora <vaurora@redhat.com>
2010-08-27 15:00:15 -0700

    union-mount: Add two superblock fields for union mounts
    Add two fields to struct super_block to support union mounts.
    s_union_lower_mnts is a pointer to a cloned vfsmount tree of all the
    lower (read-only) mounts unioned with the topmost (read-write)
    vfsmount.  These mounts may have submounts which will also be unioned;
    hence we copy the entire vfsmount tree, not just the root vfsmounts.
    s_union_count is the number of lower mounts unioned at the root of the
    file system.  This count is the maximum number of directories that
    will ever be unioned with a single directory.  We use it to allocate a
    union stack of the correct size for each directory.

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1396,6 +1396,18 @@ struct super_block {
 	 * Decremented by free_vfsmnt() if MNT_HARD_READONLY is set.
 	 */
 	int s_hard_readonly_users;
+
+	/*
+	 * Root of the private cloned vfsmount tree of the read-only
+	 * mounts in this union (set in topmost vfsmount only)
+	 */
+	struct vfsmount *s_union_lower_mnts;
+
+	/*
+	 * Number of layers in this union, not counting the topmost or
+	 * submounts.
+	 */
+	unsigned int s_union_count;
 };
 
 extern struct timespec current_fs_time(struct super_block *sb);
