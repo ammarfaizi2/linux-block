3d63c1aa7a82a78865f54792c62bc23a39427f35
Thomas Gleixner <tglx@linutronix.de>
2011-06-06 12:20:33 +0200

    sched: Move mmdrop to RCU on RT
    mmdrop() is invoked from finish_task_switch() by the incoming task to drop
    the mm which was handed over by the previous task. mmdrop() can be quite
    expensive which prevents an incoming real-time task from getting useful
    work done.
    
    Provide mmdrop_sched() which maps to mmdrop() on !RT kernels. On RT kernels
    it delagates the eventually required invocation of __mmdrop() to RCU.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4838,9 +4838,10 @@ static struct rq *finish_task_switch(struct task_struct *prev)
 	 *   provided by mmdrop(),
 	 * - a sync_core for SYNC_CORE.
 	 */
+
 	if (mm) {
 		membarrier_mm_sync_core_before_usermode(mm);
-		mmdrop(mm);
+		mmdrop_sched(mm);
 	}
 	if (unlikely(prev_state == TASK_DEAD)) {
 		if (prev->sched_class->task_dead)
