cd8f4bb6be345ad4b9985b89baa94bb764a1a572
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2012-05-16 16:31:38 -0700

    key: Remove extraneous parentheses from rcu_assign_keypointer()
    This commit removes the extraneous parentheses from rcu_assign_keypointer()
    so that rcu_assign_pointer() can be wrapped in do-while.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- include/linux/key.h
+++ include/linux/key.h
@@ -303,7 +303,7 @@ static inline bool key_is_instantiated(const struct key *key)
 				   rwsem_is_locked(&((struct key *)(KEY))->sem)))
 
 #define rcu_assign_keypointer(KEY, PAYLOAD)				\
-	(rcu_assign_pointer((KEY)->payload.rcudata, PAYLOAD))
+	rcu_assign_pointer((KEY)->payload.rcudata, PAYLOAD)
 
 #ifdef CONFIG_SYSCTL
 extern ctl_table key_sysctls[];
