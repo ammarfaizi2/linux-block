30b316392fbabf60ad0787cb0f8c6d917a7a37ca
Simon Wunderlich <simon.wunderlich@s2003.tu-chemnitz.de>
2013-08-14 08:01:38 +0200

    ath9k: enable CSA functionality in ath9k
    CSA is only enabled for one interface, but the same limitation applies
    for mac80211 too. It checks whether the beacon has been sent (different
    approaches for non-EDMA-enabled and EDMA-enabled devices), and completes
    the channel switch after that.
    
    Signed-off-by: Simon Wunderlich <siwu@hrz.tu-chemnitz.de>
    Signed-off-by: Mathias Kretschmer <mathias.kretschmer@fokus.fraunhofer.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    (cherry picked from commit d074e8d547853cc8b40cf93a460e8fbf9eaa3d00)
    
    Conflicts:
    	drivers/net/wireless/ath/ath9k/init.c

--- drivers/net/wireless/ath/ath9k/init.c
+++ drivers/net/wireless/ath/ath9k/init.c
@@ -835,6 +835,7 @@ void ath9k_set_hw_capab(struct ath_softc *sc, struct ieee80211_hw *hw)
 	hw->wiphy->flags |= WIPHY_FLAG_IBSS_RSN;
 	hw->wiphy->flags |= WIPHY_FLAG_SUPPORTS_TDLS;
 	hw->wiphy->flags |= WIPHY_FLAG_HAS_REMAIN_ON_CHANNEL;
+	hw->wiphy->flags |= WIPHY_FLAG_HAS_CHANNEL_SWITCH;
 
 #ifdef CONFIG_PM_SLEEP
 	if ((ah->caps.hw_caps & ATH9K_HW_WOW_DEVICE_CAPABLE) &&
