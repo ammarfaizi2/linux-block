21ea892c8939af2cd8ffcee6c7a5ee700e81b233
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2013-09-05 15:51:30 -0700

    mac80211: Apply ACCESS_ONCE() to avoid sparse false positive
    The sparse checking for rcu_assign_pointer() was recently upgraded
    to reject non-__kernel address spaces.  This also rejects __rcu,
    which is almost always the right thing to do.  However, the uses in
    sta_info_hash_del() are legitimate: They are assigning a pointer to an
    element from an RCU-protected list, and all elements of this list are
    already visible to caller.
    
    This commit therefore silences this false positive by laundering the
    pointer using ACCESS_ONCE() as suggested by Eric Dumazet and Josh
    Triplett.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: "John W. Linville" <linville@tuxdriver.com>
    Cc: Johannes Berg <johannes@sipsolutions.net>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: linux-wireless@vger.kernel.org
    Cc: netdev@vger.kernel.org

--- net/mac80211/sta_info.c
+++ net/mac80211/sta_info.c
@@ -74,8 +74,8 @@ static int sta_info_hash_del(struct ieee80211_local *local,
 	if (!s)
 		return -ENOENT;
 	if (s == sta) {
-		rcu_assign_pointer(local->sta_hash[STA_HASH(sta->sta.addr)],
-				   s->hnext);
+		/* Both --rcu and visible, so ACCESS_ONCE() is OK. */
+		ACCESS_ONCE(local->sta_hash[STA_HASH(sta->sta.addr)]) = s->hnext;
 		return 0;
 	}
 
@@ -84,7 +84,8 @@ static int sta_info_hash_del(struct ieee80211_local *local,
 		s = rcu_dereference_protected(s->hnext,
 					lockdep_is_held(&local->sta_mtx));
 	if (rcu_access_pointer(s->hnext)) {
-		rcu_assign_pointer(s->hnext, sta->hnext);
+		/* Both --rcu and visible, so ACCESS_ONCE() is OK. */
+		ACCESS_ONCE(s->hnext) = sta->hnext;
 		return 0;
 	}
 
