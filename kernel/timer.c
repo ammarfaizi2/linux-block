2beb1a638b54c9adba935bf4b47d4ed7d3f4bdae
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2013-12-16 10:32:01 -0800

    timers: Reduce future __run_timers() latency for newly emptied list
    The __run_timers() function currently steps through the list one jiffy at
    a time in order to update the timer wheel.  However, if the timer wheel
    is empty, no adjustment is needed other than updating ->timer_jiffies.
    Therefore, if we just emptied the timer wheel, for example, by deleting
    the last timer, we should mark the timer wheel as being up to date.
    This marking will reduce (and perhaps eliminate) the jiffy-stepping that
    a future __run_timers() call will need to do in response to some future
    timer posting or migration.  This commit therefore catches ->timer_jiffies
    for this case.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/timer.c
+++ kernel/timer.c
@@ -693,6 +693,7 @@ static int detach_if_pending(struct timer_list *timer, struct tvec_base *base,
 	detach_timer(timer, clear_pending);
 	if (!tbase_get_deferrable(timer->base)) {
 		base->active_timers--;
+		(void)catchup_timer_jiffies(base);
 		if (timer->expires == base->next_timer)
 			base->next_timer = base->timer_jiffies;
 	}
