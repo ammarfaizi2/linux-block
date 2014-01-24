5bd59af7bff2d1d008ea82c9628d79934d52a546
Janusz Dziedzic <janusz.dziedzic@tieto.com>
2014-01-24 10:48:45 +0200

    nl80211: add VHT support for set_bitrate_mask
    Add VHT MCS/NSS set support for nl80211_set_tx_bitrate_mask().
    This should be used mainly for test purpose, to check
    different MCS/NSS VHT combinations.
    
    Signed-off-by: Janusz Dziedzic <janusz.dziedzic@tieto.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    (cherry pick from commit 204e35a91c4b3327b7239d7687fbd4923edbbf08)

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -1765,6 +1765,7 @@ struct cfg80211_bitrate_mask {
 	struct {
 		u32 legacy;
 		u8 ht_mcs[IEEE80211_HT_MCS_MASK_LEN];
+		u16 vht_mcs[NL80211_VHT_NSS_MAX];
 	} control[IEEE80211_NUM_BANDS];
 };
 /**
