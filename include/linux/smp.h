7be151f51865c5dfae4c5da13534efb481c2fddf
Christoph Hellwig <hch@infradead.org>
2014-06-25 22:27:41 -0600

    kernel: use lockless list for smp_call_function_single
    Make smp_call_function_single and friends more efficient by using a
    lockless list.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Cc: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/smp.h
+++ include/linux/smp.h
@@ -12,12 +12,16 @@
 #include <linux/cpumask.h>
 #include <linux/init.h>
 #include <linux/irqflags.h>
+#include <linux/llist.h>
 
 extern void cpu_idle(void);
 
 typedef void (*smp_call_func_t)(void *info);
 struct call_single_data {
-	struct list_head list;
+	union {
+		struct list_head list;
+		struct llist_node llist;
+	};
 	smp_call_func_t func;
 	void *info;
 	u16 flags;
