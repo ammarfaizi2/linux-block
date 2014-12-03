aa192c69c5fff86c408acd3da8597a13c4af0a50
Ming Lei <ming.lei@canonical.com>
2014-12-03 19:38:04 +0800

    blk-mq: prevent unmapped hw queue from being scheduled
    When one hardware queue has no mapped software queues, it
    shouldn't have been scheduled. Otherwise WARNING or OOPS
    can triggered.
    
    blk_mq_hw_queue_mapped() helper is introduce for fixing
    the problem.
    
    Signed-off-by: Ming Lei <ming.lei@canonical.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- block/blk.h
+++ block/blk.h
@@ -2,6 +2,8 @@
 #define BLK_INTERNAL_H
 
 #include <linux/idr.h>
+#include <linux/blk-mq.h>
+#include "blk-mq.h"
 
 /* Amount of time in which a process may batch requests */
 #define BLK_BATCH_TIME	(HZ/50UL)
