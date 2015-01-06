d3a254865b2c1b953416fe877029a56811948f55
Andrew Morton <akpm@linux-foundation.org>
2015-01-06 01:00:32 +0000

    lib-bitmap-update-bitmap_onto-to-unsigned-checkpatch-fixes
    WARNING: please, no space before tabs
    #38: FILE: lib/bitmap.c:1011:
    +^Iunsigned int n, m;       ^I/* same meaning as in above comment */$
    
    total: 0 errors, 1 warnings, 19 lines checked
    
    ./patches/lib-bitmap-update-bitmap_onto-to-unsigned.patch has style problems, please review.
    
    If any of these errors are false positives, please report
    them to the maintainer, see CHECKPATCH in MAINTAINERS.
    
    Please run checkpatch prior to sending patches
    
    Cc: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- lib/bitmap.c
+++ lib/bitmap.c
@@ -1008,7 +1008,7 @@ EXPORT_SYMBOL(bitmap_bitremap);
 void bitmap_onto(unsigned long *dst, const unsigned long *orig,
 			const unsigned long *relmap, unsigned int bits)
 {
-	unsigned int n, m;       	/* same meaning as in above comment */
+	unsigned int n, m;	/* same meaning as in above comment */
 
 	if (dst == orig)	/* following doesn't handle inplace mappings */
 		return;
