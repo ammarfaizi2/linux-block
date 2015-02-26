jnsnow/linux

feature-add-sanity-checks
Add a .lock file, a NEED_FSCK flag,
and the ability to report to the daemon when
we have found inconsistent data that needs
to be investigated.

fs/cachefiles/bind.c
@@ -88,7 +88,7 @@ static int cachefiles_daemon_add_cache(struct cachefiles_cache *cache)
 	struct path path;
 	struct kstatfs stats;
 	struct dentry *graveyard, *cachedir, *root;
-	struct file *cull_index, *cull_atimes;
+	struct file *cull_index, *cull_atimes, *lockfile;
 	const struct cred *saved_cred;
 	int ret;
 
@@ -121,7 +121,7 @@ static int cachefiles_daemon_add_cache(struct cachefiles_cache *cache)
 		goto error_open_root;
 
 	cache->mnt = path.mnt;
-	root = path.dentry;
+	cache->root = root = path.dentry;
 
 	/* check parameters */
 	ret = -EOPNOTSUPP;
@@ -215,20 +215,59 @@ static int cachefiles_daemon_add_cache(struct cachefiles_cache *cache)
 
 	cache->graveyard = graveyard;
 
+	/* Before we create/open the culling/atime indices,
+	 * mark them as locked/dirty. */
+	lockfile = cachefiles_get_file(cache, root, ".lock", 0);
+	if (IS_ERR(lockfile)) {
+		_debug("IS_ERR(lockfile) ...");
+		if (PTR_ERR(lockfile) == -ENOENT) {
+			lockfile = NULL;
+		} else {
+			kerror("Failed to check for lockfile.");
+			ret = PTR_ERR(lockfile);
+			goto error_unsupported;
+		}
+	}
+
+	if (lockfile) {
+		_debug(".lock file already exists, requesting cache fsck");
+		cachefiles_mark_dirty(cache);
+	} else {
+		_debug("creating .lock file");
+		lockfile = cachefiles_get_file(cache, root, ".lock", 1);
+		if (IS_ERR(lockfile)) {
+			kerror("Failed to create lockfile.");
+			ret = PTR_ERR(lockfile);
+			goto error_unsupported;
+		}
+	}
+	/* No reason to hang on to it. */
+	fput(lockfile);
+
 	/* get the culling index file */
-	cull_index = cachefiles_get_file(cache, root, "cull_index");
+	cull_index = cachefiles_get_file(cache, root, "cull_index", 1);
 	if (IS_ERR(cull_index)) {
 		ret = PTR_ERR(cull_index);
 		goto error_bad_index;
 	}
 
+	if (i_size_read(file_inode(cull_index)) & (PAGE_SIZE - 1)) {
+		kerror("cull_index file not a multiple of PAGE_SIZE");
+		cachefiles_mark_dirty(cache);
+	}
+
 	cache->cull_index = cull_index;
 
 	/* get the culling atime file */
-	cull_atimes = cachefiles_get_file(cache, root, "cull_atimes");
+	cull_atimes = cachefiles_get_file(cache, root, "cull_atimes", 1);
 	if (IS_ERR(cull_atimes)) {
 		ret = PTR_ERR(cull_atimes);
 		goto error_bad_atimes;
+	}
+
+	if (i_size_read(file_inode(cull_atimes)) & (sizeof(__le32) - 1)) {
+		kerror("cull_atimes file not a multiple of 4 (__le32)");
+		cachefiles_mark_dirty(cache);
 	}
 
 	cache->cull_atimes = cull_atimes;
@@ -257,7 +296,6 @@ static int cachefiles_daemon_add_cache(struct cachefiles_cache *cache)
 
 	/* done */
 	set_bit(CACHEFILES_READY, &cache->flags);
-	dput(root);
 
 	printk(KERN_INFO "CacheFiles:"
 	       " File cache on %s registered\n",
@@ -299,6 +337,9 @@ error_root_object:
  */
 void cachefiles_daemon_unbind(struct cachefiles_cache *cache)
 {
+	int ret;
+	struct dentry *dentry;
+
 	_enter("");
 
 	if (test_bit(CACHEFILES_READY, &cache->flags)) {
@@ -309,12 +350,32 @@ void cachefiles_daemon_unbind(struct cachefiles_cache *cache)
 		fscache_withdraw_cache(&cache->cache);
 	}
 
+	/* Delete the .lock file. */
+	/* If bind() got as far as determining the root dir
+	 * (It may not have) */
+	if (cache->root) {
+		dentry = cachefiles_lookup_file(cache->root, ".lock");
+		if (IS_ERR(dentry)) {
+			kerror("Error, could not lookup .lock file");
+		} else if (dentry->d_inode) {
+			_debug("Got handle to .lock file");
+			ret = vfs_unlink(cache->root->d_inode, dentry);
+			if (ret) kerror("Failed to delete .lock file.");
+		} else {
+			_debug(".lock file seems to have been deleted already.");
+		}
+
+		if (!IS_ERR(dentry))
+			dput(dentry);
+	}
+
 	cachefiles_cx_free_bitmap(cache);
 	if (cache->cull_atimes)
 		fput(cache->cull_atimes);
 	if (cache->cull_index)
 		fput(cache->cull_index);
 	dput(cache->graveyard);
+	dput(cache->root);
 	mntput(cache->mnt);
 
 	kfree(cache->rootdirname);

fs/cachefiles/cull_index.c
@@ -113,6 +113,7 @@ static int cachefiles_cx_alloc_bitmap(struct cachefiles_cache *cache)
 	struct cachefiles_cx_bitmap *bm;
 	struct rb_node *last, **insert;
 	unsigned long fsize, loop, nbitmaps, nslots;
+	unsigned long naslots;
 
 	_enter("");
 
@@ -125,6 +126,13 @@ static int cachefiles_cx_alloc_bitmap(struct cachefiles_cache *cache)
 	nbitmaps >>= ilog2(SLOTS_PER_BITMAP);
 
 	_debug("nslots=%lu nbitmaps=%lu", nslots, nbitmaps);
+
+	naslots = (i_size_read(file_inode(cache->cull_atimes)) >> 2);
+	if (nslots != naslots) {
+		kerror("Warning: index slots (%lu) does not match atime slots (%lu)\n",
+		       nslots, naslots);
+		cachefiles_mark_dirty(cache);
+	}
 
 	insert = &cache->cull_bitmap.rb_node;
 	last = NULL;

fs/cachefiles/namei.c
@@ -833,19 +833,16 @@ nomem_d_alloc:
 }
 
 /*
- * get a file
+ * Lookup a file
  */
-struct file *cachefiles_get_file(struct cachefiles_cache *cache,
-				 struct dentry *dir,
-				 const char *filename)
+struct dentry *cachefiles_lookup_file(struct dentry *dir,
+				      const char *filename)
 {
 	struct dentry *dentry;
-	struct file *file;
-	struct path path;
 	unsigned long start;
 	int ret;
 
-	_enter(",,%s", filename);
+	_enter("%p,\"%s\"", dir, filename);
 
 	/* search the current directory for the element name */
 	mutex_lock(&dir->d_inode->i_mutex);
@@ -862,6 +859,59 @@ struct file *cachefiles_get_file(struct cachefiles_cache *cache,
 	_debug("dentry -> %p %s",
 	       dentry, dentry->d_inode ? "positive" : "negative");
 
+	mutex_unlock(&dir->d_inode->i_mutex);
+
+	if (dentry->d_inode)
+		_leave(" = [%lu]", dentry->d_inode->i_ino);
+	else
+		_leave(" = [N/A]");
+
+	return dentry;
+
+lookup_error:
+	ret = PTR_ERR(dentry);
+	kerror("Lookup %s failed with error %d", filename, ret);
+	mutex_unlock(&dir->d_inode->i_mutex);
+	return ERR_PTR(ret);
+
+nomem_d_alloc:
+	_leave(" = -ENOMEM");
+	mutex_unlock(&dir->d_inode->i_mutex);
+	return ERR_PTR(-ENOMEM);
+}
+
+/*
+ * open a file.
+ * will return ERR_PTR(-ENOENT) if create == 0 and file does not exist.
+ */
+struct file *cachefiles_get_file(struct cachefiles_cache *cache,
+				 struct dentry *dir,
+				 const char *filename,
+				 bool create)
+{
+	struct dentry *dentry;
+	struct file *file;
+	struct path path;
+	int ret;
+
+	_enter("%p,%p,\"%s\",%u", cache, dir, filename, create);
+
+	/* search the current directory for the element name */
+
+	dentry = cachefiles_lookup_file(dir, filename);
+	if (IS_ERR(dentry)) {
+		goto lookup_error;
+	}
+
+	/* If we don't want to force a creation but it isn't there ... */
+	if (!create && !dentry->d_inode) {
+		dput(dentry);
+		_debug("returning ERR_PTR(-ENOENT).");
+		return ERR_PTR(-ENOENT);
+	}
+
+	_debug("creating file [%s]", filename);
+	mutex_lock(&dir->d_inode->i_mutex);
 	/* we need to create the file if it doesn't exist yet */
 	if (!dentry->d_inode) {
 		ret = cachefiles_has_space(cache, 1, 0);
@@ -920,21 +970,15 @@ check_error:
 	return ERR_PTR(ret);
 
 create_error:
-	mutex_unlock(&dir->d_inode->i_mutex);
 	dput(dentry);
+	mutex_unlock(&dir->d_inode->i_mutex);
 	kerror("mkdir %s failed with error %d", filename, ret);
 	return ERR_PTR(ret);
 
 lookup_error:
-	mutex_unlock(&dir->d_inode->i_mutex);
 	ret = PTR_ERR(dentry);
 	kerror("Lookup %s failed with error %d", filename, ret);
 	return ERR_PTR(ret);
-
-nomem_d_alloc:
-	mutex_unlock(&dir->d_inode->i_mutex);
-	_leave(" = -ENOMEM");
-	return ERR_PTR(-ENOMEM);
 }
 
 /*

ammarfaizi2/linux-block

fs/cachefiles/bind.c
@@ -88,7 +88,7 @@ static int cachefiles_daemon_add_cache(struct cachefiles_cache *cache)
 	struct path path;
 	struct kstatfs stats;
 	struct dentry *graveyard, *cachedir, *root;
-	struct file *cull_index, *cull_atimes;
+	struct file *cull_index, *cull_atimes, *lockfile;
 	const struct cred *saved_cred;
 	int ret;
 
@@ -121,7 +121,7 @@ static int cachefiles_daemon_add_cache(struct cachefiles_cache *cache)
 		goto error_open_root;
 
 	cache->mnt = path.mnt;
-	root = path.dentry;
+	cache->root = root = path.dentry;
 
 	/* check parameters */
 	ret = -EOPNOTSUPP;
@@ -214,20 +214,59 @@ static int cachefiles_daemon_add_cache(struct cachefiles_cache *cache)
 
 	cache->graveyard = graveyard;
 
+	/* Before we create/open the culling/atime indices,
+	 * mark them as locked/dirty. */
+	lockfile = cachefiles_get_file(cache, root, ".lock", 0);
+	if (IS_ERR(lockfile)) {
+		_debug("IS_ERR(lockfile) ...");
+		if (PTR_ERR(lockfile) == -ENOENT) {
+			lockfile = NULL;
+		} else {
+			pr_err("Failed to check for lockfile.");
+			ret = PTR_ERR(lockfile);
+			goto error_unsupported;
+		}
+	}
+
+	if (lockfile) {
+		_debug(".lock file already exists, requesting cache fsck");
+		cachefiles_mark_dirty(cache);
+	} else {
+		_debug("creating .lock file");
+		lockfile = cachefiles_get_file(cache, root, ".lock", 1);
+		if (IS_ERR(lockfile)) {
+			pr_err("Failed to create lockfile.");
+			ret = PTR_ERR(lockfile);
+			goto error_unsupported;
+		}
+	}
+	/* No reason to hang on to it. */
+	fput(lockfile);
+
 	/* get the culling index file */
-	cull_index = cachefiles_get_file(cache, root, "cull_index");
+	cull_index = cachefiles_get_file(cache, root, "cull_index", 1);
 	if (IS_ERR(cull_index)) {
 		ret = PTR_ERR(cull_index);
 		goto error_bad_index;
 	}
 
+	if (i_size_read(file_inode(cull_index)) & (PAGE_SIZE - 1)) {
+		pr_err("cull_index file not a multiple of PAGE_SIZE");
+		cachefiles_mark_dirty(cache);
+	}
+
 	cache->cull_index = cull_index;
 
 	/* get the culling atime file */
-	cull_atimes = cachefiles_get_file(cache, root, "cull_atimes");
+	cull_atimes = cachefiles_get_file(cache, root, "cull_atimes", 1);
 	if (IS_ERR(cull_atimes)) {
 		ret = PTR_ERR(cull_atimes);
 		goto error_bad_atimes;
+	}
+
+	if (i_size_read(file_inode(cull_atimes)) & (sizeof(__le32) - 1)) {
+		pr_err("cull_atimes file not a multiple of 4 (__le32)");
+		cachefiles_mark_dirty(cache);
 	}
 
 	cache->cull_atimes = cull_atimes;
@@ -256,7 +295,6 @@ static int cachefiles_daemon_add_cache(struct cachefiles_cache *cache)
 
 	/* done */
 	set_bit(CACHEFILES_READY, &cache->flags);
-	dput(root);
 
 	pr_info("File cache on %s registered\n", cache->cache.identifier);
 
@@ -296,6 +334,9 @@ error_root_object:
  */
 void cachefiles_daemon_unbind(struct cachefiles_cache *cache)
 {
+	int ret;
+	struct dentry *dentry;
+
 	_enter("");
 
 	if (test_bit(CACHEFILES_READY, &cache->flags)) {
@@ -305,12 +346,32 @@ void cachefiles_daemon_unbind(struct cachefiles_cache *cache)
 		fscache_withdraw_cache(&cache->cache);
 	}
 
+	/* Delete the .lock file. */
+	/* If bind() got as far as determining the root dir
+	 * (It may not have) */
+	if (cache->root) {
+		dentry = cachefiles_lookup_file(cache->root, ".lock");
+		if (IS_ERR(dentry)) {
+			pr_err("Error, could not lookup .lock file");
+		} else if (dentry->d_inode) {
+			_debug("Got handle to .lock file");
+			ret = vfs_unlink(cache->root->d_inode, dentry, NULL);
+			if (ret) pr_err("Failed to delete .lock file.");
+		} else {
+			_debug(".lock file seems to have been deleted already.");
+		}
+
+		if (!IS_ERR(dentry))
+			dput(dentry);
+	}
+
 	cachefiles_cx_free_bitmap(cache);
 	if (cache->cull_atimes)
 		fput(cache->cull_atimes);
 	if (cache->cull_index)
 		fput(cache->cull_index);
 	dput(cache->graveyard);
+	dput(cache->root);
 	mntput(cache->mnt);
 
 	kfree(cache->rootdirname);

fs/cachefiles/cull_index.c
@@ -114,6 +114,7 @@ static int cachefiles_cx_alloc_bitmap(struct cachefiles_cache *cache)
 	struct cachefiles_cx_bitmap *bm;
 	struct rb_node *last, **insert;
 	unsigned long fsize, loop, nbitmaps, nslots;
+	unsigned long naslots;
 
 	_enter("");
 
@@ -126,6 +127,13 @@ static int cachefiles_cx_alloc_bitmap(struct cachefiles_cache *cache)
 	nbitmaps >>= ilog2(SLOTS_PER_BITMAP);
 
 	_debug("nslots=%lu nbitmaps=%lu", nslots, nbitmaps);
+
+	naslots = (i_size_read(file_inode(cache->cull_atimes)) >> 2);
+	if (nslots != naslots) {
+		pr_err("Warning: index slots (%lu) does not match atime slots (%lu)\n",
+		       nslots, naslots);
+		cachefiles_mark_dirty(cache);
+	}
 
 	insert = &cache->cull_bitmap.rb_node;
 	last = NULL;

fs/cachefiles/namei.c
@@ -832,19 +832,16 @@ nomem_d_alloc:
 }
 
 /*
- * get a file
+ * Lookup a file
  */
-struct file *cachefiles_get_file(struct cachefiles_cache *cache,
-				 struct dentry *dir,
-				 const char *filename)
+struct dentry *cachefiles_lookup_file(struct dentry *dir,
+				      const char *filename)
 {
 	struct dentry *dentry;
-	struct file *file;
-	struct path path;
 	unsigned long start;
 	int ret;
 
-	_enter(",,%s", filename);
+	_enter("%p,\"%s\"", dir, filename);
 
 	/* search the current directory for the element name */
 	mutex_lock(&dir->d_inode->i_mutex);
@@ -861,6 +858,59 @@ struct file *cachefiles_get_file(struct cachefiles_cache *cache,
 	_debug("dentry -> %p %s",
 	       dentry, dentry->d_inode ? "positive" : "negative");
 
+	mutex_unlock(&dir->d_inode->i_mutex);
+
+	if (dentry->d_inode)
+		_leave(" = [%lu]", dentry->d_inode->i_ino);
+	else
+		_leave(" = [N/A]");
+
+	return dentry;
+
+lookup_error:
+	ret = PTR_ERR(dentry);
+	pr_err("Lookup %s failed with error %d", filename, ret);
+	mutex_unlock(&dir->d_inode->i_mutex);
+	return ERR_PTR(ret);
+
+nomem_d_alloc:
+	_leave(" = -ENOMEM");
+	mutex_unlock(&dir->d_inode->i_mutex);
+	return ERR_PTR(-ENOMEM);
+}
+
+/*
+ * open a file.
+ * will return ERR_PTR(-ENOENT) if create == 0 and file does not exist.
+ */
+struct file *cachefiles_get_file(struct cachefiles_cache *cache,
+				 struct dentry *dir,
+				 const char *filename,
+				 bool create)
+{
+	struct dentry *dentry;
+	struct file *file;
+	struct path path;
+	int ret;
+
+	_enter("%p,%p,\"%s\",%u", cache, dir, filename, create);
+
+	/* search the current directory for the element name */
+
+	dentry = cachefiles_lookup_file(dir, filename);
+	if (IS_ERR(dentry)) {
+		goto lookup_error;
+	}
+
+	/* If we don't want to force a creation but it isn't there ... */
+	if (!create && !dentry->d_inode) {
+		dput(dentry);
+		_debug("returning ERR_PTR(-ENOENT).");
+		return ERR_PTR(-ENOENT);
+	}
+
+	_debug("creating file [%s]", filename);
+	mutex_lock(&dir->d_inode->i_mutex);
 	/* we need to create the file if it doesn't exist yet */
 	if (!dentry->d_inode) {
 		ret = cachefiles_has_space(cache, 1, 0);
@@ -919,21 +969,15 @@ check_error:
 	return ERR_PTR(ret);
 
 create_error:
-	mutex_unlock(&dir->d_inode->i_mutex);
 	dput(dentry);
+	mutex_unlock(&dir->d_inode->i_mutex);
 	pr_err("mkdir %s failed with error %d", filename, ret);
 	return ERR_PTR(ret);
 
 lookup_error:
-	mutex_unlock(&dir->d_inode->i_mutex);
 	ret = PTR_ERR(dentry);
 	pr_err("Lookup %s failed with error %d", filename, ret);
 	return ERR_PTR(ret);
-
-nomem_d_alloc:
-	mutex_unlock(&dir->d_inode->i_mutex);
-	_leave(" = -ENOMEM");
-	return ERR_PTR(-ENOMEM);
 }
 
 /*