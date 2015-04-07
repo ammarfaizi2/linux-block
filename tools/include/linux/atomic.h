c950b37b2c47aee572a6bb62a950f578b49d7384
Arnaldo Carvalho de Melo <acme@redhat.com>
2015-04-07 11:53:41 -0300

    tools include: Add basic atomic.h implementation from the kernel sources
    Uses the arch/x86/ kernel code for x86_64/i386, fallbacking to a gcc
    intrinsics implementation that has been tested in at least sparc64.
    
    Will be used for reference counting in tools/perf.
    
    Acked-by: David Ahern <dsahern@gmail.com>
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: Don Zickus <dzickus@redhat.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lkml.kernel.org/n/tip-knfpjowhgyh6x4z0kfuk389j@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

---
+++ tools/include/linux/atomic.h
@@ -0,0 +1,8 @@
+#ifndef __TOOLS_LINUX_ATOMIC_H
+#define __TOOLS_LINUX_ATOMIC_H
+#if defined(__i386__) || defined(__x86_64__)
+#include "../../arch/x86/include/asm/atomic.h"
+#else
+#include <asm-generic/atomic-gcc.h>
+#endif
+#endif /* __TOOLS_LINUX_ATOMIC_H */
