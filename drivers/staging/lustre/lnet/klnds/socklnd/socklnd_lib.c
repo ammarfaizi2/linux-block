c27a8666b212da9e06a69c3cf2c75aaed01b40cf
Al Viro <viro@zeniv.linux.org.uk>
2016-01-10 01:31:37 -0500

    ksocknal_lib_recv_iov(): recvmsg doesn't bugger iovec anymore...
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/staging/lustre/lnet/klnds/socklnd/socklnd_lib.c
+++ drivers/staging/lustre/lnet/klnds/socklnd/socklnd_lib.c
@@ -205,14 +205,7 @@ ksocknal_lib_eager_ack(ksock_conn_t *conn)
 int
 ksocknal_lib_recv_iov(ksock_conn_t *conn)
 {
-#if SOCKNAL_SINGLE_FRAG_RX
-	struct kvec scratch;
-	struct kvec *scratchiov = &scratch;
-	unsigned int niov = 1;
-#else
-	struct kvec *scratchiov = conn->ksnc_scheduler->kss_scratch_iov;
 	unsigned int niov = conn->ksnc_rx_niov;
-#endif
 	struct kvec *iov = conn->ksnc_rx_iov;
 	struct msghdr msg = {
 		.msg_flags = 0
@@ -224,20 +217,15 @@ ksocknal_lib_recv_iov(ksock_conn_t *conn)
 	int sum;
 	__u32 saved_csum;
 
-	/*
-	 * NB we can't trust socket ops to either consume our iovs
-	 * or leave them alone.
-	 */
 	LASSERT(niov > 0);
 
-	for (nob = i = 0; i < niov; i++) {
-		scratchiov[i] = iov[i];
-		nob += scratchiov[i].iov_len;
-	}
+	for (nob = i = 0; i < niov; i++)
+		nob += iov[i].iov_len;
+
 	LASSERT(nob <= conn->ksnc_rx_nob_wanted);
 
-	rc = kernel_recvmsg(conn->ksnc_sock, &msg, scratchiov, niov, nob,
-			    MSG_DONTWAIT);
+	iov_iter_kvec(&msg.msg_iter, READ | ITER_KVEC, iov, niov, nob);
+	rc = sock_recvmsg(conn->ksnc_sock, &msg, MSG_DONTWAIT);
 
 	saved_csum = 0;
 	if (conn->ksnc_proto == &ksocknal_protocol_v2x) {
