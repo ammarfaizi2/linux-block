5ff233becf3275739b95586520eca8932950d0aa
Al Viro <viro@zeniv.linux.org.uk>
2016-01-10 01:13:51 -0500

    lustre: simplify the living hell out of ksocknal_lib_recv_kiov()
    ... by using ITER_BVEC recvmsg
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/staging/lustre/lnet/klnds/socklnd/socklnd.h
+++ drivers/staging/lustre/lnet/klnds/socklnd/socklnd.h
@@ -87,8 +87,10 @@ typedef struct				  /* per scheduler state */
 	int                     kss_nconns;     /* # connections assigned to
 						 * this scheduler */
 	struct ksock_sched_info *kss_info;	/* owner of it */
-	struct page             *kss_rx_scratch_pgs[LNET_MAX_IOV];
-	struct kvec             kss_scratch_iov[LNET_MAX_IOV];
+	union {
+		struct bio_vec          kss_scratch_bvec[LNET_MAX_IOV];
+		struct kvec             kss_scratch_iov[LNET_MAX_IOV];
+	};
 } ksock_sched_t;
 
 struct ksock_sched_info {
