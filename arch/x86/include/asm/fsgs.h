7c173a49db867688ce18be3db032dc102bb717e9
Andi Kleen <ak@linux.intel.com>
2016-03-21 09:16:01 -0700

    x86: Add intrinsics/macros for new rd/wr fs/gs base instructions
    Add C intrinsics and assembler macros for the new rd/wr fs/gs base
    instructions and for swapgs.
    
    Very straight forward. Used in followon patch.
    
    For assembler only a few standard registers used by entry_64.S
    are defined.
    
    [luto: rename the variables from fs and gs to fsbase and gsbase and
     make fsgs.h safe to include on 32-bit kernels.]
    
    v2: Use __always_inline
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Andy Lutomirski <luto@kernel.org>

--- arch/x86/include/asm/fsgs.h
+++ arch/x86/include/asm/fsgs.h
@@ -32,6 +32,56 @@ static inline unsigned long read_inactive_gs_base(void)
 extern unsigned long read_task_fsbase(struct task_struct *task);
 extern unsigned long read_task_gsbase(struct task_struct *task);
 
+static __always_inline void swapgs(void)
+{
+	asm volatile("swapgs" ::: "memory");
+}
+
+/* Must be protected by X86_FEATURE_FSGSBASE check. */
+
+static __always_inline unsigned long rdgsbase(void)
+{
+	unsigned long gsbase;
+	asm volatile(".byte 0xf3,0x48,0x0f,0xae,0xc8 # rdgsbaseq %%rax"
+			: "=a" (gsbase)
+			:: "memory");
+	return gsbase;
+}
+
+static __always_inline unsigned long rdfsbase(void)
+{
+	unsigned long fsbase;
+	asm volatile(".byte 0xf3,0x48,0x0f,0xae,0xc0 # rdfsbaseq %%rax"
+			: "=a" (fsbase)
+			:: "memory");
+	return fsbase;
+}
+
+static __always_inline void wrgsbase(unsigned long gsbase)
+{
+	asm volatile(".byte 0xf3,0x48,0x0f,0xae,0xd8 # wrgsbaseq %%rax"
+			:: "a" (gsbase)
+			: "memory");
+}
+
+static __always_inline void wrfsbase(unsigned long fsbase)
+{
+	asm volatile(".byte 0xf3,0x48,0x0f,0xae,0xd0 # wrfsbaseq %%rax"
+			:: "a" (fsbase)
+			: "memory");
+}
+
+#endif /* CONFIG_X86_64 */
+
+#else /* __ASSEMBLY__ */
+
+#ifdef CONFIG_X86_64
+
+/* Handle old assemblers. */
+#define RDGSBASE_R15 .byte 0xf3,0x49,0x0f,0xae,0xcf
+#define WRGSBASE_RDI .byte 0xf3,0x48,0x0f,0xae,0xdf
+#define WRGSBASE_R15 .byte 0xf3,0x49,0x0f,0xae,0xdf
+
 #endif /* CONFIG_X86_64 */
 
 #endif /* __ASSEMBLY__ */
