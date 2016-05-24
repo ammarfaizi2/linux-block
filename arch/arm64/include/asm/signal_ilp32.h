c0751c09064c7117252a1eb66f5926b5d6a1f844
Yury Norov <ynorov@caviumnetworks.comk>
2016-05-24 03:04:50 +0300

    arm64: ilp32: introduce ilp32-specific handlers for sigframe and ucontext
    ILP32 uses AARCH32 compat structures and syscall handlers for signals.
    But ILP32 struct rt_sigframe  and ucontext differs from both LP64 and
    AARCH32. So some specific mechanism is needed to take care of it.
    
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>

---
+++ arch/arm64/include/asm/signal_ilp32.h
@@ -0,0 +1,35 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ASM_SIGNAL_ILP32_H
+#define __ASM_SIGNAL_ILP32_H
+
+#ifdef CONFIG_ARM64_ILP32
+
+#include <linux/compat.h>
+
+int ilp32_setup_rt_frame(int usig, struct ksignal *ksig, sigset_t *set,
+			  struct pt_regs *regs);
+
+#else
+
+static inline int ilp32_setup_rt_frame(int usig, struct ksignal *ksig, sigset_t *set,
+			  struct pt_regs *regs)
+{
+	return -ENOSYS;
+}
+
+#endif /* CONFIG_ARM64_ILP32 */
+
+#endif /* __ASM_SIGNAL_ILP32_H */
