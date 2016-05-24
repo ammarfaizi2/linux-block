559cb4b0b98b53a71a089200a450d4eda9bd3dbd
Yury Norov <ynorov@caviumnetworks.com>
2016-05-24 03:04:42 +0300

    arm64: introduce is_a32_task and is_a32_thread (for AArch32 compat)
    Based on patch of Andrew Pinski.
    
    This patch introduces is_a32_compat_task and is_a32_thread so it is
    easier to say this is a a32 specific thread or a generic compat thread/task.
    Corresponding functions are located in <asm/is_compat.h> to avoid mess in
    headers.
    
    Some files include both <linux/compat.h> and <asm/compat.h>,
    and this is wrong because <linux/compat.h> has <asm/compat.h> already
    included. It was fixed too.
    
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>
    Signed-off-by: Andrew Pinski <Andrew.Pinski@caviumnetworks.com>
    Signed-off-by: Bamvor Jian Zhang <bamvor.zhangjian@linaro.org>

--- arch/arm64/include/asm/processor.h
+++ arch/arm64/include/asm/processor.h
@@ -34,6 +34,7 @@
 #include <linux/string.h>
 
 #include <asm/alternative.h>
+#include <asm/is_compat.h>
 #include <asm/fpsimd.h>
 #include <asm/hw_breakpoint.h>
 #include <asm/lse.h>
@@ -47,9 +48,9 @@
  */
 #ifdef CONFIG_COMPAT
 #define TASK_SIZE_32		UL(0x100000000)
-#define TASK_SIZE		(test_thread_flag(TIF_32BIT) ? \
+#define TASK_SIZE		(is_compat_task() ?		\
 				TASK_SIZE_32 : TASK_SIZE_64)
-#define TASK_SIZE_OF(tsk)	(test_tsk_thread_flag(tsk, TIF_32BIT) ? \
+#define TASK_SIZE_OF(tsk)	(is_compat_thread(tsk) ? \
 				TASK_SIZE_32 : TASK_SIZE_64)
 #else
 #define TASK_SIZE		TASK_SIZE_64
@@ -60,7 +61,7 @@
 #define STACK_TOP_MAX		TASK_SIZE_64
 #ifdef CONFIG_COMPAT
 #define AARCH32_VECTORS_BASE	0xffff0000
-#define STACK_TOP		(test_thread_flag(TIF_32BIT) ? \
+#define STACK_TOP		(is_compat_task() ? \
 				AARCH32_VECTORS_BASE : STACK_TOP_MAX)
 #else
 #define STACK_TOP		STACK_TOP_MAX
@@ -114,7 +115,7 @@ struct thread_struct {
 #define task_user_tls(t)						\
 ({									\
 	unsigned long *__tls;						\
-	if (is_compat_thread(task_thread_info(t)))			\
+	if (is_a32_compat_thread(task_thread_info(t)))			\
 		__tls = &(t)->thread.tp2_value;				\
 	else								\
 		__tls = &(t)->thread.tp_value;				\
