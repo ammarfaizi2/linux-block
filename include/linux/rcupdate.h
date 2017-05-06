8450fdfd95d35796b2bb45316565b4d0b3f56808
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2017-05-06 13:06:22 -0700

    fixup! rcu: Refactor #includes from include/linux/rcupdate.h

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -34,14 +34,18 @@
 #define __LINUX_RCUPDATE_H
 
 #include <linux/types.h>
+#include <linux/cache.h>
+#include <linux/spinlock.h>
+#include <linux/threads.h>
+#include <linux/cpumask.h>
+#include <linux/seqlock.h>
+#include <linux/lockdep.h>
+#include <linux/debugobjects.h>
+#include <linux/bug.h>
 #include <linux/compiler.h>
 #include <linux/irqflags.h>
-#include <linux/preempt.h>
-#include <linux/bottom_half.h>
-#include <linux/atomic.h>
-#include <linux/lockdep.h>
-#include <asm/processor.h>
-#include <linux/cpumask.h>
+
+#include <asm/barrier.h>
 
 #define ULONG_CMP_GE(a, b)	(ULONG_MAX / 2 >= (a) - (b))
 #define ULONG_CMP_LT(a, b)	(ULONG_MAX / 2 < (a) - (b))
