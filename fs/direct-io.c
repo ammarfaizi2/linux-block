f7953179813a8928106359005185bfaa10d6d2cc
Jens Axboe <axboe@kernel.dk>
2017-06-16 10:48:58 -0600

    fs: add O_DIRECT support for sending down write life time hints
    Reviewed-by: Andreas Dilger <adilger@dilger.ca>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/direct-io.c
+++ fs/direct-io.c
@@ -386,6 +386,8 @@ dio_bio_alloc(struct dio *dio, struct dio_submit *sdio,
 	else
 		bio->bi_end_io = dio_bio_end_io;
 
+	bio->bi_opf |= write_hint_to_opf(iocb_write_hint(dio->iocb));
+
 	sdio->bio = bio;
 	sdio->logical_offset_in_bio = sdio->cur_page_fs_offset;
 }
