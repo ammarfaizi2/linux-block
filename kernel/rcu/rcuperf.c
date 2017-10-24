7713556bb44cc1ad198fa3349f121f1fdcf4cbe1
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2017-10-24 08:33:41 -0700

    rcuperf: Eliminate cond_resched_rcu_qs() in favor of cond_resched()
    Now that cond_resched() also provides RCU quiescent states when
    needed, it can be used in place of cond_resched_rcu_qs().  This
    commit therefore makes this change.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/rcu/rcuperf.c
+++ kernel/rcu/rcuperf.c
@@ -354,7 +354,7 @@ static bool __maybe_unused torturing_tasks(void)
  */
 static void rcu_perf_wait_shutdown(void)
 {
-	cond_resched_rcu_qs();
+	cond_resched();
 	if (atomic_read(&n_rcu_perf_writer_finished) < nrealwriters)
 		return;
 	while (!torture_must_stop())
