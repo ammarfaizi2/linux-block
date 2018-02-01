b4945b6992c27509b939c0e809ddd136dd91aa70
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2018-01-31 19:40:22 -0800

    rcu: Trace expedited GP delays due to transitioning CPUs
    If a CPU is transitioning to or from offline state, an expedited
    grace period may undergo a timed wait.  This timed wait can unduly
    delay grace periods, so this commit adds a trace statement to make
    it visible.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/rcu/tree_exp.h
+++ kernel/rcu/tree_exp.h
@@ -428,6 +428,7 @@ retry_ipi:
 			    (rnp->expmask & mask)) {
 				/* Online, so delay for a bit and try again. */
 				raw_spin_unlock_irqrestore_rcu_node(rnp, flags);
+				trace_rcu_exp_grace_period(rsp->name, rcu_exp_gp_seq_endval(rsp), TPS("selectofl"));
 				schedule_timeout_uninterruptible(1);
 				goto retry_ipi;
 			}
