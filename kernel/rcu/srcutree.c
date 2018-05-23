dbb810d0623f7615ff061995f86e494ab6ad2c6a
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2018-05-22 20:05:43 -0700

    fixup! srcu: Add address of first callback to rcutorture output
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/rcu/srcutree.c
+++ kernel/rcu/srcutree.c
@@ -1290,7 +1290,7 @@ void srcu_torture_stats_print(struct srcu_struct *sp, char *tt, char *tf)
 
 		c0 = l0 - u0;
 		c1 = l1 - u1;
-		pr_cont(" %d(%ld,%ld %p)",
+		pr_cont(" %d(%ld,%ld %1p)",
 			cpu, c0, c1, rcu_segcblist_head(&sdp->srcu_cblist));
 		s0 += c0;
 		s1 += c1;
