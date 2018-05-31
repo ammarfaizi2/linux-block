9d0046d52f038872c745c9e52ef56c8002fddf5b
David Howells <dhowells@redhat.com>
2018-05-31 22:53:48 +0100

    vfs: Provide an fspick() system call
    Provide an fspick() system call that can be used to pick an existing
    mountpoint into an fs_context which can thereafter be used to reconfigure a
    superblock (equivalent of the superblock side of -o remount).
    
    This looks like:
    
    	int fd = fspick(AT_FDCWD, "/mnt",
    			FSPICK_CLOEXEC | FSPICK_NO_AUTOMOUNT);
    	write(fd, "o intr");
    	write(fd, "o noac");
    	write(fd, "x reconfigure");
    
    At the point of fspick being called, the file descriptor referring to the
    filesystem context is in exactly the same state as the one that was created
    by fsopen() after fsmount() has been successfully called.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- include/uapi/linux/fs.h
+++ include/uapi/linux/fs.h
@@ -345,4 +345,9 @@ typedef int __bitwise __kernel_rwf_t;
 
 #define FSMOUNT_CLOEXEC		0x00000001
 
+#define FSPICK_CLOEXEC		0x00000001
+#define FSPICK_SYMLINK_NOFOLLOW	0x00000002
+#define FSPICK_NO_AUTOMOUNT	0x00000004
+#define FSPICK_EMPTY_PATH	0x00000008
+
 #endif /* _UAPI_LINUX_FS_H */
