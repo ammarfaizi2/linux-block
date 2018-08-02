e5b0615187257c93793048e8c372c22abecdea02
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2018-08-02 14:06:32 -0700

    EXP tools/memory-model: Make checkghlitmus.sh use parseargs.sh
    This commit regularizes checkghlitmus.sh by having is include parseargs.sh
    and by having it send the final determination to stderr, thus allowing
    the verbose messages to be redirected for later perusal while still
    getting an immediate overall verdict.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- tools/memory-model/scripts/checkghlitmus.sh
+++ tools/memory-model/scripts/checkghlitmus.sh
@@ -4,16 +4,18 @@
 # Runs the C-language litmus tests having a maximum number of processes
 # to run, defaults to 6.
 #
-# sh checkghlitmus.sh [ maxprocesses ]
+# sh checkghlitmus.sh
 #
-# Run from the Linux kernel tools/memory-model directory.
+# Run from the Linux kernel tools/memory-model directory.  See the
+# parseargs.sh scripts for arguments.
 
-np=${1-6}
+. scripts/parseargs.sh
 
 T=/tmp/checkghlitmus.sh.$$
 trap 'rm -rf $T' 0
 mkdir $T
 
+# Clone the repository if it is not already present.
 if test -d litmus
 then
 	:
@@ -21,27 +23,20 @@ else
 	git clone https://github.com/paulmckrcu/litmus
 	( cd litmus; git checkout origin/master )
 fi
+
+# Create a list of C-language litmus tests with "Result:" commands and
+# no more than the specified number of processes.
 find litmus -name '*.litmus' -exec grep -l -m 1 "^C " {} \; > $T/list-C
 xargs < $T/list-C egrep -l '^ \* Result: (Never|Sometimes|Always)' > $T/list-C-result
-xargs < $T/list-C-result grep -L "^P${np}" > $T/list-C-result-short
+xargs < $T/list-C-result grep -L "^P${LKMM_PROCS}" > $T/list-C-result-short
 
-# Find the checklitmus script.  If it is not where we expect it, then
-# assume that the caller has the PATH environment variable set
-# appropriately.
-if test -x scripts/checklitmus.sh
-then
-	clscript=scripts/checklitmus.sh
-else
-	clscript=checklitmus.sh
-fi
-
-sed < $T/list-C-result-short -e 's,^.*$,if ! '"$clscript"' & ; then ret=1; fi,' > $T/script
+sed < $T/list-C-result-short -e 's,^.*$,if ! scripts/checklitmus.sh & ; then ret=1; fi,' > $T/script
 ret=0
 . $T/script
 if test "$ret" -ne 0
 then
-	echo " ^^^ VERIFICATION MISMATCHES"
+	echo " ^^^ VERIFICATION MISMATCHES" 1>&2
 else
-	echo All litmus tests verified as was expected.
+	echo All litmus tests verified as was expected. 1>&2
 fi
 exit $ret
