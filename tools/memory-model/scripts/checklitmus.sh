109accdd5fed6fb12d0c22131b76a30c77ad50c6
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2018-08-10 14:46:09 -0700

    EXP tools/memory-model: Abstract result judgment from running tests
    Currently, the checklitmus.sh script runs a litmus test and then
    compares the result to that of the "Result" comment.  This works, but
    it would be good to allow checkghlitmus.sh to leverage the concurrency
    capabilities of runlitmushist.sh to run the tests while also leveraging
    the results-judgment capabilities currently locked up in checklitmus.sh.
    
    This commit therefore creates a new judgelitmus.sh script that checks
    a .litmus file against its .litmus.out counterpart.  This allows
    checkghlitmus.sh to invoke runlitmushist.sh to do any needed herd runs,
    then invoke judgelitmus.sh on all the .litmus files and their .litmus.out
    counterparts.  While in the area, this commit makes checklitmus.sh pay
    attention to the LKMM_DESTDIR environment variable.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- tools/memory-model/scripts/checklitmus.sh
+++ tools/memory-model/scripts/checklitmus.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 # SPDX-License-Identifier: GPL-2.0+
 #
-# Run a herd test and check the result against a "Result:" comment within
-# the litmus test.  If the verification result does not match that specified
-# in the litmus test, this script prints an error message prefixed with
-# "!!!" and exits with a non-zero status.  It also outputs verification
+# Run a herd test and invokes judgelitmus.sh to check the result against
+# a "Result:" comment within the litmus test.  It also outputs verification
 # results to a file whose name is that of the specified litmus test, but
 # with ".out" appended.
 #
@@ -12,7 +10,8 @@
 #	checklitmus.sh file.litmus
 #
 # Run this in the directory containing the memory model, specifying the
-# pathname of the litmus test to check.
+# pathname of the litmus test to check.  The caller is expected to have
+# properly set up the LKMM environment variables.
 #
 # Copyright IBM Corporation, 2018
 #
@@ -28,43 +27,8 @@ else
 	echo ' --- ' error: \"$litmus\" is not a readable file
 	exit 255
 fi
-if grep -q '^ \* Result: ' $litmus
-then
-	outcome=`grep -m 1 '^ \* Result: ' $litmus | awk '{ print $3 }'`
-else
-	outcome=specified
-fi
 
 echo Herd options: $herdoptions > $LKMM_DESTDIR/$litmus.out
 /usr/bin/time $LKMM_TIMEOUT_CMD herd7 $herdoptions $litmus >> $LKMM_DESTDIR/$litmus.out 2>&1
-grep "Herd options:" $litmus.out
-grep '^Observation' $litmus.out
-if grep -q '^Observation' $litmus.out
-then
-	:
-else
-	cat $litmus.out
-	echo ' !!! Verification error' $litmus
-	echo ' !!! Verification error' >> $litmus.out 2>&1
-	exit 255
-fi
-if test "$outcome" = DEADLOCK
-then
-	if grep '^Observation' $litmus.out | grep -q 'Never 0 0$'
-	then
-		ret=0
-	else
-		echo " !!! Unexpected non-$outcome verification" $litmus
-		echo " !!! Unexpected non-$outcome verification" >> $litmus.out 2>&1
-		ret=1
-	fi
-elif grep '^Observation' $litmus.out | grep -q $outcome || test "$outcome" = Maybe
-then
-	ret=0
-else
-	echo " !!! Unexpected non-$outcome verification" $litmus
-	echo " !!! Unexpected non-$outcome verification" >> $litmus.out 2>&1
-	ret=1
-fi
-tail -2 $litmus.out | head -1
-exit $ret
+
+scripts/judgelitmus.sh $litmus
