02291976aec417b9c8b0a4ce12062cf2a630e7f8
David Howells <dhowells@redhat.com>
2018-08-12 10:29:22 +0100

    vfs: syscall: Add fsinfo() to query filesystem information
    Add a system call to allow filesystem information to be queried.  A request
    value can be given to indicate the desired attribute.  Support is provided
    for enumerating multi-value attributes.
    
    ===============
    NEW SYSTEM CALL
    ===============
    
    The new system call looks like:
    
    	int ret = fsinfo(int dfd,
    			 const char *filename,
    			 const struct fsinfo_params *params,
    			 void *buffer,
    			 size_t buf_size);
    
    The params parameter optionally points to a block of parameters:
    
    	struct fsinfo_params {
    		__u32	at_flags;
    		__u32	request;
    		__u32	Nth;
    		__u32	Mth;
    		__u32	__reserved[6];
    	};
    
    If params is NULL, it is assumed params->request should be
    fsinfo_attr_statfs, params->Nth should be 0, params->Mth should be 0 and
    params->at_flags should be 0.
    
    If params is given, all of params->__reserved[] must be 0.
    
    dfd, filename and params->at_flags indicate the file to query.  There is no
    equivalent of lstat() as that can be emulated with fsinfo() by setting
    AT_SYMLINK_NOFOLLOW in params->at_flags.  There is also no equivalent of
    fstat() as that can be emulated by passing a NULL filename to fsinfo() with
    the fd of interest in dfd.  AT_NO_AUTOMOUNT can also be used to an allow
    automount point to be queried without triggering it.
    
    params->request indicates the attribute/attributes to be queried.  This can
    be one of:
    
    	FSINFO_ATTR_STATFS		- statfs-style info
    	FSINFO_ATTR_FSINFO		- Information about fsinfo()
    	FSINFO_ATTR_IDS			- Filesystem IDs
    	FSINFO_ATTR_LIMITS		- Filesystem limits
    	FSINFO_ATTR_SUPPORTS		- What's supported in statx(), IOC flags
    	FSINFO_ATTR_CAPABILITIES	- Filesystem capabilities
    	FSINFO_ATTR_TIMESTAMP_INFO	- Inode timestamp info
    	FSINFO_ATTR_VOLUME_ID		- Volume ID (string)
    	FSINFO_ATTR_VOLUME_UUID		- Volume UUID
    	FSINFO_ATTR_VOLUME_NAME		- Volume name (string)
    	FSINFO_ATTR_CELL_NAME		- Cell name (string)
    	FSINFO_ATTR_DOMAIN_NAME		- Domain name (string)
    	FSINFO_ATTR_REALM_NAME		- Realm name (string)
    	FSINFO_ATTR_SERVER_NAME		- Name of the Nth server (string)
    	FSINFO_ATTR_SERVER_ADDRESS	- Mth address of the Nth server
    	FSINFO_ATTR_PARAMETER		- Nth mount parameter (string)
    	FSINFO_ATTR_SOURCE		- Nth mount source name (string)
    	FSINFO_ATTR_NAME_ENCODING	- Filename encoding (string)
    	FSINFO_ATTR_NAME_CODEPAGE	- Filename codepage (string)
    	FSINFO_ATTR_IO_SIZE		- I/O size hints
    
    Some attributes (such as the servers backing a network filesystem) can have
    multiple values.  These can be enumerated by setting params->Nth and
    params->Mth to 0, 1, ... until ENODATA is returned.
    
    buffer and buf_size point to the reply buffer.  The buffer is filled up to
    the specified size, even if this means truncating the reply.  The full size
    of the reply is returned.  In future versions, this will allow extra fields
    to be tacked on to the end of the reply, but anyone not expecting them will
    only get the subset they're expecting.  If either buffer of buf_size are 0,
    no copy will take place and the data size will be returned.
    
    At the moment, this will only work on x86_64 and i386 as it requires the
    system call to be wired up.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: linux-api@vger.kernel.org

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -64,6 +64,8 @@ struct fscrypt_operations;
 struct fs_context;
 struct fsconfig_parser;
 struct fsconfig_param;
+struct fsinfo_kparams;
+enum fsinfo_attribute;
 
 extern void __init inode_init(void);
 extern void __init inode_init_early(void);
@@ -1849,6 +1851,7 @@ struct super_operations {
 	int (*thaw_super) (struct super_block *);
 	int (*unfreeze_fs) (struct super_block *);
 	int (*statfs) (struct dentry *, struct kstatfs *);
+	int (*get_fsinfo) (struct dentry *, struct fsinfo_kparams *);
 	int (*remount_fs) (struct super_block *, int *, char *, size_t);
 	int (*reconfigure) (struct super_block *, struct fs_context *);
 	void (*umount_begin) (struct super_block *);
@@ -2226,6 +2229,7 @@ extern int iterate_mounts(int (*)(struct vfsmount *, void *), void *,
 extern int vfs_statfs(const struct path *, struct kstatfs *);
 extern int user_statfs(const char __user *, struct kstatfs *);
 extern int fd_statfs(int, struct kstatfs *);
+extern int vfs_fsinfo(const struct path *, struct fsinfo_kparams *);
 extern int freeze_super(struct super_block *super);
 extern int thaw_super(struct super_block *super);
 extern bool our_mnt(struct vfsmount *mnt);
