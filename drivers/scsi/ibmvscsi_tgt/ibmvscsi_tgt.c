befb8764d2f4a1d82b3ed136780dc05a998ca08e
Rob Herring <robh@kernel.org>
2018-08-27 20:04:57 -0500

    WIP: vio
    Signed-off-by: Rob Herring <robh@kernel.org>

--- drivers/scsi/ibmvscsi_tgt/ibmvscsi_tgt.c
+++ drivers/scsi/ibmvscsi_tgt/ibmvscsi_tgt.c
@@ -3474,8 +3474,7 @@ static int ibmvscsis_probe(struct vio_dev *vdev,
 		vscsi->dds.window[LOCAL].liobn,
 		vscsi->dds.window[REMOTE].liobn);
 
-	strcpy(vscsi->eye, "VSCSI ");
-	strncat(vscsi->eye, vdev->name, MAX_EYE);
+	snprintf(vscsi->eye, MAX_EYE, "VSCSI %pOFn", vdev->dev.of_node);
 
 	vscsi->dds.unit_id = vdev->unit_address;
 	strncpy(vscsi->dds.partition_name, partition_name,
