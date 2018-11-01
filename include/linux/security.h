821fcaa68a98db6146f3eb012cec1839125b9a52
David Howells <dhowells@redhat.com>
2018-11-01 23:07:25 +0000

    vfs: Remove unused code after filesystem context changes
    Remove code that is now unused after the filesystem context changes.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- include/linux/security.h
+++ include/linux/security.h
@@ -261,8 +261,6 @@ int security_sb_mountpoint(struct fs_context *fc, struct path *mountpoint,
 int security_sb_alloc(struct super_block *sb);
 void security_sb_free(struct super_block *sb);
 int security_sb_eat_lsm_opts(char *options, struct security_mnt_opts *opts);
-int security_sb_remount(struct super_block *sb, void *data);
-int security_sb_kern_mount(struct super_block *sb, int flags, void *data);
 int security_sb_show_options(struct seq_file *m, struct super_block *sb);
 int security_sb_statfs(struct dentry *dentry);
 int security_sb_mount(const char *dev_name, const struct path *path,
@@ -604,16 +602,6 @@ static inline void security_sb_free(struct super_block *sb)
 
 static inline int security_sb_eat_lsm_opts(char *options,
 					   struct security_mnt_opts *opts)
-{
-	return 0;
-}
-
-static inline int security_sb_remount(struct super_block *sb, void *data)
-{
-	return 0;
-}
-
-static inline int security_sb_kern_mount(struct super_block *sb, int flags, void *data)
 {
 	return 0;
 }
