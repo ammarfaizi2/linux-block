1f4b91a93080bb70213576765b405696ce5092d2
Stephen Boyd <sboyd@kernel.org>
2018-11-06 10:13:47 -0800

    clk: mediatek: Drop THIS_MODULE from platform_driver
    This is already set by platform_driver_register(), so we can remove it
    here.
    
    Cc: Ryder Lee <ryder.lee@mediatek.com>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/mediatek/clk-mt2712.c
+++ drivers/clk/mediatek/clk-mt2712.c
@@ -1445,7 +1445,6 @@ static struct platform_driver clk_mt2712_drv = {
 	.probe = platform_driver_probe_by_of_match_data,
 	.driver = {
 		.name = "clk-mt2712",
-		.owner = THIS_MODULE,
 		.of_match_table = of_match_clk_mt2712,
 	},
 };
