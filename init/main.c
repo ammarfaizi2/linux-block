ae90ffa14ce9dbcb738c775791aac0367d906a7f
Paul E. McKenney <paulmck@linux.ibm.com>
2018-11-06 18:58:01 -0800

    main: Replace rcu_barrier_sched() with rcu_barrier()
    Now that all RCU flavors have been consolidated, rcu_barrier_sched()
    is but a synonym for rcu_barrier().  This commit therefore replaces
    the former with the latter.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: "Steven Rostedt (VMware)" <rostedt@goodmis.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: <linux-mm@kvack.org>

--- init/main.c
+++ init/main.c
@@ -1051,7 +1051,7 @@ static void mark_readonly(void)
 		 * flushed so that we don't hit false positives looking for
 		 * insecure pages which are W+X.
 		 */
-		rcu_barrier_sched();
+		rcu_barrier();
 		mark_rodata_ro();
 		rodata_test();
 	} else
