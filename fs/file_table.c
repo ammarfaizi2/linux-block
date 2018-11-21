67e82f8ba664f6aa16186d7b4a1a5ecc3a319c01
Jens Axboe <axboe@kernel.dk>
2018-11-21 10:32:39 -0700

    fs: add fget_many() and fput_many()
    Some uses cases repeatedly get and put references to the same file, but
    the only exposed interface is doing these one at the time. As each of
    these entail an atomic inc or dec on a shared structure, that cost can
    add up.
    
    Add fget_many(), which works just like fget(), except it takes an
    argument for how many references to get on the file. Ditto fput_many(),
    which can drop an arbitrary number of references to a file.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/file_table.c
+++ fs/file_table.c
@@ -326,9 +326,9 @@ void flush_delayed_fput(void)
 
 static DECLARE_DELAYED_WORK(delayed_fput_work, delayed_fput);
 
-void fput(struct file *file)
+void fput_many(struct file *file, unsigned int refs)
 {
-	if (atomic_long_dec_and_test(&file->f_count)) {
+	if (atomic_long_sub_and_test(refs, &file->f_count)) {
 		struct task_struct *task = current;
 
 		if (likely(!in_interrupt() && !(task->flags & PF_KTHREAD))) {
@@ -346,6 +346,12 @@ void fput(struct file *file)
 			schedule_delayed_work(&delayed_fput_work, 1);
 	}
 }
+
+void fput(struct file *file)
+{
+	fput_many(file, 1);
+}
+
 
 /*
  * synchronous analog of fput(); for kernel threads that might be needed
