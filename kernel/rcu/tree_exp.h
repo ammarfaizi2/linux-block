a3c364ec02d62f1cf544ef45040189d4114ab250
Paul E. McKenney <paulmck@linux.ibm.com>
2018-11-26 11:12:39 -0800

    rcu: Remove preemption disabling from expedited CPU selection
    It turns out that it is queue_delayed_work_on() rather than
    queue_work_on() that has difficulties when used concurrently with
    CPU-hotplug removal operations.  It is therefore unnecessary to protect
    CPU identification and queue_work_on() with preempt_disable().
    
    This commit therefore removes the preempt_disable() and preempt_enable()
    from sync_rcu_exp_select_cpus(), which has the further benefit of reducing
    the number of changes that must be maintained in the -rt patchset.
    
    Reported-by: Thomas Gleixner <tglx@linutronix.de>
    Reported-by: Sebastian Siewior <bigeasy@linutronix.de>
    Suggested-by: Boqun Feng <boqun.feng@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>

--- kernel/rcu/tree_exp.h
+++ kernel/rcu/tree_exp.h
@@ -449,7 +449,6 @@ static void sync_rcu_exp_select_cpus(smp_call_func_t func)
 			continue;
 		}
 		INIT_WORK(&rnp->rew.rew_work, sync_rcu_exp_select_node_cpus);
-		preempt_disable();
 		cpu = find_next_bit(&rnp->ffmask, BITS_PER_LONG, -1);
 		/* If all offline, queue the work on an unbound CPU. */
 		if (unlikely(cpu > rnp->grphi - rnp->grplo))
@@ -457,7 +456,6 @@ static void sync_rcu_exp_select_cpus(smp_call_func_t func)
 		else
 			cpu += rnp->grplo;
 		queue_work_on(cpu, rcu_par_gp_wq, &rnp->rew.rew_work);
-		preempt_enable();
 		rnp->exp_need_flush = true;
 	}
 
