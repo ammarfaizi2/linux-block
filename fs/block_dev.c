817c739877e4af79ffe4d28135dc0581026a42d6
Jens Axboe <axboe@kernel.dk>
2018-11-30 08:47:03 -0700

    block: implement bio helper to add iter bvec pages to bio
    For an ITER_BVEC, we can just iterate the iov and add the pages
    to the bio directly. This requires that the caller doesn't releases
    the pages on IO completion, we add a BIO_HOLD_PAGES flag for that.
    
    The current two callers of bio_iov_iter_get_pages() are updated to
    check if they need to release pages on completion. This makes them
    work with bvecs that contain kernel mapped pages already.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/block_dev.c
+++ fs/block_dev.c
@@ -324,8 +324,9 @@ static void blkdev_bio_end_io(struct bio *bio)
 		struct bio_vec *bvec;
 		int i;
 
-		bio_for_each_segment_all(bvec, bio, i)
-			put_page(bvec->bv_page);
+		if (!bio_flagged(bio, BIO_HOLD_PAGES))
+			bio_for_each_segment_all(bvec, bio, i)
+				put_page(bvec->bv_page);
 		bio_put(bio);
 	}
 }
