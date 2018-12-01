6898553076b763ccc18c0370ac6ed5c011015635
Paul E. McKenney <paulmck@linux.ibm.com>
2018-11-30 17:17:21 -0800

    rcu: Remove unused rcu_cpu_kthread_loops per-CPU variable
    The rcu_cpu_kthread_loops variable used to provide debugfs information,
    but is no longer used.  This commit therefore removes it.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>

--- kernel/rcu/tree.h
+++ kernel/rcu/tree.h
@@ -407,7 +407,6 @@ int rcu_dynticks_snap(struct rcu_data *rdp);
 #ifdef CONFIG_RCU_BOOST
 DECLARE_PER_CPU(unsigned int, rcu_cpu_kthread_status);
 DECLARE_PER_CPU(int, rcu_cpu_kthread_cpu);
-DECLARE_PER_CPU(unsigned int, rcu_cpu_kthread_loops);
 DECLARE_PER_CPU(char, rcu_cpu_has_work);
 #endif /* #ifdef CONFIG_RCU_BOOST */
 
