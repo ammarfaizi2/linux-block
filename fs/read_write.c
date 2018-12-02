81d908e5cd051d14ac553a4cd247695691f6ae51
Andy Lutomirski <luto@kernel.org>
2018-12-02 11:32:15 -0800

    fs: Remove the compat version of sendfile64()
    The native and compat definitions of the sendfile64 syscall have
    identical bodies.  The only differences are that the compat version
    takes a compat_loff_t * parameter and a compat_size_t parameter
    instead of a loff_t * and a size_t.  The distinction between
    loff_t * and compat_loff_t * is irrelevant -- they are the same
    type.
    
    This changes powerpc and parisc to use the non-compat sendfile64
    entry point.  Those were the only architectures that used the compat
    entry point.
    
    Cc: "James E.J. Bottomley" <jejb@parisc-linux.org>
    Cc: Helge Deller <deller@gmx.de>
    Cc: linux-parisc@vger.kernel.org
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.corg>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: linuxppc-dev@lists.ozlabs.org
    Signed-off-by: Andy Lutomirski <luto@kernel.org>

--- fs/read_write.c
+++ fs/read_write.c
@@ -1545,24 +1545,6 @@ COMPAT_SYSCALL_DEFINE4(sendfile, int, out_fd, int, in_fd,
 
 	return do_sendfile(out_fd, in_fd, NULL, count, 0);
 }
-
-COMPAT_SYSCALL_DEFINE4(sendfile64, int, out_fd, int, in_fd,
-		compat_loff_t __user *, offset, compat_size_t, count)
-{
-	loff_t pos;
-	ssize_t ret;
-
-	if (offset) {
-		if (unlikely(copy_from_user(&pos, offset, sizeof(loff_t))))
-			return -EFAULT;
-		ret = do_sendfile(out_fd, in_fd, &pos, count, 0);
-		if (unlikely(put_user(pos, offset)))
-			return -EFAULT;
-		return ret;
-	}
-
-	return do_sendfile(out_fd, in_fd, NULL, count, 0);
-}
 #endif
 
 /**
