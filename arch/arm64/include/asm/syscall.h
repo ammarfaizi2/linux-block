69231c5f741dcc06f41f9675ed6b7d5790a75047
Yury Norov <ynorov@marvell.com>
2019-01-08 01:48:29 +0300

    arm64: introduce AUDIT_ARCH_AARCH64ILP32 for ilp32
    syscall_get_arch() currently makes no difference between arm64 and
    arm64/ilp32. Fix it by adding AUDIT_ARCH_AARCH64ILP32.
    
    Reported-by: Andy Lutomirski <luto@amacapital.net>
    Signed-off-by: Yury Norov <ynorov@marvell.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/syscall.h
+++ arch/arm64/include/asm/syscall.h
@@ -122,6 +122,9 @@ static inline int syscall_get_arch(void)
 	if (is_a32_compat_task())
 		return AUDIT_ARCH_ARM;
 
+	else if (is_ilp32_compat_task())
+		return AUDIT_ARCH_AARCH64ILP32;
+
 	return AUDIT_ARCH_AARCH64;
 }
 
