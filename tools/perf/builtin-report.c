kernelorg-mirror/linux_kernel_git_jolsa_perf

perf report: Add support to display script stack

tools/perf/builtin-report.c
@@ -62,6 +62,7 @@ struct report {
 	bool			header;
 	bool			header_only;
 	bool			nonany_branch_mode;
+	bool			script;
 	int			max_stack;
 	struct perf_read_values	show_threads_values;
 	const char		*pretty_printing_style;
@@ -169,6 +170,61 @@ static int hist_iter__branch_callback(struct hist_entry_iter *iter,
 	return 0;
 }
 
+static int script_resolve(struct machine *machine, struct addr_location *al,
+			  struct perf_sample *sample)
+{
+	struct thread *thread = machine__findnew_thread(machine, sample->pid,
+							sample->tid);
+
+	if (thread == NULL)
+		return -1;
+
+	al->machine  = machine;
+	al->thread   = thread;
+	al->addr     = sample->user_data.ip;
+	al->cpumode  = sample->cpumode;
+	al->filtered = 0;
+	al->level    = '.';
+	al->map      = thread->user_map;
+
+	if (thread__is_filtered(thread))
+		al->filtered |= (1 << HIST_FILTER__THREAD);
+
+	al->sym = NULL;
+	al->cpu = sample->cpu;
+	al->socket = -1;
+
+	if (al->cpu >= 0) {
+		struct perf_env *env = machine->env;
+
+		if (env && env->cpu)
+			al->socket = env->cpu[al->cpu].socket_id;
+	}
+
+	if (al->map) {
+		struct dso *dso = al->map->dso;
+
+		if (symbol_conf.dso_list &&
+		    (!dso || !(strlist__has_entry(symbol_conf.dso_list,
+						  dso->short_name) ||
+			       (dso->short_name != dso->long_name &&
+				strlist__has_entry(symbol_conf.dso_list,
+						   dso->long_name))))) {
+			al->filtered |= (1 << HIST_FILTER__DSO);
+		}
+
+		al->sym = map__find_symbol(al->map, al->addr);
+	}
+
+	if (symbol_conf.sym_list &&
+		(!al->sym || !strlist__has_entry(symbol_conf.sym_list,
+						al->sym->name))) {
+		al->filtered |= (1 << HIST_FILTER__SYMBOL);
+	}
+
+	return 0;
+}
+
 static int process_sample_event(struct perf_tool *tool,
 				union perf_event *event,
 				struct perf_sample *sample,
@@ -188,10 +244,21 @@ static int process_sample_event(struct perf_tool *tool,
 	if (perf_time__skip_sample(&rep->ptime, sample->time))
 		return 0;
 
-	if (machine__resolve(machine, &al, sample) < 0) {
-		pr_debug("problem processing %d event, skipping it.\n",
-			 event->header.type);
-		return -1;
+	if (rep->script) {
+		if (!sample->user_data.ip)
+			return 0;
+
+		if (script_resolve(machine, &al, sample) < 0) {
+			pr_debug("problem processing %d event, skipping it.\n",
+				 event->header.type);
+			return -1;
+		}
+	} else {
+		if (machine__resolve(machine, &al, sample) < 0) {
+			pr_debug("problem processing %d event, skipping it.\n",
+				 event->header.type);
+			return -1;
+		}
 	}
 
 	if (symbol_conf.hide_unresolved && al.sym == NULL)
@@ -870,6 +937,7 @@ int cmd_report(int argc, const char **argv)
 		    "Show raw trace event output (do not use print fmt or plugins)"),
 	OPT_BOOLEAN(0, "hierarchy", &symbol_conf.report_hierarchy,
 		    "Show entries in a hierarchy"),
+	OPT_BOOLEAN(0, "script", &report.script, "script mode"),
 	OPT_CALLBACK_DEFAULT(0, "stdio-color", NULL, "mode",
 			     "'always' (default), 'never' or 'auto' only applicable to --stdio mode",
 			     stdio__config_color, "always"),
@@ -916,6 +984,9 @@ int cmd_report(int argc, const char **argv)
 		pr_err("Invalid file: %s\n", symbol_conf.kallsyms_name);
 		return -EINVAL;
 	}
+
+	if (report.script)
+		symbol_conf.annotation_disabled = true;
 
 	if (report.use_stdio)
 		use_browser = 0;

ammarfaizi2/linux-block

tools/perf/builtin-report.c
@@ -41,6 +41,7 @@
 #include "util/auxtrace.h"
 #include "util/units.h"
 #include "util/branch.h"
+#include "util/script-sample.h"
 
 #include <dlfcn.h>
 #include <errno.h>
@@ -69,6 +70,7 @@ struct report {
 	bool			header_only;
 	bool			nonany_branch_mode;
 	bool			group_set;
+	bool			script;
 	int			max_stack;
 	struct perf_read_values	show_threads_values;
 	struct annotation_options annotation_opts;
@@ -225,6 +227,61 @@ static int process_feature_event(struct perf_session *session,
 	return 0;
 }
 
+static int script_resolve(struct machine *machine, struct addr_location *al,
+			  struct perf_sample *sample)
+{
+	struct thread *thread = machine__findnew_thread(machine, sample->pid,
+							sample->tid);
+
+	if (thread == NULL)
+		return -1;
+
+	al->machine  = machine;
+	al->thread   = thread;
+	al->addr     = sample->script_stack.ip;
+	al->cpumode  = sample->cpumode;
+	al->filtered = 0;
+	al->level    = '.';
+	al->map      = thread->user_map;
+
+	if (thread__is_filtered(thread))
+		al->filtered |= (1 << HIST_FILTER__THREAD);
+
+	al->sym = NULL;
+	al->cpu = sample->cpu;
+	al->socket = -1;
+
+	if (al->cpu >= 0) {
+		struct perf_env *env = machine->env;
+
+		if (env && env->cpu)
+			al->socket = env->cpu[al->cpu].socket_id;
+	}
+
+	if (al->map) {
+		struct dso *dso = al->map->dso;
+
+		if (symbol_conf.dso_list &&
+		    (!dso || !(strlist__has_entry(symbol_conf.dso_list,
+						  dso->short_name) ||
+			       (dso->short_name != dso->long_name &&
+				strlist__has_entry(symbol_conf.dso_list,
+						   dso->long_name))))) {
+			al->filtered |= (1 << HIST_FILTER__DSO);
+		}
+
+		al->sym = map__find_symbol(al->map, al->addr);
+	}
+
+	if (symbol_conf.sym_list &&
+		(!al->sym || !strlist__has_entry(symbol_conf.sym_list,
+						al->sym->name))) {
+		al->filtered |= (1 << HIST_FILTER__SYMBOL);
+	}
+
+	return 0;
+}
+
 static int process_sample_event(struct perf_tool *tool,
 				union perf_event *event,
 				struct perf_sample *sample,
@@ -246,10 +303,18 @@ static int process_sample_event(struct perf_tool *tool,
 		return 0;
 	}
 
-	if (machine__resolve(machine, &al, sample) < 0) {
-		pr_debug("problem processing %d event, skipping it.\n",
-			 event->header.type);
-		return -1;
+	if (rep->script) {
+		if (script_resolve(machine, &al, sample) < 0) {
+			pr_debug("problem processing %d event, skipping it.\n",
+				 event->header.type);
+			return -1;
+		}
+	} else {
+		if (machine__resolve(machine, &al, sample) < 0) {
+			pr_debug("problem processing %d event, skipping it.\n",
+				 event->header.type);
+			return -1;
+		}
 	}
 
 	if (symbol_conf.hide_unresolved && al.sym == NULL)
@@ -1118,6 +1183,7 @@ int cmd_report(int argc, const char **argv)
 		    "Show raw trace event output (do not use print fmt or plugins)"),
 	OPT_BOOLEAN(0, "hierarchy", &symbol_conf.report_hierarchy,
 		    "Show entries in a hierarchy"),
+	OPT_BOOLEAN(0, "script", &report.script, "script mode"),
 	OPT_CALLBACK_DEFAULT(0, "stdio-color", NULL, "mode",
 			     "'always' (default), 'never' or 'auto' only applicable to --stdio mode",
 			     stdio__config_color, "always"),
@@ -1171,6 +1237,9 @@ int cmd_report(int argc, const char **argv)
 		pr_err("Invalid file: %s\n", symbol_conf.kallsyms_name);
 		return -EINVAL;
 	}
+
+	if (report.script)
+		symbol__script_init();
 
 	if (report.inverted_callchain)
 		callchain_param.order = ORDER_CALLER;