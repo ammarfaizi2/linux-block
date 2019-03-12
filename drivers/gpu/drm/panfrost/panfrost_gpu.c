5b8f6ce2ab7d7b12517795425a08d0917cfffcb8
Rob Herring <robh@kernel.org>
2019-03-12 15:28:14 -0500

    drm/panfrost: Power off on driver shutdown
    Signed-off-by: Rob Herring <robh@kernel.org>

--- drivers/gpu/drm/panfrost/panfrost_gpu.c
+++ drivers/gpu/drm/panfrost/panfrost_gpu.c
@@ -452,6 +452,14 @@ static void panfrost_gpu_power_on(struct panfrost_device *pfdev)
 		dev_err(pfdev->dev, "error powering up gpu");
 }
 
+static void panfrost_gpu_power_off(struct panfrost_device *pfdev)
+{
+	gpu_write(pfdev, SHADER_PWROFF_LO, 0);
+	gpu_write(pfdev, TILER_PWROFF_LO, 0);
+	gpu_write(pfdev, L2_PWROFF_LO, 0);
+	gpu_write(pfdev, STACK_PWROFF_LO, 0);
+}
+
 int panfrost_gpu_init(struct panfrost_device *pfdev)
 {
 	int err, irq;
@@ -481,7 +489,7 @@ int panfrost_gpu_init(struct panfrost_device *pfdev)
 
 void panfrost_gpu_fini(struct panfrost_device *pfdev)
 {
-
+	panfrost_gpu_power_off(pfdev);
 }
 
 u32 panfrost_gpu_get_latest_flush_id(struct panfrost_device *pfdev)
