17bfe41bb7edac92640063e291fd068ee04030f2
Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
2019-06-26 09:37:34 +1000

    mm/mmap: move common defines to mman-common.h
    Two architecture that use arch specific MMAP flags are powerpc and sparc.
    We still have few flag values common across them and other architectures.
    Consolidate this in mman-common.h.
    
    Also update the comment to indicate where to find HugeTLB specific
    reserved values
    
    Link: http://lkml.kernel.org/r/20190604090950.31417-1-aneesh.kumar@linux.ibm.com
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- include/uapi/asm-generic/mman-common.h
+++ include/uapi/asm-generic/mman-common.h
@@ -25,7 +25,11 @@
 # define MAP_UNINITIALIZED 0x0		/* Don't support this flag */
 #endif
 
-/* 0x0100 - 0x40000 flags are defined in asm-generic/mman.h */
+/* 0x0100 - 0x4000 flags are defined in asm-generic/mman.h */
+#define MAP_POPULATE		0x008000	/* populate (prefault) pagetables */
+#define MAP_NONBLOCK		0x010000	/* do not block on IO */
+#define MAP_STACK		0x020000	/* give out an address that is best suited for process/thread stacks */
+#define MAP_HUGETLB		0x040000	/* create a huge page mapping */
 #define MAP_SYNC		0x080000 /* perform synchronous page faults for the mapping */
 #define MAP_FIXED_NOREPLACE	0x100000	/* MAP_FIXED which doesn't unmap underlying mapping */
 
