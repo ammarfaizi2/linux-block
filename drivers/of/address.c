bd58bab3c45d7d76bbdf3ad7b395bad161da41da
Robin Murphy <robin.murphy@arm.com>
2019-07-03 18:42:20 +0100

    of: Make of_dma_get_range() work on bus nodes
    Since the "dma-ranges" property is only valid for a node representing a
    bus, of_dma_get_range() currently assumes the node passed in is a leaf
    representing a device, and starts the walk from its parent. In cases
    like PCI host controllers on typical FDT systems, however, where the PCI
    endpoints are probed dynamically the initial leaf node represents the
    'bus' itself, and this logic means we fail to consider any "dma-ranges"
    describing the host bridge itself. Rework the logic such that
    of_dma_get_range() works correctly starting from a bus node containing
    "dma-ranges".
    
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    [robh: Allow for the bus child node to still be passed in]
    Signed-off-by: Rob Herring <robh@kernel.org>

--- drivers/of/address.c
+++ drivers/of/address.c
@@ -922,18 +922,9 @@ int of_dma_get_range(struct device_node *np, u64 *dma_addr, u64 *paddr, u64 *siz
 	if (!node)
 		return -EINVAL;
 
-	while (1) {
-		struct device_node *parent;
-
-		naddr = of_n_addr_cells(node);
-		nsize = of_n_size_cells(node);
-
-		parent = __of_get_dma_parent(node);
-		of_node_put(node);
-
-		node = parent;
-		if (!node)
-			break;
+	while (node) {
+		naddr = of_bus_n_addr_cells(node);
+		nsize = of_bus_n_size_cells(node);
 
 		ranges = of_get_property(node, "dma-ranges", &len);
 
@@ -941,12 +932,7 @@ int of_dma_get_range(struct device_node *np, u64 *dma_addr, u64 *paddr, u64 *siz
 		if (ranges && len > 0)
 			break;
 
-		/*
-		 * At least empty ranges has to be defined for parent node if
-		 * DMA is supported
-		 */
-		if (!ranges)
-			break;
+		node = of_get_next_dma_parent(node);
 	}
 
 	if (!ranges) {
@@ -965,7 +951,7 @@ int of_dma_get_range(struct device_node *np, u64 *dma_addr, u64 *paddr, u64 *siz
 	 * size		: nsize cells
 	 */
 	dmaaddr = of_read_number(ranges, naddr);
-	*paddr = of_translate_dma_address(np, ranges);
+	*paddr = of_translate_dma_address(node, ranges + naddr);
 	if (*paddr == OF_BAD_ADDR) {
 		pr_err("translation of DMA address(%llx) to CPU address failed node(%pOF)\n",
 		       dmaaddr, np);
