50ca7786c26fba9db211ed89843e0da47b0cfbc0
Kalle Valo <kvalo@codeaurora.org>
2019-07-24 15:52:33 +0300

    Revert "ath11k: add HE support"
    As upstream mac80211 does not support HE yet remove the out-of-tree mac80211 HE
    patches and corresponding ath11k patches from ath11k-bringup branch.
    
    This reverts commit 17aca2d9a969788a7f1e3e0c72b5485bf6a432a4.

--- drivers/net/wireless/ath/ath11k/core.h
+++ drivers/net/wireless/ath/ath11k/core.h
@@ -431,8 +431,6 @@ struct ath11k {
 
 	struct {
 		struct ieee80211_supported_band sbands[NUM_NL80211_BANDS];
-		struct ieee80211_sband_iftype_data
-			iftype[NUM_NL80211_BANDS][NUM_NL80211_IFTYPES];
 	} mac;
 	unsigned long dev_flags;
 	unsigned int filter_flags;
