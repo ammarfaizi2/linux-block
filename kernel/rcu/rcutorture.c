5318379138b7bd6c45f325a4c268c9c89037dcbb
Paul E. McKenney <paulmck@linux.ibm.com>
2019-08-14 15:42:05 -0700

    fixup! rcutorture: Emulate dyntick aspect of userspace nohz_full sojourn
    Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>

--- kernel/rcu/rcutorture.c
+++ kernel/rcu/rcutorture.c
@@ -1766,8 +1766,11 @@ static unsigned long rcu_torture_fwd_prog_cbfree(void)
 		kfree(rfcp);
 		freed++;
 		rcu_torture_fwd_prog_cond_resched(freed);
-		if (tick_nohz_full_enabled())
+		if (tick_nohz_full_enabled()) {
+			local_irq_save(flags);
 			rcu_momentary_dyntick_idle();
+			local_irq_restore(flags);
+		}
 	}
 	return freed;
 }
@@ -1842,6 +1845,7 @@ static void rcu_torture_fwd_prog_nr(int *tested, int *tested_tries)
 static void rcu_torture_fwd_prog_cr(void)
 {
 	unsigned long cver;
+	unsigned long flags;
 	unsigned long gps;
 	int i;
 	long n_launders;
@@ -1902,8 +1906,11 @@ static void rcu_torture_fwd_prog_cr(void)
 		}
 		cur_ops->call(&rfcp->rh, rcu_torture_fwd_cb_cr);
 		rcu_torture_fwd_prog_cond_resched(n_launders + n_max_cbs);
-		if (tick_nohz_full_enabled())
+		if (tick_nohz_full_enabled()) {
+			local_irq_save(flags);
 			rcu_momentary_dyntick_idle();
+			local_irq_restore(flags);
+		}
 	}
 	stoppedat = jiffies;
 	n_launders_cb_snap = READ_ONCE(n_launders_cb);
