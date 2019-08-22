60dfe9b835273ea019f44d95ed012aa94c03555c
Rob Herring <robh@kernel.org>
2019-08-22 15:42:14 +0100

    perf: arm64: Add test for userspace counter access on heterogeneous systems
    Userspace counter access only works on heterogeneous systems with some
    restrictions. The userspace process must be pinned to a homogeneous
    subset of CPUs and must open the corresponding PMU for those CPUs. This
    commit adds a test implementing these requirements.
    
    Signed-off-by: Rob Herring <robh@kernel.org>
    ---
    v4:
    - Update perf_evsel__mmap params
    v2:
    - Drop all but heterogeneous test as others covered by libperf tests
    - Rework to use libperf

--- tools/perf/arch/arm64/include/arch-tests.h
+++ tools/perf/arch/arm64/include/arch-tests.h
@@ -2,11 +2,18 @@
 #ifndef ARCH_TESTS_H
 #define ARCH_TESTS_H
 
+#include <linux/compiler.h>
+
 #ifdef HAVE_DWARF_UNWIND_SUPPORT
 struct thread;
 struct perf_sample;
+int test__arch_unwind_sample(struct perf_sample *sample,
+			     struct thread *thread);
 #endif
 
 extern struct test arch_tests[];
+int test__rd_pinned(struct test __maybe_unused *test,
+		       int __maybe_unused subtest);
+
 
 #endif
