89538f9f8dda5e7738d71f179f9321740b680734
Paul E. McKenney <paulmck@kernel.org>
2019-09-19 16:44:45 -0700

    rcu: Add crude self-test for early boot kfree_rcu()
    This commit adds a crude self-test for early-boot kfree_rcu().  This does
    not check that the memory is actually eventually freed, but simply
    exercises the kfree_rcu() code during early boot.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/update.c
+++ kernel/rcu/update.c
@@ -853,14 +853,22 @@ static void test_callback(struct rcu_head *r)
 
 DEFINE_STATIC_SRCU(early_srcu);
 
+struct early_boot_kfree_rcu {
+	struct rcu_head *rh;
+};
+
 static void early_boot_test_call_rcu(void)
 {
 	static struct rcu_head head;
 	static struct rcu_head shead;
+	struct early_boot_kfree_rcu *rhp;
 
 	call_rcu(&head, test_callback);
 	if (IS_ENABLED(CONFIG_SRCU))
 		call_srcu(&early_srcu, &shead, test_callback);
+	rhp = kmalloc(sizeof(*rhp), GFP_KERNEL);
+	if (!WARN_ON_ONCE(!rhp))
+		kfree_rcu(rhp, rh);
 }
 
 void rcu_early_boot_tests(void)
