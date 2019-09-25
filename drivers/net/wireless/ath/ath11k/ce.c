3dcb3e76a2601ea7c662585b6450d035e16cb9a9
Vasanthakumar Thiagarajan <vthiagar@codeaurora.org>
2019-09-25 09:06:11 +0300

    ath11k: Use struct_size() to get the structure size with trailing array
    The size calculation of struct ath11k_ce_ring with trailing skb array
    got simplified with struct_size() kernel macro.
    
    Signed-off-by: Vasanthakumar Thiagarajan <vthiagar@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/ath11k/ce.c
+++ drivers/net/wireless/ath/ath11k/ce.c
@@ -406,8 +406,7 @@ ath11k_ce_alloc_ring(struct ath11k_base *ab, int nentries, int desc_sz)
 	struct ath11k_ce_ring *ce_ring;
 	dma_addr_t base_addr;
 
-	ce_ring = kzalloc(sizeof(*ce_ring) + (nentries * sizeof(*ce_ring->skb)),
-			  GFP_KERNEL);
+	ce_ring = kzalloc(struct_size(ce_ring, skb, nentries), GFP_KERNEL);
 	if (ce_ring == NULL)
 		return ERR_PTR(-ENOMEM);
 
