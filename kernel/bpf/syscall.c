75d015882d54c66a6e145954e7fafc9cc1fcc6fd
Christian Brauner <christian.brauner@ubuntu.com>
2019-10-09 17:30:56 +0200

    bpf: use copy_struct_from_user() in bpf_prog_get_info_by_fd()
    In v5.4-rc2 we added a new helper (cf. [1]) copy_struct_from_user().
    This helper is intended for all codepaths that copy structs from
    userspace that are versioned by size. bpf_prog_get_info_by_fd() does
    exactly what copy_struct_from_user() is doing.
    Note that copy_struct_from_user() is calling min() already. So
    technically, the min_t() call could go. But the info_len is used further
    below so leave it.
    
    [1]: f5a1a536fa14 ("lib: introduce copy_struct_from_user() helper")
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -2312,13 +2312,10 @@ static int bpf_prog_get_info_by_fd(struct bpf_prog *prog,
 	u32 ulen;
 	int err;
 
-	err = bpf_check_uarg_tail_zero(uinfo, sizeof(info), info_len);
+	info_len = min_t(u32, sizeof(info), info_len);
+	err = copy_struct_from_user(&info, sizeof(info), uinfo, info_len);
 	if (err)
 		return err;
-	info_len = min_t(u32, sizeof(info), info_len);
-
-	if (copy_from_user(&info, uinfo, info_len))
-		return -EFAULT;
 
 	info.type = prog->type;
 	info.id = prog->aux->id;
