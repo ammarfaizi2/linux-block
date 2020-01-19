898c97d247f721d874119358467f7ec1a2dfa168
Christian Brauner <christian.brauner@ubuntu.com>
2020-01-19 21:03:01 +0100

    pidfd: introduce caps
    Almost since the inception of pidfds people have been pushing for
    capability like behavior built on top of them. So far this didn't make
    sense since we had no new features built on top of pidfd that would
    enable us to introduce capability-like behavior on top of them.
    But for a while now people have been sending patches that want to make
    use of pidfds. And recently we took the pidfd_getfd() syscall and are
    ready to merge it soon. This is a good time to introduce pidfd
    capabilities alongside it right away. Thus, the first capability is to
    be able to retrieve file descriptors from a task identified via pidfd.
    By setting the PIDFD_CAP_GETFD flag the creator of a pidfd (through
    clone or pidfd_open) can bequest its credentials to the pidfd. The
    credentials will be stashed in struct pidfd_struct and will be used in
    place of the callers creds in the ptrace_may_access() permission check
    that decides whether the caller can retrieve a pidfd from the target
    process identified by the pidfd. A privileged process can thus delegate
    its PIDFD_CAP_GETFD ability to lesser privileged processes.
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- include/linux/pidfd.h
+++ include/linux/pidfd.h
@@ -10,6 +10,8 @@ struct pid;
 
 struct pidfd_struct {
 	struct pid *pid;
+	const struct cred *creator_cred;
+	u32 flags;
 };
 
 static inline struct pid *pidfd_pid(const struct pidfd_struct *pidfd)
@@ -22,6 +24,13 @@ static inline struct pid *pidfd_get_pid(struct pidfd_struct *pidfd)
 	return get_pid(pidfd_pid(pidfd));
 }
 extern struct pidfd_struct *pidfd_file(const struct file *file);
+
+static inline bool pidfd_has_flag(const struct pidfd_struct *pidfd, u32 flags)
+{
+	return (pidfd->flags & flags) == flags;
+}
+
+int pidfd_flags_allowed(u32 flags, const struct task_struct *child);
 
 struct pidfd_struct *pidfd_alloc(struct pid *pid);
 void pidfd_put(struct pidfd_struct *pidfd);
--- include/uapi/linux/sched.h
+++ include/uapi/linux/sched.h
@@ -42,6 +42,9 @@
  */
 #define CLONE_NEWTIME	0x00000080	/* New time namespace */
 
+/* pidfd can be used to retrieve file descriptors from another task */
+#define PIDFD_CAP_GETFD (1UL << 0)
+
 #ifndef __ASSEMBLY__
 /**
  * struct clone_args - arguments for the clone3 syscall
@@ -81,6 +84,8 @@
  * @set_tid_size: This defines the size of the array referenced
  *                in @set_tid. This cannot be larger than the
  *                kernel's limit of nested PID namespaces.
+ * @pidfd_flags:  Set of operations which are possible with this
+ *                pidfd.
  *
  * The structure is versioned by size and thus extensible.
  * New struct members must go at the end of the struct and
@@ -97,6 +102,7 @@ struct clone_args {
 	__aligned_u64 tls;
 	__aligned_u64 set_tid;
 	__aligned_u64 set_tid_size;
+	__aligned_u64 pidfd_flags;
 };
 #endif
 
