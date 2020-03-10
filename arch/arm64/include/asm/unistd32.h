2ad17cd3b4ed55d16896a071b0f5a09795bc8748
Christian Brauner <christian.brauner@ubuntu.com>
2020-03-10 10:21:38 +0100

    arch: wire up fsinfo syscall
    This wires up the fsinfo() syscall for all architectures.
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -883,6 +883,8 @@ __SYSCALL(__NR_clone3, sys_clone3)
 __SYSCALL(__NR_openat2, sys_openat2)
 #define __NR_pidfd_getfd 438
 __SYSCALL(__NR_pidfd_getfd, sys_pidfd_getfd)
+#define __NR_fsinfo 441
+__SYSCALL(__NR_fsinfo, sys_fsinfo)
 
 /*
  * Please add new compat syscalls above this comment and update
