ac51ac8266fbf719c898539bb77a7ac8db762f89
Paul E. McKenney <paulmck@kernel.org>
2020-03-20 14:29:08 -0700

    rcu-tasks: Allow rcu_read_unlock_trace() under scheduler locks
    The rcu_read_unlock_trace() can invoke rcu_read_unlock_trace_special(),
    which in turn can call wake_up().  Therefore, if any scheduler lock is
    held across a call to rcu_read_unlock_trace(), self-deadlock can occur.
    This commit therefore uses the irq_work facility to defer the wake_up()
    to a clean environment where no scheduler locks will be held.
    
    Reported-by: Steven Rostedt <rostedt@goodmis.org>
    [ paulmck: Update #includes for m68k per kbuild test robot. ]
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/update.c
+++ kernel/rcu/update.c
@@ -40,6 +40,8 @@
 #include <linux/rcupdate_wait.h>
 #include <linux/sched/isolation.h>
 #include <linux/kprobes.h>
+#include <linux/slab.h>
+#include <linux/irq_work.h>
 
 #define CREATE_TRACE_POINTS
 
