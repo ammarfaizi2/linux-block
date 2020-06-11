0b625a48d15b2d12789e4dba166dcce3ac86ab0f
Rob Herring <robh@kernel.org>
2020-06-11 15:14:02 -0600

    PCI: Set default bridge parent device
    The host bridge's parent device is always the platform device. As we
    already have a pointer to it in the devres functions, let's initialize
    the parent device. Drivers can still override the parent if desired.
    
    Cc: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- drivers/pci/controller/pcie-brcmstb.c
+++ drivers/pci/controller/pcie-brcmstb.c
@@ -996,7 +996,6 @@ static int brcm_pcie_probe(struct platform_device *pdev)
 		}
 	}
 
-	bridge->dev.parent = &pdev->dev;
 	bridge->busnr = 0;
 	bridge->ops = &brcm_pcie_ops;
 	bridge->sysdata = pcie;
