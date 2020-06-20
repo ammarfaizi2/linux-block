d1a7265148e2b9cb70d30586998601ac1fb5c703
Michael Ellerman <mpe@ellerman.id.au>
2020-06-20 22:28:00 +1000

    Revert "powerpc/syscalls: Split SPU-ness out of ABI"
    This reverts commit 35e32a6cb5f694fda54a5f391917e4ceefa0fece.

--- arch/powerpc/kernel/syscalls/syscalltbl.sh
+++ arch/powerpc/kernel/syscalls/syscalltbl.sh
@@ -6,7 +6,6 @@ out="$2"
 my_abis=`echo "($3)" | tr ',' '|'`
 my_abi="$4"
 offset="$5"
-spu_table="$6"
 
 emit() {
 	t_nxt="$1"
@@ -29,16 +28,9 @@ grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
 	while read nr abi name entry compat ; do
 		if [ "$my_abi" = "c32" ] && [ ! -z "$compat" ]; then
 			emit $((nxt+offset)) $((nr+offset)) $compat
-			nxt=$((nr+1))
-		elif [ "$my_abi" = "spu" ]; then
-			grep -E "^$nr[[:space:]]+$name[[:space:]]+spu[[:space:]]*$" "$spu_table" > /dev/null
-			if [ $? -eq 0 ]; then
-				emit $((nxt+offset)) $((nr+offset)) $entry
-				nxt=$((nr+1))
-			fi
 		else
 			emit $((nxt+offset)) $((nr+offset)) $entry
-			nxt=$((nr+1))
 		fi
+		nxt=$((nr+1))
 	done
 ) > "$out"
