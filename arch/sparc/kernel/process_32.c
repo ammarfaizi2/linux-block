d955712fbe93c9c4ce1a8515f2fe763b70902cd8
Al Viro <viro@zeniv.linux.org.uk>
2020-08-20 10:55:25 -0400

    sparc32: take ->thread.flags out
    it was used for two things - dealing with unusual ->kregs for kernel
    threads and "emulate unaligned userland accesses for this thread";
    the former is killed off by the previous commit, the latter never
    had been set in the first place.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/sparc/kernel/process_32.c
+++ arch/sparc/kernel/process_32.c
@@ -217,9 +217,6 @@ void flush_thread(void)
 		clear_thread_flag(TIF_USEDFPU);
 #endif
 	}
-
-	/* This task is no longer a kernel thread. */
-	current->thread.flags &= ~SPARC_FLAG_KTHREAD;
 }
 
 static inline struct sparc_stackf __user *
@@ -334,7 +331,6 @@ int copy_thread(unsigned long clone_flags, unsigned long sp,
 		extern int nwindows;
 		unsigned long psr;
 		memset(new_stack, 0, STACKFRAME_SZ + TRACEREG_SZ);
-		p->thread.flags |= SPARC_FLAG_KTHREAD;
 		p->thread.current_ds = KERNEL_DS;
 		ti->kpc = (((unsigned long) ret_from_kernel_thread) - 0x8);
 		childregs->u_regs[UREG_G1] = sp; /* function */
@@ -346,7 +342,6 @@ int copy_thread(unsigned long clone_flags, unsigned long sp,
 	}
 	memcpy(new_stack, (char *)regs - STACKFRAME_SZ, STACKFRAME_SZ + TRACEREG_SZ);
 	childregs->u_regs[UREG_FP] = sp;
-	p->thread.flags &= ~SPARC_FLAG_KTHREAD;
 	p->thread.current_ds = USER_DS;
 	ti->kpc = (((unsigned long) ret_from_fork) - 0x8);
 	ti->kpsr = current->thread.fork_kpsr | PSR_PIL;
