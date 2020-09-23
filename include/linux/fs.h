fa010a3ae3bdad2acb47ab08d2addfbdc00ddfa9
Christian Brauner <christian.brauner@ubuntu.com>
2020-09-23 16:59:03 +0200

    inode: make init and permission helpers idmapped mount aware
    The inode_owner_or_capable() helper determines whether the caller is the
    owner of the inode or is capable with respect to that inode. Allow it to
    handle idmapped mounts. If the inode is accessed through an idmapped
    mount we first need to map it according to the mount's user namespace.
    Afterwards the checks are identical to non-idmapped mounts. If the
    initial user namespace is passed nothing changes so non-idmapped mounts
    will see identical behavior as before.
    
    Similarly, we allow the inode_init_owner() helper to handle idmapped
    mounts. It initializes a new inode on idmapped mounts by mapping the
    fsuid and fsgid of the caller from the mount's user namespace. If the
    initial user namespace is passed nothing changes so non-idmapped mounts
    will see identical behavior as before.
    
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: David Howells <dhowells@redhat.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: linux-fsdevel@vger.kernel.org
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>
    ---
    /* v2 */
    - Christoph Hellwig <hch@lst.de>:
      - Don't pollute the vfs with additional helpers simply extend the existing
        helpers with an additional argument and switch all callers.
    
    /* v3 */
    unchanged
    
    /* v4 */
    - Christoph Hellwig <hch@lst.de>:
      - Change commit message to reflect the fact that no new permission helpers are
        introduced but only the existing ones changed.
    
    - Serge Hallyn <serge@hallyn.com>:
      - Use "mnt_userns" to refer to a vfsmount's userns everywhere to make
        terminology consistent.
      - Note that inode_owner_or_capable() contains a comparison of the
        caller's fsuid to the inode's i_uid to determine whether the caller
        is the owner of the inode. Serge asked whether it is possible that
        the caller's fsuid is INVALID_UID. If so and i_uid_into_mnt() makes
        the inode's i_uid INVALID_UID too then the caller could potentially
        gain privilege over an inode it doesn't own. But this is not
        feasible since the callers fsuid should never be INVALID_UID unless
        we have a severe bug how we manage set*id() calls or cred
        management. Also, if it is possible that the caller's fsuid were to
        be INVALID_UID we might already have an issue in case a filesystem
        was mounted with an s_user_ns that doesn't contain a mapping for a
        inode that was created when the filesystem was mounted with a
        different user namespace.

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1763,8 +1763,8 @@ static inline bool sb_start_intwrite_trylock(struct super_block *sb)
 	return __sb_start_write_trylock(sb, SB_FREEZE_FS);
 }
 
-
-extern bool inode_owner_or_capable(const struct inode *inode);
+extern bool inode_owner_or_capable(struct user_namespace *mnt_userns,
+				   const struct inode *inode);
 
 /*
  * VFS helper functions..
@@ -1806,8 +1806,9 @@ extern long compat_ptr_ioctl(struct file *file, unsigned int cmd,
 /*
  * VFS file helper functions.
  */
-extern void inode_init_owner(struct inode *inode, const struct inode *dir,
-			umode_t mode);
+extern void inode_init_owner(struct user_namespace *mnt_userns,
+			     struct inode *inode, const struct inode *dir,
+			     umode_t mode);
 extern bool may_open_dev(const struct path *path);
 
 /*
