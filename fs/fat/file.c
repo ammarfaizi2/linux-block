c33e49da61d9c5ec875fb63c7ebc424ea01dd417
Christian Brauner <christian.brauner@ubuntu.com>
2020-10-28 04:22:28 +0100

    fat: handle idmapped mounts
    Let fat handle idmapped mounts. This allows to have the same fat mount appear
    in multiple locations with different id mappings. This allows to expose a vfat
    formatted USB stick to multiple user with different ids on the host or in user
    namespaces:
    
    mount -o uid=1000,gid=1000 /dev/sdb /mnt
    
    u1001@f2-vm:/lower1$ ls -ln /mnt/
    total 4
    -rwxr-xr-x 1 1000 1000 4 Oct 28 03:44 aaa
    -rwxr-xr-x 1 1000 1000 0 Oct 28 01:09 bbb
    -rwxr-xr-x 1 1000 1000 0 Oct 28 01:10 ccc
    -rwxr-xr-x 1 1000 1000 0 Oct 28 03:46 ddd
    -rwxr-xr-x 1 1000 1000 0 Oct 28 04:01 eee
    
    mount2 --idmap both:1000:1001:1
    
    u1001@f2-vm:/lower1$ ls -ln /lower1/
    total 4
    -rwxr-xr-x 1 1001 1001 4 Oct 28 03:44 aaa
    -rwxr-xr-x 1 1001 1001 0 Oct 28 01:09 bbb
    -rwxr-xr-x 1 1001 1001 0 Oct 28 01:10 ccc
    -rwxr-xr-x 1 1001 1001 0 Oct 28 03:46 ddd
    -rwxr-xr-x 1 1001 1001 0 Oct 28 04:01 eee
    
    u1001@f2-vm:/lower1$ touch /lower1/fff
    
    u1001@f2-vm:/lower1$ ls -ln /lower1/fff
    -rwxr-xr-x 1 1001 1001 0 Oct 28 04:03 /lower1/fff
    
    u1001@f2-vm:/lower1$ ls -ln /mnt/fff
    -rwxr-xr-x 1 1000 1000 0 Oct 28 04:03 /mnt/fff
    
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: David Howells <dhowells@redhat.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: linux-fsdevel@vger.kernel.org
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>
    ---
    /* v2 */
    unchanged

--- fs/fat/file.c
+++ fs/fat/file.c
@@ -398,7 +398,7 @@ int fat_getattr(const struct path *path, struct kstat *stat,
 		u32 request_mask, unsigned int flags)
 {
 	struct inode *inode = d_inode(path->dentry);
-	generic_fillattr(&init_user_ns, inode, stat);
+	generic_fillattr(mnt_user_ns(path->mnt), inode, stat);
 	stat->blksize = MSDOS_SB(inode->i_sb)->cluster_size;
 
 	if (MSDOS_SB(inode->i_sb)->options.nfs == FAT_NFS_NOSTALE_RO) {
@@ -447,12 +447,13 @@ static int fat_sanitize_mode(const struct msdos_sb_info *sbi,
 	return 0;
 }
 
-static int fat_allow_set_time(struct msdos_sb_info *sbi, struct inode *inode)
+static int fat_allow_set_time(struct user_namespace *user_ns,
+			      struct msdos_sb_info *sbi, struct inode *inode)
 {
 	umode_t allow_utime = sbi->options.allow_utime;
 
-	if (!uid_eq(current_fsuid(), inode->i_uid)) {
-		if (in_group_p(inode->i_gid))
+	if (!uid_eq(current_fsuid(), i_uid_into_mnt(user_ns, inode))) {
+		if (in_group_p(i_gid_into_mnt(user_ns, inode)))
 			allow_utime >>= 3;
 		if (allow_utime & MAY_WRITE)
 			return 1;
@@ -477,11 +478,11 @@ int fat_setattr(struct user_namespace *user_ns, struct dentry *dentry,
 	/* Check for setting the inode time. */
 	ia_valid = attr->ia_valid;
 	if (ia_valid & TIMES_SET_FLAGS) {
-		if (fat_allow_set_time(sbi, inode))
+		if (fat_allow_set_time(user_ns, sbi, inode))
 			attr->ia_valid &= ~TIMES_SET_FLAGS;
 	}
 
-	error = setattr_prepare(&init_user_ns, dentry, attr);
+	error = setattr_prepare(user_ns, dentry, attr);
 	attr->ia_valid = ia_valid;
 	if (error) {
 		if (sbi->options.quiet)
@@ -551,7 +552,7 @@ int fat_setattr(struct user_namespace *user_ns, struct dentry *dentry,
 		fat_truncate_time(inode, &attr->ia_mtime, S_MTIME);
 	attr->ia_valid &= ~(ATTR_ATIME|ATTR_CTIME|ATTR_MTIME);
 
-	setattr_copy(&init_user_ns, inode, attr);
+	setattr_copy(user_ns, inode, attr);
 	mark_inode_dirty(inode);
 out:
 	return error;
