87e30a4a2455992e95615962a2c35c79e363e8ae
Paul E. McKenney <paulmck@kernel.org>
2020-11-23 12:07:50 -0800

    squash! srcu: Make Tiny SRCU use multi-bit grace-period counter
    [ paulmck: Fix ->srcu_lock_nesting[] indexing per Neeraj Upadhyay. ]
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- include/linux/srcutiny.h
+++ include/linux/srcutiny.h
@@ -81,7 +81,7 @@ static inline void srcu_torture_stats_print(struct srcu_struct *ssp,
 {
 	int idx;
 
-	idx = READ_ONCE(ssp->srcu_idx) & 0x1;
+	idx = ((READ_ONCE(ssp->srcu_idx) + 1) & 0x2) >> 1;
 	pr_alert("%s%s Tiny SRCU per-CPU(idx=%d): (%hd,%hd)\n",
 		 tt, tf, idx,
 		 READ_ONCE(ssp->srcu_lock_nesting[!idx]),
