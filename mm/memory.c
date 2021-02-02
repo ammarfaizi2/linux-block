1597f3feb00b013e634d0765559d7310b09d9d4f
Liam R. Howlett <Liam.Howlett@Oracle.com>
2021-02-02 09:11:59 -0500

    mm/memory: Whitespace fix
    Signed-off-by: Liam R. Howlett <Liam.Howlett@Oracle.com>

--- mm/memory.c
+++ mm/memory.c
@@ -1599,7 +1599,7 @@ void zap_vma_ptes(struct vm_area_struct *vma, unsigned long address,
 		unsigned long size)
 {
 	if (address < vma->vm_start || address + size > vma->vm_end ||
-	    		!(vma->vm_flags & VM_PFNMAP))
+			!(vma->vm_flags & VM_PFNMAP))
 		return;
 
 	zap_page_range_single(vma, address, size, NULL);
