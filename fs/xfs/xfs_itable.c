b9daa0ea3bf7c566aa9749818e3e9a52f6040c18
Christoph Hellwig <hch@lst.de>
2021-03-14 10:59:39 -0700

    xfs: also reject BULKSTAT_SINGLE in a mount user namespace
    BULKSTAT_SINGLE exposed the ondisk uids/gids just like bulkstat, and can
    be called on any inode, including ones not visible in the current mount.
    
    Fixes: f736d93d76d3 ("xfs: support idmapped mounts")
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_itable.c
+++ fs/xfs/xfs_itable.c
@@ -170,6 +170,12 @@ xfs_bulkstat_one(
 	struct xfs_trans	*tp;
 	int			error;
 
+	if (breq->mnt_userns != &init_user_ns) {
+		xfs_warn_ratelimited(breq->mp,
+			"bulkstat not supported inside of idmapped mounts.");
+		return -EINVAL;
+	}
+
 	ASSERT(breq->icount == 1);
 
 	bc.buf = kmem_zalloc(sizeof(struct xfs_bulkstat),
