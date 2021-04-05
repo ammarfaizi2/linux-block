1cfc00bc39b0140e30e3fe3f297cf07986bd4327
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2021-04-05 15:59:54 +0200

    kbuild: scripts/install.sh: properly quote all variables
    A few variables are quoted to handle spaces in directory names, but not
    all of them.  Properly quote everything so that the kernel build can
    handle working correctly with directory names with spaces.
    
    This change makes the script "shellcheck" clean now.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- scripts/install.sh
+++ scripts/install.sh
@@ -33,21 +33,21 @@ verify "$3"
 
 # User may have a custom install script
 
-if [ -x ~/bin/${INSTALLKERNEL} ]; then exec ~/bin/${INSTALLKERNEL} "$@"; fi
-if [ -x /sbin/${INSTALLKERNEL} ]; then exec /sbin/${INSTALLKERNEL} "$@"; fi
+if [ -x ~/bin/"${INSTALLKERNEL}" ]; then exec ~/bin/"${INSTALLKERNEL}" "$@"; fi
+if [ -x /sbin/"${INSTALLKERNEL}" ]; then exec /sbin/"${INSTALLKERNEL}" "$@"; fi
 
 # Default install - same as make zlilo
 
-if [ -f $4/vmlinuz ]; then
-	mv $4/vmlinuz $4/vmlinuz.old
+if [ -f "$4"/vmlinuz ]; then
+	mv "$4"/vmlinuz "$4"/vmlinuz.old
 fi
 
-if [ -f $4/System.map ]; then
-	mv $4/System.map $4/System.old
+if [ -f "$4"/System.map ]; then
+	mv "$4"/System.map "$4"/System.old
 fi
 
-cat $2 > $4/vmlinuz
-cp $3 $4/System.map
+cat "$2" > "$4"/vmlinuz
+cp "$3" "$4"/System.map
 
 if [ -x /sbin/lilo ]; then
        /sbin/lilo
