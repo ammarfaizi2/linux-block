88a00d5210d54f0d0fc3e38156d2eb519662681c
Ingo Molnar <mingo@kernel.org>
2021-06-03 23:44:28 +0200

    headers/deps: ipc/shm: Move the 'struct shmid_ds' definition to ipc/shm.c
    Only that .c file is using it - unnecessary to keep it in a header,
    where it introduces unnecessary header dependencies in <linux/sched.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- ipc/shm.c
+++ ipc/shm.c
@@ -97,6 +97,21 @@ static const struct vm_operations_struct shm_vm_ops;
 #define shm_unlock(shp)			\
 	ipc_unlock(&(shp)->shm_perm)
 
+/* Obsolete, used only for backwards compatibility and libc5 compiles */
+struct shmid_ds {
+	struct ipc_perm		shm_perm;	/* operation perms */
+	int			shm_segsz;	/* size of segment (bytes) */
+	__kernel_old_time_t	shm_atime;	/* last attach time */
+	__kernel_old_time_t	shm_dtime;	/* last detach time */
+	__kernel_old_time_t	shm_ctime;	/* last change time */
+	__kernel_ipc_pid_t	shm_cpid;	/* pid of creator */
+	__kernel_ipc_pid_t	shm_lpid;	/* pid of last operator */
+	unsigned short		shm_nattch;	/* no. of current attaches */
+	unsigned short		shm_unused;	/* compatibility */
+	void			*shm_unused2;	/* ditto - used by DIPC */
+	void			*shm_unused3;	/* unused */
+};
+
 static int newseg(struct ipc_namespace *, struct ipc_params *);
 static void shm_open(struct vm_area_struct *vma);
 static void shm_close(struct vm_area_struct *vma);
