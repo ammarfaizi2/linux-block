48374d8fa3c29fcbedf24080d1b36ad718eaf005
Ingo Molnar <mingo@kernel.org>
2021-06-03 23:44:28 +0200

    headers/deps: ipc/shm: Move the 'struct shmid_ds' definition to ipc/shm.c
    Only that .c file is using it - unnecessary to keep it in a header,
    where it introduces unnecessary header dependencies in <linux/sched.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/uapi/linux/shm.h
+++ include/uapi/linux/shm.h
@@ -24,21 +24,6 @@
 #define SHMALL (ULONG_MAX - (1UL << 24)) /* max shm system wide (pages) */
 #define SHMSEG SHMMNI			 /* max shared segs per process */
 
-/* Obsolete, used only for backwards compatibility and libc5 compiles */
-struct shmid_ds {
-	struct ipc_perm		shm_perm;	/* operation perms */
-	int			shm_segsz;	/* size of segment (bytes) */
-	__kernel_old_time_t	shm_atime;	/* last attach time */
-	__kernel_old_time_t	shm_dtime;	/* last detach time */
-	__kernel_old_time_t	shm_ctime;	/* last change time */
-	__kernel_ipc_pid_t	shm_cpid;	/* pid of creator */
-	__kernel_ipc_pid_t	shm_lpid;	/* pid of last operator */
-	unsigned short		shm_nattch;	/* no. of current attaches */
-	unsigned short 		shm_unused;	/* compatibility */
-	void 			*shm_unused2;	/* ditto - used by DIPC */
-	void			*shm_unused3;	/* unused */
-};
-
 /* Include the definition of shmid64_ds and shminfo64 */
 #include <asm/shmbuf.h>
 
