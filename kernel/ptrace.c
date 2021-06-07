ac01b09a94dba177b4f8be0da862ebafdf70146a
Ingo Molnar <mingo@kernel.org>
2021-06-07 11:19:47 +0200

    headers/deps: signals: Move task_struct::saved_sigmask to per_task()
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -1099,7 +1099,7 @@ int ptrace_request(struct task_struct *child, long request,
 		}
 
 		if (test_tsk_restore_sigmask(child))
-			mask = &child->saved_sigmask;
+			mask = &per_task(child, saved_sigmask);
 		else
 			mask = &per_task(child, blocked);
 
