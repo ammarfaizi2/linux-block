34aed241636f58e307ba2014ef222bdbc5bb92ea
Ingo Molnar <mingo@kernel.org>
2021-06-07 10:02:35 +0200

    headers/deps: mm: Move task_struct::vmacache to per_task()
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -473,9 +473,6 @@ struct task_struct {
 	struct mm_struct		*mm;
 	struct mm_struct		*active_mm;
 
-	/* Per-thread vma caching: */
-	struct vmacache			vmacache;
-
 #ifdef SPLIT_RSS_COUNTING
 	struct task_rss_stat		rss_stat;
 #endif
