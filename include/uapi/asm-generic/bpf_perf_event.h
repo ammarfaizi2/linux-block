6c068810ed8950a5242eed7cecaa1409e1f5ff88
Ingo Molnar <mingo@kernel.org>
2021-06-08 23:50:19 +0200

    headers/deps: bpf: Use <uapi/linux/ptrace.h> in <uapi/asm-generic/bpf_perf_event.h>
    The regular <linux/ptrace.h> header is much larger.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/uapi/asm-generic/bpf_perf_event.h
+++ include/uapi/asm-generic/bpf_perf_event.h
@@ -1,7 +1,7 @@
 #ifndef _UAPI__ASM_GENERIC_BPF_PERF_EVENT_H__
 #define _UAPI__ASM_GENERIC_BPF_PERF_EVENT_H__
 
-#include <linux/ptrace.h>
+#include <uapi/linux/ptrace.h>
 
 /* Export kernel pt_regs structure */
 typedef struct pt_regs bpf_user_pt_regs_t;
