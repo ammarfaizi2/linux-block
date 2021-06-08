f9606b692a6ada9e88c0a858a705f7a1ccd2d5fc
Ingo Molnar <mingo@kernel.org>
2021-06-08 20:51:41 +0200

    headers/uninline: Uninline multi-use function: Uninline scm_recv()
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/net/scm.h
+++ include/net/scm.h
@@ -99,36 +99,8 @@ static inline void scm_passec(struct socket *sock, struct msghdr *msg, struct sc
 { }
 #endif /* CONFIG_SECURITY_NETWORK */
 
-static __inline__ void scm_recv(struct socket *sock, struct msghdr *msg,
-				struct scm_cookie *scm, int flags)
-{
-	if (!msg->msg_control) {
-		if (test_bit(SOCK_PASSCRED, &sock->flags) || scm->fp)
-			msg->msg_flags |= MSG_CTRUNC;
-		scm_destroy(scm);
-		return;
-	}
-
-	if (test_bit(SOCK_PASSCRED, &sock->flags)) {
-		struct user_namespace *current_ns = current_user_ns();
-		struct ucred ucreds = {
-			.pid = scm->creds.pid,
-			.uid = from_kuid_munged(current_ns, scm->creds.uid),
-			.gid = from_kgid_munged(current_ns, scm->creds.gid),
-		};
-		put_cmsg(msg, SOL_SOCKET, SCM_CREDENTIALS, sizeof(ucreds), &ucreds);
-	}
-
-	scm_destroy_cred(scm);
-
-	scm_passec(sock, msg, scm);
-
-	if (!scm->fp)
-		return;
-	
-	scm_detach_fds(msg, scm);
-}
-
+extern void scm_recv(struct socket *sock, struct msghdr *msg,
+		     struct scm_cookie *scm, int flags);
 
 #endif /* __LINUX_NET_SCM_H */
 
--- net/core/scm.c
+++ net/core/scm.c
@@ -51,6 +51,37 @@ int scm_send(struct socket *sock, struct msghdr *msg,
 }
 EXPORT_SYMBOL(scm_send);
 
+void scm_recv(struct socket *sock, struct msghdr *msg,
+	      struct scm_cookie *scm, int flags)
+{
+	if (!msg->msg_control) {
+		if (test_bit(SOCK_PASSCRED, &sock->flags) || scm->fp)
+			msg->msg_flags |= MSG_CTRUNC;
+		scm_destroy(scm);
+		return;
+	}
+
+	if (test_bit(SOCK_PASSCRED, &sock->flags)) {
+		struct user_namespace *current_ns = current_user_ns();
+		struct ucred ucreds = {
+			.pid = scm->creds.pid,
+			.uid = from_kuid_munged(current_ns, scm->creds.uid),
+			.gid = from_kgid_munged(current_ns, scm->creds.gid),
+		};
+		put_cmsg(msg, SOL_SOCKET, SCM_CREDENTIALS, sizeof(ucreds), &ucreds);
+	}
+
+	scm_destroy_cred(scm);
+
+	scm_passec(sock, msg, scm);
+
+	if (!scm->fp)
+		return;
+
+	scm_detach_fds(msg, scm);
+}
+EXPORT_SYMBOL(scm_recv);
+
 /*
  *	Only allow a user to send credentials, that they could set with
  *	setu(g)id.
