c543885e0d8a232de5aea25c34c2acafd9ad1b63
Thomas Gleixner <tglx@linutronix.de>
2021-06-10 15:27:58 +0200

    x86/fpu: Rename copy_user_to_xregs() and copy_xregs_to_user()
    The function names for xsave[s]/xrstor[s] operations are horribly named and
    a permanent source of confusion.
    
    Rename:
    	copy_xregs_to_user() to xsave_to_user_sigframe()
    	copy_user_to_xregs() to xrstor_from_user_sigframe()
    
    so it's entirely clear what this is about. This is also a clear indicator
    of the potentially different storage format because this is user ABI and
    cannot use compacted format.
    
    No functional change.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/fpu/signal.c
+++ arch/x86/kernel/fpu/signal.c
@@ -129,7 +129,7 @@ static inline int copy_fpregs_to_sigframe(struct xregs_state __user *buf)
 	int err;
 
 	if (use_xsave())
-		err = copy_xregs_to_user(buf);
+		err = xsave_to_user_sigframe(buf);
 	else if (use_fxsr())
 		err = copy_fxregs_to_user((struct fxregs_state __user *) buf);
 	else
@@ -276,7 +276,7 @@ static int copy_user_to_fpregs_zeroing(void __user *buf, u64 xbv, int fx_only)
 		} else {
 			init_bv = xfeatures_mask_user() & ~xbv;
 
-			r = copy_user_to_xregs(buf, xbv);
+			r = xrstor_from_user_sigframe(buf, xbv);
 			if (!r && unlikely(init_bv))
 				xrstor_from_kernel(&init_fpstate.xsave, init_bv);
 			return r;
