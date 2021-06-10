027431e8de9e80eb01ee39167c3b0d3f24a89d4f
Ingo Molnar <mingo@kernel.org>
2021-06-10 13:39:47 +0200

    headers/uninline: Uninline multi-use function: eventpoll_release()
    This also makes <linux/eventpoll.h> a standalone header, by decoupling
    it from the definition of 'struct file'.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/eventpoll.h
+++ include/linux/eventpoll.h
@@ -26,34 +26,7 @@ struct file *get_epoll_tfile_raw_ptr(struct file *file, int tfd, unsigned long t
 
 /* Used to release the epoll bits inside the "struct file" */
 void eventpoll_release_file(struct file *file);
-
-/*
- * This is called from inside fs/file_table.c:__fput() to unlink files
- * from the eventpoll interface. We need to have this facility to cleanup
- * correctly files that are closed without being removed from the eventpoll
- * interface.
- */
-static inline void eventpoll_release(struct file *file)
-{
-
-	/*
-	 * Fast check to avoid the get/release of the semaphore. Since
-	 * we're doing this outside the semaphore lock, it might return
-	 * false negatives, but we don't care. It'll help in 99.99% of cases
-	 * to avoid the semaphore lock. False positives simply cannot happen
-	 * because the file in on the way to be removed and nobody ( but
-	 * eventpoll ) has still a reference to this file.
-	 */
-	if (likely(!file->f_ep))
-		return;
-
-	/*
-	 * The file is being closed while it is still linked to an epoll
-	 * descriptor. We need to handle this by correctly unlinking it
-	 * from its containers.
-	 */
-	eventpoll_release_file(file);
-}
+void eventpoll_release(struct file *file);
 
 int do_epoll_ctl(int epfd, int op, int fd, struct epoll_event *epds,
 		 bool nonblock);
