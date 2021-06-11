33b422a600f5d1eccd07b69b9e75f72f9619ac6d
Ingo Molnar <mingo@kernel.org>
2021-06-11 12:36:49 +0200

    headers/deps: net: Move header includes to the top of the file in <net/net_namespace.h>
    It's easier to reorganize headers if all inclusions are at the top of the file.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/net/net_namespace.h
+++ include/net/net_namespace.h
@@ -40,6 +40,13 @@
 #include <linux/skbuff.h>
 #include <linux/notifier.h>
 
+#include <linux/seq_file_net.h>
+
+#ifndef CONFIG_NET_NS
+#include <linux/sched.h>
+#include <linux/nsproxy.h>
+#endif
+
 struct user_namespace;
 struct proc_dir_entry;
 struct net_device;
@@ -180,8 +187,6 @@ struct net {
 #endif
 } __randomize_layout;
 
-#include <linux/seq_file_net.h>
-
 /* Init's network namespace */
 extern struct net init_net;
 
@@ -196,8 +201,6 @@ void net_ns_barrier(void);
 struct ns_common *get_net_ns(struct ns_common *ns);
 struct net *get_net_ns_by_fd(int fd);
 #else /* CONFIG_NET_NS */
-#include <linux/sched.h>
-#include <linux/nsproxy.h>
 static inline struct net *copy_net_ns(unsigned long flags,
 	struct user_namespace *user_ns, struct net *old_net)
 {
