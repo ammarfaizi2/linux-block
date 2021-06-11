da79375a634a9222bc60a33c0dbda3f05060e3e9
Ingo Molnar <mingo@kernel.org>
2021-06-11 10:18:04 +0200

    headers/deps: mm: Move the MAX_ORDER* definitions from <linux/mmzone.h> to <linux/gfp_types.h>
    It's a much smaller header, and there's code that only needs the
    basic defines.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/gfp_types.h
+++ include/linux/gfp_types.h
@@ -4,6 +4,22 @@
 
 #include <uapi/linux/types.h>
 
+/* Free memory management - zoned buddy allocator.  */
+#ifndef CONFIG_FORCE_MAX_ZONEORDER
+#define MAX_ORDER 11
+#else
+#define MAX_ORDER CONFIG_FORCE_MAX_ZONEORDER
+#endif
+#define MAX_ORDER_NR_PAGES (1 << (MAX_ORDER - 1))
+
+/*
+ * PAGE_ALLOC_COSTLY_ORDER is the order at which allocations are deemed
+ * costly to service.  That is between allocation orders which should
+ * coalesce naturally under reasonable reclaim pressure and those which
+ * will not.
+ */
+#define PAGE_ALLOC_COSTLY_ORDER 3
+
 /* The typedef is in types.h but we want the documentation here */
 #if 0
 /**
--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -21,25 +21,10 @@
 #include <linux/page-flags-layout.h>
 #include <linux/atomic.h>
 #include <linux/mm_types.h>
+#include <linux/gfp_types.h>
 #include <linux/page-flags.h>
 #include <linux/local_lock.h>
 #include <asm/page.h>
-
-/* Free memory management - zoned buddy allocator.  */
-#ifndef CONFIG_FORCE_MAX_ZONEORDER
-#define MAX_ORDER 11
-#else
-#define MAX_ORDER CONFIG_FORCE_MAX_ZONEORDER
-#endif
-#define MAX_ORDER_NR_PAGES (1 << (MAX_ORDER - 1))
-
-/*
- * PAGE_ALLOC_COSTLY_ORDER is the order at which allocations are deemed
- * costly to service.  That is between allocation orders which should
- * coalesce naturally under reasonable reclaim pressure and those which
- * will not.
- */
-#define PAGE_ALLOC_COSTLY_ORDER 3
 
 enum migratetype {
 	MIGRATE_UNMOVABLE,
