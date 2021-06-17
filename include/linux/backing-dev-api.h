626582dd40debfc78e1b4c6350a3ef91526cbdbf
Ingo Molnar <mingo@kernel.org>
2021-06-17 11:35:38 +0200

    headers/deps: bdi: Move 'struct backing_dev_info' from <linux/backing-dev-types.h> to <linux/backing-dev-api.h>
    This structure is only used by bdi internal code, and it includes
    a number of complex types we can avoid in <linux/backing-dev-types.h>.
    
    This will allow further optimization of <linux/backing-dev-types.h>'s
    header dependencies.
    
    Fix up the places that depend on the full backing_dev_info definition.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/backing-dev-api.h
+++ include/linux/backing-dev-api.h
@@ -19,6 +19,44 @@
 #include <linux/blkdev.h>
 #include <linux/percpu_counter_api.h>
 
+struct backing_dev_info {
+	u64 id;
+	struct rb_node rb_node; /* keyed by ->id */
+	struct list_head bdi_list;
+	unsigned long ra_pages;	/* max readahead in PAGE_SIZE units */
+	unsigned long io_pages;	/* max allowed IO size */
+
+	struct kref refcnt;	/* Reference counter for the structure */
+	unsigned int capabilities; /* Device capabilities */
+	unsigned int min_ratio;
+	unsigned int max_ratio, max_prop_frac;
+
+	/*
+	 * Sum of avg_write_bw of wbs with dirty inodes.  > 0 if there are
+	 * any dirty wbs, which is depended upon by bdi_has_dirty().
+	 */
+	atomic_long_t tot_write_bandwidth;
+
+	struct bdi_writeback wb;  /* the root writeback info for this bdi */
+	struct list_head wb_list; /* list of all wbs */
+#ifdef CONFIG_CGROUP_WRITEBACK
+	struct radix_tree_root cgwb_tree; /* radix tree of active cgroup wbs */
+	struct mutex cgwb_release_mutex;  /* protect shutdown of wb structs */
+	struct rw_semaphore wb_switch_rwsem; /* no cgwb switch while syncing */
+#endif
+	wait_queue_head_t wb_waitq;
+
+	struct device *dev;
+	char dev_name[64];
+	struct device *owner;
+
+	struct timer_list laptop_mode_wb_timer;
+
+#ifdef CONFIG_DEBUG_FS
+	struct dentry *debug_dir;
+#endif
+};
+
 enum {
 	BLK_RW_ASYNC	= 0,
 	BLK_RW_SYNC	= 1,
