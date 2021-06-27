eb66d60e0b0b099128857f7c1054779fadbf7d06
Ingo Molnar <mingo@kernel.org>
2021-06-27 20:03:43 +0200

    headers/uninline: Uninline multi-use function: dma_map_single_attrs()
    At 20 instructions this is not a small function - and uninlining it
    also decouples <linux/dma-mapping.h> from <linux/device_api.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -319,17 +319,8 @@ static inline void dma_free_noncoherent(struct device *dev, size_t size,
 	dma_free_pages(dev, size, virt_to_page(vaddr), dma_handle, dir);
 }
 
-static inline dma_addr_t dma_map_single_attrs(struct device *dev, void *ptr,
-		size_t size, enum dma_data_direction dir, unsigned long attrs)
-{
-	/* DMA must never operate on areas that might be remapped. */
-	if (dev_WARN_ONCE(dev, is_vmalloc_addr(ptr),
-			  "rejecting DMA map of vmalloc memory\n"))
-		return DMA_MAPPING_ERROR;
-	debug_dma_map_single(dev, ptr, size);
-	return dma_map_page_attrs(dev, virt_to_page(ptr), offset_in_page(ptr),
-			size, dir, attrs);
-}
+dma_addr_t dma_map_single_attrs(struct device *dev, void *ptr,
+		size_t size, enum dma_data_direction dir, unsigned long attrs);
 
 static inline void dma_unmap_single_attrs(struct device *dev, dma_addr_t addr,
 		size_t size, enum dma_data_direction dir, unsigned long attrs)
