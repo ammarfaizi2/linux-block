1ec1e7a65e8f0e2f1503e68ad77029f0c626122e
Ingo Molnar <mingo@kernel.org>
2021-06-29 11:37:36 +0200

    headers/prep: Fix header to build standalone: <asm-generic/bitops/find.h>
    Fix:
    
      ./include/asm-generic/bitops/find.h:32:17: error: implicit declaration of function ‘GENMASK’ [-Werror=implicit-function-declaration]
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- tools/include/linux/find.h
+++ tools/include/linux/find.h
@@ -8,6 +8,8 @@
 
 #include <linux/bitops.h>
 
+#include <linux/bits.h>
+
 extern unsigned long _find_next_bit(const unsigned long *addr1,
 		const unsigned long *addr2, unsigned long nbits,
 		unsigned long start, unsigned long invert, unsigned long le);
