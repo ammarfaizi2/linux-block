30628611b524fb4c46d7c6779512877026b2215b
Ingo Molnar <mingo@kernel.org>
2021-07-04 11:52:20 +0200

    headers/deps: net: Optimize <uapi/linux/rtnetlink.h> dependencies
    Also convert linux/rtnetlink.h's inlined rcu_dereference() uses to macros,
    this way we can avoid including <linux/rcupdate.h> in files that don't
    need it.
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
      before: | #include <uapi/linux/rtnetlink.h>       | LOC: 15,699 | headers:  318
       after: | #include <uapi/linux/rtnetlink.h>       | LOC:  1,508 | headers:   32
    
    Doing this also shrinks <linux/rtnetlink.h>'s size substantially:
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
      before: | #include <linux/rtnetlink.h>            | LOC: 17,385 | headers:  349
       after: | #include <linux/rtnetlink.h>            | LOC:  6,620 | headers:  147
    
    These headers are part of the <linux/tcp.h> superheader's dependency chain.
    
    Fix dangling dependencies.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/uapi/linux/rtnetlink.h
+++ include/uapi/linux/rtnetlink.h
@@ -2,11 +2,11 @@
 #ifndef _UAPI__LINUX_RTNETLINK_H
 #define _UAPI__LINUX_RTNETLINK_H
 
-#include <linux/types.h>
-#include <linux/netlink.h>
-#include <linux/if_link.h>
-#include <linux/if_addr.h>
-#include <linux/neighbour.h>
+#include <uapi/linux/types.h>
+#include <uapi/linux/netlink.h>
+#include <uapi/linux/if_link.h>
+#include <uapi/linux/if_addr.h>
+#include <uapi/linux/neighbour.h>
 
 /* rtnetlink families. Values up to 127 are reserved for real address
  * families, values above 128 may be used arbitrarily.
