81c5bbda56fd5fed14dc844b253a223c8f66b23c
Ingo Molnar <mingo@kernel.org>
2021-07-04 10:46:27 +0200

    headers/deps: notifiers: Move the NOTIFY_* constants into <linux/notifier_types.h>
    Some code only needs these constants, not the full notifier APIs.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/notifier_api.h
+++ include/linux/notifier_api.h
@@ -165,16 +165,6 @@ extern int blocking_notifier_call_chain_robust(struct blocking_notifier_head *nh
 extern int raw_notifier_call_chain_robust(struct raw_notifier_head *nh,
 		unsigned long val_up, unsigned long val_down, void *v);
 
-#define NOTIFY_DONE		0x0000		/* Don't care */
-#define NOTIFY_OK		0x0001		/* Suits me */
-#define NOTIFY_STOP_MASK	0x8000		/* Don't call further */
-#define NOTIFY_BAD		(NOTIFY_STOP_MASK|0x0002)
-						/* Bad/Veto action */
-/*
- * Clean way to return from the notifier and stop further calls.
- */
-#define NOTIFY_STOP		(NOTIFY_OK|NOTIFY_STOP_MASK)
-
 /* Encapsulate (negative) errno value (in particular, NOTIFY_BAD <=> EPERM). */
 static inline int notifier_from_errno(int err)
 {
