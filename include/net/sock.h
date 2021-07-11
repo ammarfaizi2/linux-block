e0b16f3adbaa8e3c3398800586c3d199792d994c
Ingo Molnar <mingo@kernel.org>
2021-07-12 00:42:41 +0200

    headers/deps: net: Split out <net/sock_batch.h> from <net/sock.h>
    The <linux/percpu_counter_api.h> header is pretty large, but only
    a small minority of APIs defined in <net/sock.h> require it.
    
    Split them out.
    
    This is going to allow the reduction of <net/sock.h> header
    dependencies.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1377,32 +1377,6 @@ sk_memory_allocated_sub(struct sock *sk, int amt)
 	atomic_long_sub(amt, sk->sk_prot->memory_allocated);
 }
 
-#define SK_ALLOC_PERCPU_COUNTER_BATCH 16
-
-static inline void sk_sockets_allocated_dec(struct sock *sk)
-{
-	percpu_counter_add_batch(sk->sk_prot->sockets_allocated, -1,
-				 SK_ALLOC_PERCPU_COUNTER_BATCH);
-}
-
-static inline void sk_sockets_allocated_inc(struct sock *sk)
-{
-	percpu_counter_add_batch(sk->sk_prot->sockets_allocated, 1,
-				 SK_ALLOC_PERCPU_COUNTER_BATCH);
-}
-
-static inline u64
-sk_sockets_allocated_read_positive(struct sock *sk)
-{
-	return percpu_counter_read_positive(sk->sk_prot->sockets_allocated);
-}
-
-static inline int
-proto_sockets_allocated_sum_positive(struct proto *prot)
-{
-	return percpu_counter_sum_positive(prot->sockets_allocated);
-}
-
 static inline long
 proto_memory_allocated(struct proto *prot)
 {
--- include/net/sock_batch.h
+++ include/net/sock_batch.h
@@ -1,1 +1,34 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _SOCK_BATCH_H
+#define _SOCK_BATCH_H
+
 #include <net/sock.h>
+#include <linux/percpu_counter_api.h>
+
+#define SK_ALLOC_PERCPU_COUNTER_BATCH 16
+
+static inline void sk_sockets_allocated_dec(struct sock *sk)
+{
+	percpu_counter_add_batch(sk->sk_prot->sockets_allocated, -1,
+				 SK_ALLOC_PERCPU_COUNTER_BATCH);
+}
+
+static inline void sk_sockets_allocated_inc(struct sock *sk)
+{
+	percpu_counter_add_batch(sk->sk_prot->sockets_allocated, 1,
+				 SK_ALLOC_PERCPU_COUNTER_BATCH);
+}
+
+static inline u64
+sk_sockets_allocated_read_positive(struct sock *sk)
+{
+	return percpu_counter_read_positive(sk->sk_prot->sockets_allocated);
+}
+
+static inline int
+proto_sockets_allocated_sum_positive(struct proto *prot)
+{
+	return percpu_counter_sum_positive(prot->sockets_allocated);
+}
+
+#endif	/* _SOCK_BATCH_H */
