50d515c44cc0dfb09625c41c49b1c0d976afdb4c
Ingo Molnar <mingo@kernel.org>
2021-07-12 00:14:51 +0200

    headers/deps: net: Separate out <linux/socket_alloc.h>
    Move the 'struct socket_alloc' functionality out of <net/sock.h>,
    to decouple it from type definitions in <linux/fs.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/net/socket_alloc.h
+++ include/net/socket_alloc.h
@@ -1,1 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _SOCKET_ALLOC_H
+#define _SOCKET_ALLOC_H
+
 #include <net/sock.h>
+#include <linux/fs.h>
+#include <linux/net.h>
+
+struct socket_alloc {
+	struct socket socket;
+	struct inode vfs_inode;
+};
+
+static inline struct socket *SOCKET_I(struct inode *inode)
+{
+	return &container_of(inode, struct socket_alloc, vfs_inode)->socket;
+}
+
+static inline struct inode *SOCK_INODE(struct socket *socket)
+{
+	return &container_of(socket, struct socket_alloc, socket)->vfs_inode;
+}
+
+#endif	/* _SOCKET_ALLOC_H */
