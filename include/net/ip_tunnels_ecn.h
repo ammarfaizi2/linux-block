92c39607dece9ef27eb2f1ad9a86bd77ac8f4d1a
Ingo Molnar <mingo@kernel.org>
2021-07-12 01:06:48 +0200

    headers/deps: net: Move ip_tunnel_ecn_encap() into <net/ip_tunnels_ecn.h>
    This is the only function in <linux/ip_tunnels.h> that requires
    the <net/inet_ecn_api.h> header.
    
    Move the function out, so that <linux/ip_tunnels.h> (which is
    included over 700 times in a distro build) can become more lightweight.
    
    Just 5 .c files require the new header.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/net/ip_tunnels_ecn.h
+++ include/net/ip_tunnels_ecn.h
@@ -1,1 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __NET_IP_TUNNELS_ECN_H
+#define __NET_IP_TUNNELS_ECN_H 1
+
 #include <net/ip_tunnels.h>
+#include <net/inet_ecn.h>
+
+/* Propogate ECN bits out */
+static inline u8 ip_tunnel_ecn_encap(u8 tos, const struct iphdr *iph,
+				     const struct sk_buff *skb)
+{
+	u8 inner = ip_tunnel_get_dsfield(iph, skb);
+
+	return INET_ECN_encapsulate(tos, inner);
+}
+
+#endif /* __NET_IP_TUNNELS_ECN_H */
