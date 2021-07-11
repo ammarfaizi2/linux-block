1bf2d359b908101aa6bab0a7cd5445c45665f99f
Ingo Molnar <mingo@kernel.org>
2021-07-12 00:50:48 +0200

    headers/deps: net/sunrpc: Optimize <linux/sunrpc/types.h>
    This header gets included over 200 times in a distro build:
    
        201 In file included from ./include/linux/sunrpc/types.h:14,
    
    But this functionality doesn't really offer anything that direct
    inclusion of headers couldn't:
    
     #include <linux/timer.h>
     #include <linux/sched/signal.h>
     #include <linux/workqueue.h>
     #include <linux/sunrpc/debug.h>
     #include <linux/list.h>
    
    And these are large headers that preprocess to a lot of lines of code:
    
      #include <linux/sunrpc/types.h>      | LOC: 14324 | headers: 338 |
    
    So this header is basically spreading a lot of unnecessary
    header inclusions to ~200 files.
    
    Do this differently: remove the inclusions & use direct headers.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sunrpc/types.h
+++ include/linux/sunrpc/types.h
@@ -10,12 +10,6 @@
 #ifndef _LINUX_SUNRPC_TYPES_H_
 #define _LINUX_SUNRPC_TYPES_H_
 
-#include <linux/timer.h>
-#include <linux/sched/signal.h>
-#include <linux/workqueue.h>
-#include <linux/sunrpc/debug.h>
-#include <linux/list.h>
-
 /*
  * Shorthands
  */
