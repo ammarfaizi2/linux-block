79d79e7b7595a36213d80c3ae5e7f74eede55241
Ingo Molnar <mingo@kernel.org>
2021-07-12 01:52:36 +0200

    headers/deps: net: Move netlink_skb_clone() into its own header
    In an effort to decouple <linux/netlink.h> from <linux/mm_api.h>,
    move out this function - whose use of is_vmalloc_addr() couples
    the commonly included <linux/netlink.h> header to MM headers.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/netlink_api_skb_clone.h
+++ include/linux/netlink_api_skb_clone.h
@@ -1,1 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __LINUX_NETLINK_API_SKB_CLONE_H
+#define __LINUX_NETLINK_API_SKB_CLONE_H
+
+#include <linux/skbuff_api.h>
 #include <linux/netlink.h>
+#include <linux/mm_page_address.h>
+
+static inline struct sk_buff *
+netlink_skb_clone(struct sk_buff *skb, gfp_t gfp_mask)
+{
+	struct sk_buff *nskb;
+
+	nskb = skb_clone(skb, gfp_mask);
+	if (!nskb)
+		return NULL;
+
+	/* This is a large skb, set destructor callback to release head */
+	if (is_vmalloc_addr(skb->head))
+		nskb->destructor = skb->destructor;
+
+	return nskb;
+}
+
+#endif	/* __LINUX_NETLINK_API_SKB_CLONE_H */
