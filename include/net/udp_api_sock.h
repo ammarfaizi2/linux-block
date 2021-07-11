4c0b8243dba314dfd906dcd6167bf9b66edcf954
Ingo Molnar <mingo@kernel.org>
2021-07-12 01:59:35 +0200

    headers/deps: net: Decouple <net/udp.h> from <net/sock_api.h>
    Move two methods that depend on <net/sock_api.h> facilities
    to a separate <net/udp_api_sock.h> header.
    
    This allows the main <net/udp.h> header to remain independent
    of <net/sock_api.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/net/udp_api_sock.h
+++ include/net/udp_api_sock.h
@@ -1,1 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _UDP_API_SOCK_H
+#define _UDP_API_SOCK_H
+
 #include <net/udp.h>
+#include <net/sock_api.h>
+
+static inline void udp_lib_close(struct sock *sk, long timeout)
+{
+	sk_common_release(sk);
+}
+
+static inline int udp_rqueue_get(struct sock *sk)
+{
+	return sk_rmem_alloc_get(sk) - READ_ONCE(udp_sk(sk)->forward_deficit);
+}
+#endif	/* _UDP_API_SOCK_H */
