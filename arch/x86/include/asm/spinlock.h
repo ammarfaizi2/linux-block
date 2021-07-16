a42dfc4a3bd6f7c2a4433fd650da7d948af3f539
Ingo Molnar <mingo@kernel.org>
2021-07-16 22:15:41 +0200

    headers/deps: locking/x86: Optimize <asm/spinlock.h> dependencies
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
      before: | #include <asm/spinlock.h>               | LOC:  4,788 | headers:  132
       after: | #include <asm/spinlock.h>               | LOC:  4,340 | headers:  120
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/spinlock.h
+++ arch/x86/include/asm/spinlock.h
@@ -2,14 +2,6 @@
 #ifndef _ASM_X86_SPINLOCK_H
 #define _ASM_X86_SPINLOCK_H
 
-#include <linux/jump_label.h>
-#include <linux/atomic.h>
-#include <asm/page.h>
-#include <asm/processor.h>
-#include <linux/compiler.h>
-#include <asm/paravirt.h>
-#include <asm/bitops.h>
-
 /*
  * Your basic SMP spinlocks, allowing only a single CPU anywhere
  *
