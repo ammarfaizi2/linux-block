bdf0db6d42fbde0a76186ff1022497ccbc549277
Christian Brauner <christian.brauner@ubuntu.com>
2021-07-16 10:37:32 +0200

    ceph/acl: allow idmapped set_acl inode op
    Enable ceph_set_acl() to handle idmapped mounts. This is just a matter
    of passing down the mount's idmapping.
    
    Cc: Jeff Layton <jlayton@kernel.org>
    Cc: Ilya Dryomov <idryomov@gmail.com>
    Cc: ceph-devel@vger.kernel.org
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- fs/ceph/acl.c
+++ fs/ceph/acl.c
@@ -104,7 +104,7 @@ int ceph_set_acl(struct user_namespace *mnt_userns, struct inode *inode,
 	case ACL_TYPE_ACCESS:
 		name = XATTR_NAME_POSIX_ACL_ACCESS;
 		if (acl) {
-			ret = posix_acl_update_mode(&init_user_ns, inode,
+			ret = posix_acl_update_mode(mnt_userns, inode,
 						    &new_mode, &acl);
 			if (ret)
 				goto out;
