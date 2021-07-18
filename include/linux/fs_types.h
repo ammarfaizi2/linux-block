f38b14785ca0e2f3db96aca6fe28680a86115a82
Ingo Molnar <mingo@kernel.org>
2021-07-18 09:56:44 +0200

    headers/deps: fs: Optimize <linux/fs_types.h> dependencies
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
      before: | #include <linux/fs_types.h>             | LOC:  5,011 | headers:  204
       after: | #include <linux/fs_types.h>             | LOC:  4,559 | headers:  190
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/fs_types.h
+++ include/linux/fs_types.h
@@ -2,7 +2,7 @@
 #ifndef _LINUX_FS_TYPES_H
 #define _LINUX_FS_TYPES_H
 
-#include <linux/list_bl.h>
+#include <linux/list_bl_types.h>
 #include <linux/cache.h>
 #include <linux/path.h>
 #include <linux/list_lru.h>
