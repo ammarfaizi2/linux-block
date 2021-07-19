a05cc4c5357d031a93f91ce8031434adc35e40b7
Ingo Molnar <mingo@kernel.org>
2021-07-19 14:46:51 +0200

    headers/deps: fs: Introduce <linux/fs_api_dir_context.h> header
    Move the 'struct dir_context' related APIs into a separate header.
    
    Besides reducing the size of <linux/fs.h> directly - one of the biggest headers
    in the kernel - doing this also allows a decoupling of <linux/fs.h> from
    <linux/fs_entry_types.h> in the future: these dir-context APIs were the only
    users of the types defined by <linux/fs_entry_types.h>
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/fs_api_dir_context.h
+++ include/linux/fs_api_dir_context.h
@@ -1,1 +1,43 @@
-#include <linux/fs.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_FS_API_DIR_CONTEXT_H
+#define _LINUX_FS_API_DIR_CONTEXT_H
+
+#include <linux/fs_api.h>
+
+#include <linux/fs_entry_types.h>
+
+static inline bool dir_emit(struct dir_context *ctx,
+			    const char *name, int namelen,
+			    u64 ino, unsigned type)
+{
+	return ctx->actor(ctx, name, namelen, ctx->pos, ino, type) == 0;
+}
+
+static inline bool dir_emit_dot(struct file *file, struct dir_context *ctx)
+{
+	return ctx->actor(ctx, ".", 1, ctx->pos,
+			  file->f_path.dentry->d_inode->i_ino, DT_DIR) == 0;
+}
+
+static inline bool dir_emit_dotdot(struct file *file, struct dir_context *ctx)
+{
+	return ctx->actor(ctx, "..", 2, ctx->pos,
+			  parent_ino(file->f_path.dentry), DT_DIR) == 0;
+}
+
+static inline bool dir_emit_dots(struct file *file, struct dir_context *ctx)
+{
+	if (ctx->pos == 0) {
+		if (!dir_emit_dot(file, ctx))
+			return false;
+		ctx->pos = 1;
+	}
+	if (ctx->pos == 1) {
+		if (!dir_emit_dotdot(file, ctx))
+			return false;
+		ctx->pos = 2;
+	}
+	return true;
+}
+
+#endif /* _LINUX_FS_API_DIR_CONTEXT_H */
