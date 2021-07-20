a19cfd3ef6047a0d13c29fab32a7edb82e435856
Ingo Molnar <mingo@kernel.org>
2021-07-20 19:22:43 +0200

    headers/deps: u64_stats: Move the u64_stats_t types into <linux/u64_stats_sync_types.h>
    Make the types header applicable to <linux/netdevice_types.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/u64_stats_sync_types.h
+++ include/linux/u64_stats_sync_types.h
@@ -2,6 +2,8 @@
 #ifndef _LINUX_U64_STATS_SYNC_TYPES_H
 #define _LINUX_U64_STATS_SYNC_TYPES_H
 
+#include <linux/types.h>
+
 #include <asm/bitsperlong.h>
 
 #if BITS_PER_LONG==32 && defined(CONFIG_SMP)
@@ -13,5 +15,20 @@ struct u64_stats_sync {
 	seqcount_t	seq;
 #endif
 };
+
+#if BITS_PER_LONG == 64
+#include <asm/local64.h>
+
+typedef struct {
+	local64_t	v;
+} u64_stats_t ;
+
+#else
+
+typedef struct {
+	u64		v;
+} u64_stats_t;
+
+#endif
 
 #endif /* _LINUX_U64_STATS_SYNC_TYPES_H */
