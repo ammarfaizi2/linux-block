69f8f59e217df396de2755908ecda2c87483bf55
Ingo Molnar <mingo@kernel.org>
2021-07-22 11:35:51 +0200

    headers/deps: kobject: Optimize <linux/kobject_api.h> dependencies, remove <linux/kref_api.h> inclusion
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
      before: | #include <linux/kobject_api.h>          | LOC:  4,429 | headers:  145
       after: | #include <linux/kobject_api.h>          | LOC:  2,258 | headers:  130
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/kobject_api.h
+++ include/linux/kobject_api.h
@@ -24,7 +24,6 @@
 #include <linux/compiler.h>
 #include <linux/container_of.h>
 #include <linux/spinlock.h>
-#include <linux/kref_api.h>
 #include <linux/kobject_ns.h>
 #include <linux/wait.h>
 #include <linux/atomic.h>
