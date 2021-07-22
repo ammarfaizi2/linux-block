b3bacbd05a450d6a6efc8da3d7d6fde1fb846a7d
Paul E. McKenney <paulmck@kernel.org>
2021-07-22 14:12:07 -0700

    EXP cpu: CPU-hotplug progress checks
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -128,6 +128,9 @@ void clear_tasks_mm_cpumask(int cpu);
 int remove_cpu(unsigned int cpu);
 int cpu_device_down(struct device *dev);
 extern void smp_shutdown_nonboot_cpus(unsigned int primary_cpu);
+extern void cpu_hp_start_now(void);
+extern void cpu_hp_stop_now(void);
+extern void cpu_hp_check_delay(const char *s, void *func);
 
 #else /* CONFIG_HOTPLUG_CPU */
 
@@ -141,6 +144,9 @@ static inline void cpu_hotplug_disable(void) { }
 static inline void cpu_hotplug_enable(void) { }
 static inline int remove_cpu(unsigned int cpu) { return -EPERM; }
 static inline void smp_shutdown_nonboot_cpus(unsigned int primary_cpu) { }
+static inline void cpu_hp_start_now(void) { }
+static inline void cpu_hp_stop_now(void) { }
+static inline void cpu_hp_check_delay(const char *s, void *func) { }
 #endif	/* !CONFIG_HOTPLUG_CPU */
 
 /* Wrappers which go away once all code is converted */
