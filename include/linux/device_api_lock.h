a769ef41242c584708cded200159457e0620d61f
Ingo Molnar <mingo@kernel.org>
2021-08-01 01:53:53 +0200

    headers/deps: drivers/core: Move the device_lock*() APIs from <linux/device_api.h> to the new <linux/device_api_lock.h> header
    This will allow the removal of the <linux/mutex_api.h> dependency from <linux/device_api.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/device_api_lock.h
+++ include/linux/device_api_lock.h
@@ -1,2 +1,35 @@
-#include <linux/lockdep_api.h>
+// SPDX-License-Identifier: GPL-2.0
+#ifndef _DEVICE_API_LOCK_H_
+#define _DEVICE_API_LOCK_H_
+
 #include <linux/device_api.h>
+
+#include <linux/lockdep_api.h>
+#include <linux/mutex_api.h>
+
+static inline void device_lock(struct device *dev)
+{
+	mutex_lock(&dev->mutex);
+}
+
+static inline int device_lock_interruptible(struct device *dev)
+{
+	return mutex_lock_interruptible(&dev->mutex);
+}
+
+static inline int device_trylock(struct device *dev)
+{
+	return mutex_trylock(&dev->mutex);
+}
+
+static inline void device_unlock(struct device *dev)
+{
+	mutex_unlock(&dev->mutex);
+}
+
+static inline void device_lock_assert(struct device *dev)
+{
+	lockdep_assert_held(&dev->mutex);
+}
+
+#endif /* _DEVICE_API_LOCK_H_ */
