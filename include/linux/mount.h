f9521fb6ea9fdee8ee26903bb03b36e441ed5ae7
Ingo Molnar <mingo@kernel.org>
2021-08-02 17:48:12 +0200

    headers/deps: fs: Optimize <linux/mount.h> dependencies
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
      before: | #include <linux/mount.h>                | LOC:  2,859 | headers:  128
       after: | #include <linux/mount.h>                | LOC:    314 | headers:   40
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/mount.h
+++ include/linux/mount.h
@@ -10,12 +10,7 @@
 #ifndef _LINUX_MOUNT_H
 #define _LINUX_MOUNT_H
 
-#include <linux/types.h>
-#include <linux/list.h>
-#include <linux/nodemask.h>
-#include <linux/spinlock.h>
-#include <linux/seqlock.h>
-#include <linux/atomic.h>
+#include <asm/barrier.h>
 
 struct super_block;
 struct vfsmount;
