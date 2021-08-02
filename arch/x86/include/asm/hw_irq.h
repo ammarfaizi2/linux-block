6de82e54fd8524e49ce3ad8e3a36980818381350
Ingo Molnar <mingo@kernel.org>
2021-08-02 22:58:17 +0200

    headers/deps: irq/x86: Move arch local definitions into the new <asm/x86_irq.h> header
    Don't burden the <asm/hw_irq.h> header with these definitions,
    which gets included in over 1,500 places:
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
      before: | #include <asm/hw_irq.h>                 | LOC:  1,497 | headers:   99
       after: | #include <asm/hw_irq.h>                 | LOC:    449 | headers:   29
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/hw_irq.h
+++ arch/x86/include/asm/hw_irq.h
@@ -20,14 +20,13 @@
 
 #ifndef __ASSEMBLY__
 
-#include <linux/percpu.h>
-#include <linux/smp.h>
-
-#include <linux/atomic.h>
 #include <asm/irq.h>
-#include <asm/sections.h>
 
-#ifdef	CONFIG_X86_LOCAL_APIC
+/* Statistics */
+extern atomic_t irq_err_count;
+extern atomic_t irq_mis_count;
+
+#ifdef CONFIG_X86_LOCAL_APIC
 struct irq_data;
 struct pci_dev;
 struct msi_desc;
@@ -91,44 +90,8 @@ struct irq_cfg {
 	unsigned int		vector;
 };
 
-extern struct irq_cfg *irq_cfg(unsigned int irq);
-extern struct irq_cfg *irqd_cfg(struct irq_data *irq_data);
-extern void lock_vector_lock(void);
-extern void unlock_vector_lock(void);
-#ifdef CONFIG_SMP
-extern void send_cleanup_vector(struct irq_cfg *);
-extern void irq_complete_move(struct irq_cfg *cfg);
-#else
-static inline void send_cleanup_vector(struct irq_cfg *c) { }
-static inline void irq_complete_move(struct irq_cfg *c) { }
-#endif
+#endif /* CONFIG_X86_LOCAL_APIC */
 
-extern void apic_ack_edge(struct irq_data *data);
-#else	/*  CONFIG_X86_LOCAL_APIC */
-static inline void lock_vector_lock(void) {}
-static inline void unlock_vector_lock(void) {}
-#endif	/* CONFIG_X86_LOCAL_APIC */
-
-/* Statistics */
-extern atomic_t irq_err_count;
-extern atomic_t irq_mis_count;
-
-extern void elcr_set_level_irq(unsigned int irq);
-
-extern char irq_entries_start[];
-#ifdef CONFIG_TRACING
-#define trace_irq_entries_start irq_entries_start
-#endif
-
-extern char spurious_entries_start[];
-
-#define VECTOR_UNUSED		NULL
-#define VECTOR_SHUTDOWN		((void *)-1L)
-#define VECTOR_RETRIGGERED	((void *)-2L)
-
-typedef struct irq_desc* vector_irq_t[NR_VECTORS];
-DECLARE_PER_CPU(vector_irq_t, vector_irq);
-
-#endif /* !ASSEMBLY_ */
+#endif /* __ASSEMBLY__ */
 
 #endif /* _ASM_X86_HW_IRQ_H */
--- arch/x86/include/asm/x86_irq.h
+++ arch/x86/include/asm/x86_irq.h
@@ -1,1 +1,49 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_X86_X86_IRQ_H
+#define _ASM_X86_X86_IRQ_H
+
+#include <linux/irqdesc.h>
+#include <linux/percpu.h>
+
+#include <linux/smp.h>
+
+#include <linux/atomic.h>
+#include <asm/sections.h>
 #include <asm/hw_irq.h>
+
+#ifdef	CONFIG_X86_LOCAL_APIC
+extern struct irq_cfg *irq_cfg(unsigned int irq);
+extern struct irq_cfg *irqd_cfg(struct irq_data *irq_data);
+extern void lock_vector_lock(void);
+extern void unlock_vector_lock(void);
+#ifdef CONFIG_SMP
+extern void send_cleanup_vector(struct irq_cfg *);
+extern void irq_complete_move(struct irq_cfg *cfg);
+#else
+static inline void send_cleanup_vector(struct irq_cfg *c) { }
+static inline void irq_complete_move(struct irq_cfg *c) { }
+#endif
+
+extern void apic_ack_edge(struct irq_data *data);
+#else	/*  CONFIG_X86_LOCAL_APIC */
+static inline void lock_vector_lock(void) {}
+static inline void unlock_vector_lock(void) {}
+#endif	/* CONFIG_X86_LOCAL_APIC */
+
+extern void elcr_set_level_irq(unsigned int irq);
+
+extern char irq_entries_start[];
+#ifdef CONFIG_TRACING
+#define trace_irq_entries_start irq_entries_start
+#endif
+
+extern char spurious_entries_start[];
+
+#define VECTOR_UNUSED		NULL
+#define VECTOR_SHUTDOWN		((void *)-1L)
+#define VECTOR_RETRIGGERED	((void *)-2L)
+
+typedef struct irq_desc* vector_irq_t[NR_VECTORS];
+DECLARE_PER_CPU(vector_irq_t, vector_irq);
+
+#endif /* _ASM_X86_X86_IRQ_H */
