72f847c2f4c5e806565095a5355408f002adeac7
Ingo Molnar <mingo@kernel.org>
2021-08-02 18:25:23 +0200

    headers/deps: irq: Move the generic effective IRQ affinity methods into <linux/irq_api_eff_affinity.h>
    This decouples <linux/irq.h> from <linux/cpumask_api.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -891,36 +891,6 @@ static inline struct cpumask *irq_data_get_affinity_mask(struct irq_data *d)
 	return d->common->affinity;
 }
 
-#ifdef CONFIG_GENERIC_IRQ_EFFECTIVE_AFF_MASK
-static inline
-struct cpumask *irq_data_get_effective_affinity_mask(struct irq_data *d)
-{
-	return d->common->effective_affinity;
-}
-static inline void irq_data_update_effective_affinity(struct irq_data *d,
-						      const struct cpumask *m)
-{
-	cpumask_copy(d->common->effective_affinity, m);
-}
-#else
-static inline void irq_data_update_effective_affinity(struct irq_data *d,
-						      const struct cpumask *m)
-{
-}
-static inline
-struct cpumask *irq_data_get_effective_affinity_mask(struct irq_data *d)
-{
-	return d->common->affinity;
-}
-#endif
-
-static inline struct cpumask *irq_get_effective_affinity_mask(unsigned int irq)
-{
-	struct irq_data *d = irq_get_irq_data(irq);
-
-	return d ? irq_data_get_effective_affinity_mask(d) : NULL;
-}
-
 unsigned int arch_dynirq_lower_bound(unsigned int from);
 
 int __irq_alloc_descs(int irq, unsigned int from, unsigned int cnt, int node,
--- include/linux/irq_api_eff_affinity.h
+++ include/linux/irq_api_eff_affinity.h
@@ -1,1 +1,41 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_IRQ_API_EFF_AFFINITY_H
+#define _LINUX_IRQ_API_EFF_AFFINITY_H
+
 #include <linux/irq.h>
+
+#ifdef CONFIG_GENERIC_IRQ_EFFECTIVE_AFF_MASK
+# include <linux/cpumask_api.h>
+#endif
+
+#ifdef CONFIG_GENERIC_IRQ_EFFECTIVE_AFF_MASK
+static inline
+struct cpumask *irq_data_get_effective_affinity_mask(struct irq_data *d)
+{
+	return d->common->effective_affinity;
+}
+static inline void irq_data_update_effective_affinity(struct irq_data *d,
+						      const struct cpumask *m)
+{
+	cpumask_copy(d->common->effective_affinity, m);
+}
+#else
+static inline void irq_data_update_effective_affinity(struct irq_data *d,
+						      const struct cpumask *m)
+{
+}
+static inline
+struct cpumask *irq_data_get_effective_affinity_mask(struct irq_data *d)
+{
+	return d->common->affinity;
+}
+#endif
+
+static inline struct cpumask *irq_get_effective_affinity_mask(unsigned int irq)
+{
+	struct irq_data *d = irq_get_irq_data(irq);
+
+	return d ? irq_data_get_effective_affinity_mask(d) : NULL;
+}
+
+#endif /* _LINUX_IRQ_API_EFF_AFFINITY_H */
