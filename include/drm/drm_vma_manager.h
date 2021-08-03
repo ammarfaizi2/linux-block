80968e7fc6239371c5d8bc99b3ccfe84e985bffa
Ingo Molnar <mingo@kernel.org>
2021-08-03 09:12:13 +0200

    headers/deps: drm: Split <drm/drm_vma_manager_api_vm_lock.h> out of <drm/drm_vma_manager.h>
    <drm/drm_vma_manager.h> is included around 1,000 times in a typical distro build.
    
    Decouple <drm/drm_vma_manager.h> from <linux/spinlock_api.h> by
    moving the ->vm_lock methods to a new <drm/drm_vma_manager_api_vm_lock.h>
    header.
    
    These methods are only used in ~9 files, so this enables a
    big net reduction in <linux/spinlock_api.h> usage.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/drm/drm_vma_manager.h
+++ include/drm/drm_vma_manager.h
@@ -105,55 +105,6 @@ drm_vma_offset_exact_lookup_locked(struct drm_vma_offset_manager *mgr,
 }
 
 /**
- * drm_vma_offset_lock_lookup() - Lock lookup for extended private use
- * @mgr: Manager object
- *
- * Lock VMA manager for extended lookups. Only locked VMA function calls
- * are allowed while holding this lock. All other contexts are blocked from VMA
- * until the lock is released via drm_vma_offset_unlock_lookup().
- *
- * Use this if you need to take a reference to the objects returned by
- * drm_vma_offset_lookup_locked() before releasing this lock again.
- *
- * This lock must not be used for anything else than extended lookups. You must
- * not call any other VMA helpers while holding this lock.
- *
- * Note: You're in atomic-context while holding this lock!
- */
-static inline void drm_vma_offset_lock_lookup(struct drm_vma_offset_manager *mgr)
-{
-	read_lock(&mgr->vm_lock);
-}
-
-/**
- * drm_vma_offset_unlock_lookup() - Unlock lookup for extended private use
- * @mgr: Manager object
- *
- * Release lookup-lock. See drm_vma_offset_lock_lookup() for more information.
- */
-static inline void drm_vma_offset_unlock_lookup(struct drm_vma_offset_manager *mgr)
-{
-	read_unlock(&mgr->vm_lock);
-}
-
-/**
- * drm_vma_node_reset() - Initialize or reset node object
- * @node: Node to initialize or reset
- *
- * Reset a node to its initial state. This must be called before using it with
- * any VMA offset manager.
- *
- * This must not be called on an already allocated node, or you will leak
- * memory.
- */
-static inline void drm_vma_node_reset(struct drm_vma_offset_node *node)
-{
-	memset(node, 0, sizeof(*node));
-	node->vm_files = RB_ROOT;
-	rwlock_init(&node->vm_lock);
-}
-
-/**
  * drm_vma_node_start() - Return start address for page-based addressing
  * @node: Node to inspect
  *
