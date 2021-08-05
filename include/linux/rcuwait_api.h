5326a4dcb03acd01d29b8bb22da0fcba30d2999c
Ingo Molnar <mingo@kernel.org>
2021-08-05 09:37:12 +0200

    headers/deps: rcu: Split <linux/rcuwait.h> into <linux/rcuwait_types.h> and <linux/rcuwait_api.h>
    <linux/rcuwait.h> is included in <linux/fs.h> indirectly, which is a key header.
    
    In order to reduce the size of this header, we are splitting it into three parts:
    
     - <linux/rcuwait_types.h>: lightweight header with only type definitions
                                and initializers.
    
     - <linux/rcuwait_api.h>: API declarations and inline functions. This header
                              is inevitably more heavyweight.
    
     - <linux/rcuwait.h>: Simple default header that right now includes both
                          the types and the API headers, but will only
                          use the types header once all affected code
                          is converted.
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
    	  | #include <linux/rcuwait_api.h>          | LOC:  3,375 | headers:  153
              | #include <linux/rcuwait_types.h>        | LOC:     32 | headers:   19
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    # Conflicts:
    #	include/linux/rcuwait_types.h

--- include/linux/rcuwait_api.h
+++ include/linux/rcuwait_api.h
@@ -1,1 +1,57 @@
-#include <linux/rcuwait.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_RCUWAIT_API_H_
+#define _LINUX_RCUWAIT_API_H_
+
+#include <linux/rcuwait_types.h>
+
+#include <linux/rcupdate.h>
+
+/*
+ * Note: this provides no serialization and, just as with waitqueues,
+ * requires care to estimate as to whether or not the wait is active.
+ */
+static inline int rcuwait_active(struct rcuwait *w)
+{
+	return !!rcu_access_pointer(w->task);
+}
+
+extern int rcuwait_wake_up(struct rcuwait *w);
+
+/*
+ * The caller is responsible for locking around rcuwait_wait_event(),
+ * and [prepare_to/finish]_rcuwait() such that writes to @task are
+ * properly serialized.
+ */
+
+static inline void prepare_to_rcuwait(struct rcuwait *w)
+{
+	rcu_assign_pointer(w->task, current);
+}
+
+extern void finish_rcuwait(struct rcuwait *w);
+
+#define rcuwait_wait_event(w, condition, state)				\
+({									\
+	int __ret = 0;							\
+	prepare_to_rcuwait(w);						\
+	for (;;) {							\
+		/*							\
+		 * Implicit barrier (A) pairs with (B) in		\
+		 * rcuwait_wake_up().					\
+		 */							\
+		set_current_state(state);				\
+		if (condition)						\
+			break;						\
+									\
+		if (signal_pending_state(state, current)) {		\
+			__ret = -EINTR;					\
+			break;						\
+		}							\
+									\
+		schedule();						\
+	}								\
+	finish_rcuwait(w);						\
+	__ret;								\
+})
+
+#endif /* _LINUX_RCUWAIT_API_H_ */
