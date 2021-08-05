9ff0368c1c61407317e5433af99cecf8ada7a847
Ingo Molnar <mingo@kernel.org>
2021-08-05 09:36:05 +0200

    headers/deps: time: Move <linux/ktime.h> to <linux/ktime_types.h>
    Use the standard triple-header splitup:
    
     - <linux/ktime_types.h>: lightweight header with only type definitions
                              and initializers.
    
     - <linux/ktime_api.h>: API declarations and inline functions. This header
                            is inevitably more heavyweight.
    
     - <linux/ktime.h>: Simple default header, that maps to <linux/ktime_types.h>.
    
    No change in header behavior.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    # Conflicts:
    #	include/linux/ktime_types.h

--- include/linux/ktime.h
+++ include/linux/ktime.h
@@ -1,30 +1,7 @@
-/*
- *  include/linux/ktime.h
- *
- *  ktime_t - nanosecond-resolution time format.
- *
- *   Copyright(C) 2005, Thomas Gleixner <tglx@linutronix.de>
- *   Copyright(C) 2005, Red Hat, Inc., Ingo Molnar
- *
- *  data type definitions, declarations, prototypes and macros.
- *
- *  Started by: Thomas Gleixner and Ingo Molnar
- *
- *  Credits:
- *
- *  	Roman Zippel provided the ideas and primary code snippets of
- *  	the ktime_t union and further simplifications of the original
- *  	code.
- *
- *  For licencing details see kernel-base/COPYING
- */
-#ifndef _LINUX_KTIME_H
-#define _LINUX_KTIME_H
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __LINUX_KTIME_H
+#define __LINUX_KTIME_H
 
-#include <linux/types.h>
-#include <vdso/ktime.h>
+#include <linux/ktime_types.h>
 
-/* Nanosecond scalar representation for kernel time values */
-typedef s64	ktime_t;
-
-#endif
+#endif /* __LINUX_KTIME_H */
