74f0f95d3dab5141a31e330ae14d4b3afe54ee93
Ingo Molnar <mingo@kernel.org>
2021-08-05 09:37:15 +0200

    headers/deps: net: Split <linux/rcu_sync.h> into <linux/rcu_sync_types.h> and <linux/rcu_sync_api.h>
    <linux/rcu_sync.h> is a medium size header, included in <linux/fs.h>, which
    is a key header we want to reduce the size of.
    
    In order to reduce its impact, we are splitting it into three parts:
    
     - <linux/rcu_sync_types.h>: lightweight header with only type definitions
                                 and initializers.
    
     - <linux/rcu_sync_api.h>: API declarations and inline functions. This header
                               is inevitably more heavyweight.
    
     - <linux/rcu_sync.h>: Simple default header that right now includes both
                           the types and the API headers, but will only
                           use the types header once all affected code
                           is converted.
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    # Conflicts:
    #	include/linux/rcu_sync_types.h

--- include/linux/rcu_sync_api.h
+++ include/linux/rcu_sync_api.h
@@ -1,1 +1,36 @@
-#include <linux/rcu_sync.h>
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * RCU-based infrastructure for lightweight reader-writer locking
+ *
+ * Copyright (c) 2015, Red Hat, Inc.
+ *
+ * Author: Oleg Nesterov <oleg@redhat.com>
+ */
+
+#ifndef _LINUX_RCU_SYNC_API_H_
+#define _LINUX_RCU_SYNC_API_H_
+
+#include <linux/rcu_sync_types.h>
+
+#include <linux/rcupdate.h>
+
+/**
+ * rcu_sync_is_idle() - Are readers permitted to use their fastpaths?
+ * @rsp: Pointer to rcu_sync structure to use for synchronization
+ *
+ * Returns true if readers are permitted to use their fastpaths.  Must be
+ * invoked within some flavor of RCU read-side critical section.
+ */
+static inline bool rcu_sync_is_idle(struct rcu_sync *rsp)
+{
+	RCU_LOCKDEP_WARN(!rcu_read_lock_any_held(),
+			 "suspicious rcu_sync_is_idle() usage");
+	return !READ_ONCE(rsp->gp_state); /* GP_IDLE */
+}
+
+extern void rcu_sync_enter_start(struct rcu_sync *);
+extern void rcu_sync_enter(struct rcu_sync *);
+extern void rcu_sync_exit(struct rcu_sync *);
+extern void rcu_sync_dtor(struct rcu_sync *);
+
+#endif /* _LINUX_RCU_SYNC_API_H_ */
--- include/linux/rcu_sync_types.h
+++ include/linux/rcu_sync_types.h
@@ -1,1 +1,35 @@
-#include <linux/rcu_sync.h>
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * RCU-based infrastructure for lightweight reader-writer locking
+ *
+ * Copyright (c) 2015, Red Hat, Inc.
+ *
+ * Author: Oleg Nesterov <oleg@redhat.com>
+ */
+
+#ifndef _LINUX_RCU_SYNC_TYPES_H_
+#define _LINUX_RCU_SYNC_TYPES_H_
+
+#include <linux/wait_types.h>
+
+/* Structure to mediate between updaters and fastpath-using readers.  */
+struct rcu_sync {
+	int			gp_state;
+	int			gp_count;
+	wait_queue_head_t	gp_wait;
+
+	struct rcu_head		cb_head;
+};
+
+#define __RCU_SYNC_INITIALIZER(name) {					\
+		.gp_state = 0,						\
+		.gp_count = 0,						\
+		.gp_wait = __WAIT_QUEUE_HEAD_INITIALIZER(name.gp_wait),	\
+	}
+
+#define	DEFINE_RCU_SYNC(name)	\
+	struct rcu_sync name = __RCU_SYNC_INITIALIZER(name)
+
+extern void rcu_sync_init(struct rcu_sync *);
+
+#endif /* _LINUX_RCU_SYNC_TYPES_H_ */
