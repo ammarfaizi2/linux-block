3082b22640114e6d815e0bba55455516de5be2f4
Ingo Molnar <mingo@kernel.org>
2021-08-05 09:36:16 +0200

    headers/deps: kref: Split <linux/kref.h> into <linux/kref_types.h> and <linux/kref_api.h>
    <linux/kref.h> is a medium header, included in a lot of other headers
    in a spurious fashion:
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              |   #include <linux/kref.h>                        | LOC:  3162 | headers:  86 |
    
    In order to reduce this impact, we are going to split the header into
    three parts:
    
     - <linux/kref_types.h>: lightweight header with only type definitions
                                        and initializers.
    
     - <linux/kref_api.h>: API declarations and inline functions. This header
                                      is inevitably more heavyweight.
    
     - <linux/kref.h>: Simple default header that right now includes both
                         the types and the API headers, but will only
                         use the types header once all affected code
                         is converted.
    
    The potential size reduction for headers using <linux/kref_types.h> is significant:
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
    	  | #include <linux/kref_api.h>                    | LOC:  3162 | headers:  86 |
    	  | #include <linux/kref_types.h>                  | LOC:   183 | headers:  26 |
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    # Conflicts:
    #	include/linux/kref_types.h

--- include/linux/kref_types.h
+++ include/linux/kref_types.h
@@ -1,1 +1,14 @@
-#include <linux/kref.h>
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _KREF_TYPES_H
+#define _KREF_TYPES_H
+
+#include <linux/refcount_types.h>
+
+struct kref {
+	refcount_t refcount;
+};
+
+#define KREF_INIT(n)	{ .refcount = REFCOUNT_INIT(n), }
+
+#endif /* _KREF_TYPES_H */
