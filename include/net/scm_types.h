45166de4db04a8941726599ca484afb10bd114a5
Ingo Molnar <mingo@kernel.org>
2021-08-05 09:36:53 +0200

    headers/deps: net: Split <net/scm.h> into <net/scm_types.h> and <net/scm_api.h>
    <net/scm.h> is included over 5,500 times in other headers.
    
    In order to reduce its impact, we are splitting it into three parts:
    
     - <net/scm_types.h>: lightweight header with only type definitions
                          and initializers.
    
     - <net/scm_api.h>: API declarations and inline functions. This header
                        is inevitably more heavyweight.
    
     - <net/scm.h>: Simple default header that right now includes both
                    the types and the API headers, but will only
                    use the types header once all affected code
                    is converted.
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    # Conflicts:
    #	include/net/scm_api.h
    #	include/net/scm_types.h

--- include/net/scm_types.h
+++ include/net/scm_types.h
@@ -1,1 +1,14 @@
-#include <net/scm.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __LINUX_NET_SCM_TYPES_H
+#define __LINUX_NET_SCM_TYPES_H
+
+#include <linux/uidgid.h>
+
+struct scm_creds {
+	u32	pid;
+	kuid_t	uid;
+	kgid_t	gid;
+};
+
+#endif /* __LINUX_NET_SCM_TYPES_H */
+
