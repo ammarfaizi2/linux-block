67a2955b1e6133a605d92ab6e47ba25b35e18e15
Ingo Molnar <mingo@kernel.org>
2021-08-05 09:36:17 +0200

    headers/deps: time: Move <linux/timerqueue.h> to <linux/timerqueue_types.h>
    Use the standard triple-header splitup:
    
     - <linux/timerqueue_types.h>: lightweight header with only type definitions
                                   and initializers.
    
     - <linux/timerqueue_api.h>: API declarations and inline functions. This header
                                 is inevitably more heavyweight.
    
     - <linux/timerqueue.h>: Simple default header, that maps to <linux/timerqueue_types.h>.
    
    No change in header behavior.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    # Conflicts:
    #	include/linux/timerqueue_types.h

--- include/linux/timerqueue.h
+++ include/linux/timerqueue.h
@@ -2,16 +2,6 @@
 #ifndef _LINUX_TIMERQUEUE_H
 #define _LINUX_TIMERQUEUE_H
 
-#include <linux/rbtree_types.h>
-#include <linux/ktime.h>
-
-struct timerqueue_node {
-	struct rb_node node;
-	ktime_t expires;
-};
-
-struct timerqueue_head {
-	struct rb_root_cached rb_root;
-};
+#include <linux/timerqueue_types.h>
 
 #endif /* _LINUX_TIMERQUEUE_H */
