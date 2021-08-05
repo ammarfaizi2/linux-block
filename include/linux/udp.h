456c4d0cdc225c8923d229a027ed85a69bd79d12
Ingo Molnar <mingo@kernel.org>
2021-08-05 09:37:57 +0200

    headers/deps: net: Split <linux/udp_api.h> out of <linux/udp.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    # Conflicts:
    #	include/linux/udp_api.h

--- include/linux/udp.h
+++ include/linux/udp.h
@@ -121,31 +121,6 @@ static inline bool udp_get_no_check6_rx(struct sock *sk)
 	return udp_sk(sk)->no_check6_rx;
 }
 
-static inline void udp_cmsg_recv(struct msghdr *msg, struct sock *sk,
-				 struct sk_buff *skb)
-{
-	int gso_size;
-
-	if (skb_shinfo(skb)->gso_type & SKB_GSO_UDP_L4) {
-		gso_size = skb_shinfo(skb)->gso_size;
-		put_cmsg(msg, SOL_UDP, UDP_GRO, sizeof(gso_size), &gso_size);
-	}
-}
-
-static inline bool udp_unexpected_gso(struct sock *sk, struct sk_buff *skb)
-{
-	if (!skb_is_gso(skb))
-		return false;
-
-	if (skb_shinfo(skb)->gso_type & SKB_GSO_UDP_L4 && !udp_sk(sk)->accept_udp_l4)
-		return true;
-
-	if (skb_shinfo(skb)->gso_type & SKB_GSO_FRAGLIST && !udp_sk(sk)->accept_udp_fraglist)
-		return true;
-
-	return false;
-}
-
 static inline void udp_allow_gso(struct sock *sk)
 {
 	udp_sk(sk)->accept_udp_l4 = 1;
