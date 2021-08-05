bb09cadfa0f047d464f9e03ea848b61d491daa60
Ingo Molnar <mingo@kernel.org>
2021-08-05 09:36:13 +0200

    headers/deps: locking/refcount: Split <linux/refcount.h> into <linux/refcount_types.h> and <linux/refcount_api.h>
    <linux/refcount.h> is included in a lot of other headers:
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
    	  | #include <linux/refcount.h>                    | LOC:  3115 | headers:  82 |
    
    In order to reduce this impact, we are going to split the header into
    three parts:
    
     - <linux/refcount_types.h>: lightweight header with only type definitions
                                        and initializers.
    
     - <linux/refcount_api.h>: API declarations and inline functions. This header
                                      is inevitably more heavyweight.
    
     - <linux/refcount.h>: Simple default header that right now includes both
                         the types and the API headers, but will only
                         use the types header once all affected code
                         is converted.
    
    The size and complexity difference is significant:
    
    	  | #include <linux/refcount_api.h>                | LOC:  3115 | headers:  82 |
    	  | #include <linux/refcount_types.h>              | LOC:   180 | headers:  25 |
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    # Conflicts:
    #	include/linux/refcount_types.h

--- include/linux/refcount_types.h
+++ include/linux/refcount_types.h
@@ -1,1 +1,21 @@
-#include <linux/refcount.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_REFCOUNT_TYPES_H
+#define _LINUX_REFCOUNT_TYPES_H
+
+#include <linux/types.h>
+
+/**
+ * typedef refcount_t - variant of atomic_t specialized for reference counts
+ * @refs: atomic_t counter field
+ *
+ * The counter saturates at REFCOUNT_SATURATED and will not move once
+ * there. This avoids wrapping the counter and causing 'spurious'
+ * use-after-free bugs.
+ */
+typedef struct refcount_struct {
+	atomic_t refs;
+} refcount_t;
+
+#define REFCOUNT_INIT(n)	{ .refs = ATOMIC_INIT(n), }
+
+#endif /* _LINUX_REFCOUNT_TYPES_H */
