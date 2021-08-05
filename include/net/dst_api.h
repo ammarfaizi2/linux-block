410ec9f9533306f86b86334db31e0a816f98bbf6
Ingo Molnar <mingo@kernel.org>
2021-08-05 11:11:42 +0200

    headers/deps: net: Move *skb_tunnel_rx() from <net/dst_api.h> to <net/dst_api_tunnel.h>
    This decouples <net/dst_api.h> from <linux/netdevice.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    # Conflicts:
    #	include/net/dst_api_tunnel.h

--- include/net/dst_api.h
+++ include/net/dst_api.h
@@ -268,49 +268,6 @@ static inline bool skb_dst_force(struct sk_buff *skb)
 }
 
 
-/**
- *	__skb_tunnel_rx - prepare skb for rx reinsert
- *	@skb: buffer
- *	@dev: tunnel device
- *	@net: netns for packet i/o
- *
- *	After decapsulation, packet is going to re-enter (netif_rx()) our stack,
- *	so make some cleanups. (no accounting done)
- */
-static inline void __skb_tunnel_rx(struct sk_buff *skb, struct net_device *dev,
-				   struct net *net)
-{
-	skb->dev = dev;
-
-	/*
-	 * Clear hash so that we can recalulate the hash for the
-	 * encapsulated packet, unless we have already determine the hash
-	 * over the L4 4-tuple.
-	 */
-	skb_clear_hash_if_not_l4(skb);
-	skb_set_queue_mapping(skb, 0);
-	skb_scrub_packet(skb, !net_eq(net, dev_net(dev)));
-}
-
-/**
- *	skb_tunnel_rx - prepare skb for rx reinsert
- *	@skb: buffer
- *	@dev: tunnel device
- *	@net: netns for packet i/o
- *
- *	After decapsulation, packet is going to re-enter (netif_rx()) our stack,
- *	so make some cleanups, and perform accounting.
- *	Note: this accounting is not SMP safe.
- */
-static inline void skb_tunnel_rx(struct sk_buff *skb, struct net_device *dev,
-				 struct net *net)
-{
-	/* TODO : stats should be SMP safe */
-	dev->stats.rx_packets++;
-	dev->stats.rx_bytes += skb->len;
-	__skb_tunnel_rx(skb, dev, net);
-}
-
 static inline u32 dst_tclassid(const struct sk_buff *skb)
 {
 #ifdef CONFIG_IP_ROUTE_CLASSID
