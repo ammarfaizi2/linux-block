8e5f5e1a9dfea2306dbb39b7604e48a3b26fbbe7
Ingo Molnar <mingo@kernel.org>
2021-08-05 09:37:14 +0200

    headers/deps: net: Split <linux/percpu_rwsem.h> into <linux/percpu_rwsem_types.h> and <linux/percpu_rwsem_api.h>
    <linux/percpu_rwsem.h> is a medium size header, included in <linux/fs.h>, which
    is a key header we want to reduce the size of.
    
    In order to reduce this impact, we are splitting the header into three parts:
    
     - <linux/percpu_rwsem_types.h>: lightweight header with only type definitions
                                     and initializers.
    
     - <linux/percpu_rwsem_api.h>: API declarations and inline functions. This header
                                   is inevitably more heavyweight.
    
     - <linux/percpu_rwsem.h>: Simple default header that right now includes both
                               the types and the API headers, but will only
                               use the types header once all affected code
                               is converted.
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    # Conflicts:
    #	include/linux/percpu_rwsem_types.h

--- include/linux/percpu_rwsem_types.h
+++ include/linux/percpu_rwsem_types.h
@@ -1,1 +1,54 @@
-#include <linux/percpu_rwsem.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_PERCPU_RWSEM_TYPES_H
+#define _LINUX_PERCPU_RWSEM_TYPES_H
+
+#include <linux/atomic.h>
+#include <linux/percpu.h>
+#include <linux/rcuwait.h>
+#include <linux/wait.h>
+#include <linux/rcu_sync.h>
+#include <linux/lockdep.h>
+
+struct percpu_rw_semaphore {
+	struct rcu_sync		rss;
+	unsigned int __percpu	*read_count;
+	struct rcuwait		writer;
+	wait_queue_head_t	waiters;
+	atomic_t		block;
+#ifdef CONFIG_DEBUG_LOCK_ALLOC
+	struct lockdep_map	dep_map;
+#endif
+};
+
+#ifdef CONFIG_DEBUG_LOCK_ALLOC
+#define __PERCPU_RWSEM_DEP_MAP_INIT(lockname)	.dep_map = { .name = #lockname },
+#else
+#define __PERCPU_RWSEM_DEP_MAP_INIT(lockname)
+#endif
+
+#define __DEFINE_PERCPU_RWSEM(name, is_static)				\
+static DEFINE_PER_CPU(unsigned int, __percpu_rwsem_rc_##name);		\
+is_static struct percpu_rw_semaphore name = {				\
+	.rss = __RCU_SYNC_INITIALIZER(name.rss),			\
+	.read_count = &__percpu_rwsem_rc_##name,			\
+	.writer = __RCUWAIT_INITIALIZER(name.writer),			\
+	.waiters = __WAIT_QUEUE_HEAD_INITIALIZER(name.waiters),		\
+	.block = ATOMIC_INIT(0),					\
+	__PERCPU_RWSEM_DEP_MAP_INIT(name)				\
+}
+
+#define DEFINE_PERCPU_RWSEM(name)		\
+	__DEFINE_PERCPU_RWSEM(name, /* not static */)
+#define DEFINE_STATIC_PERCPU_RWSEM(name)	\
+	__DEFINE_PERCPU_RWSEM(name, static)
+
+extern int __percpu_init_rwsem(struct percpu_rw_semaphore *,
+				const char *, struct lock_class_key *);
+
+#define percpu_init_rwsem(sem)					\
+({								\
+	static struct lock_class_key rwsem_key;			\
+	__percpu_init_rwsem(sem, #sem, &rwsem_key);		\
+})
+
+#endif /* _LINUX_PERCPU_RWSEM_TYPES_H */
