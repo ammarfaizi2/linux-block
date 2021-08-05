1ac747caf17c6894820e3f9f551058bfed48bea4
Ingo Molnar <mingo@kernel.org>
2021-08-05 09:36:52 +0200

    headers/deps: net/dql: Split <linux/dynamic_queue_limits.h> into <linux/dynamic_queue_limits_types.h> and <linux/dynamic_queue_limits_api.h>
    <linux/dynamic_queue_limits.h> is included in a lot of other headers.
    
    In order to reduce the impact a bit, we are splitting it into three parts:
    
     - <linux/dynamic_queue_limits_types.h>: lightweight header with only type definitions
                                             and initializers.
    
     - <linux/dynamic_queue_limits_api.h>: API declarations and inline functions. This header
                                           is inevitably more heavyweight.
    
     - <linux/dynamic_queue_limits.h>: Simple default header that right now includes both
                                       the types and the API headers, but will only
                                       use the types header once all affected code
                                       is converted.
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              | #include <linux/dynamic_queue_limits_api.h>   | LOC:   751 | headers:   56
              | #include <linux/dynamic_queue_limits_types.h> | LOC:   263 | headers:   41
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    # Conflicts:
    #	include/linux/dynamic_queue_limits_api.h
    #	include/linux/dynamic_queue_limits_types.h

--- include/linux/dynamic_queue_limits_types.h
+++ include/linux/dynamic_queue_limits_types.h
@@ -1,1 +1,31 @@
-#include <linux/dynamic_queue_limits.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_DQL_TYPES_H
+#define _LINUX_DQL_TYPES_H
+
+#include <linux/cache.h>
+
+struct dql {
+	/* Fields accessed in enqueue path (dql_queued) */
+	unsigned int	num_queued;		/* Total ever queued */
+	unsigned int	adj_limit;		/* limit + num_completed */
+	unsigned int	last_obj_cnt;		/* Count at last queuing */
+
+	/* Fields accessed only by completion path (dql_completed) */
+
+	unsigned int	limit ____cacheline_aligned_in_smp; /* Current limit */
+	unsigned int	num_completed;		/* Total ever completed */
+
+	unsigned int	prev_ovlimit;		/* Previous over limit */
+	unsigned int	prev_num_queued;	/* Previous queue total */
+	unsigned int	prev_last_obj_cnt;	/* Previous queuing cnt */
+
+	unsigned int	lowest_slack;		/* Lowest slack found */
+	unsigned long	slack_start_time;	/* Time slacks seen */
+
+	/* Configuration */
+	unsigned int	max_limit;		/* Max limit */
+	unsigned int	min_limit;		/* Minimum limit */
+	unsigned int	slack_hold_time;	/* Time to measure slack */
+};
+
+#endif /* _LINUX_DQL_TYPES_H */
