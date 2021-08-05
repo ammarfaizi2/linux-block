d37f2d679dfdfae12c10a8013987bb29b99e59c4
Ingo Molnar <mingo@kernel.org>
2021-08-05 09:37:12 +0200

    headers/deps: rcu: Split <linux/rcuwait.h> into <linux/rcuwait_types.h> and <linux/rcuwait_api.h>
    <linux/rcuwait.h> is included in <linux/fs.h> indirectly, which is a key header.
    
    In order to reduce the size of this header, we are splitting it into three parts:
    
     - <linux/rcuwait_types.h>: lightweight header with only type definitions
                                and initializers.
    
     - <linux/rcuwait_api.h>: API declarations and inline functions. This header
                              is inevitably more heavyweight.
    
     - <linux/rcuwait.h>: Simple default header that right now includes both
                          the types and the API headers, but will only
                          use the types header once all affected code
                          is converted.
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
    	  | #include <linux/rcuwait_api.h>          | LOC:  3,375 | headers:  153
              | #include <linux/rcuwait_types.h>        | LOC:     32 | headers:   19
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    # Conflicts:
    #	include/linux/rcuwait_types.h

--- include/linux/rcuwait_types.h
+++ include/linux/rcuwait_types.h
@@ -1,1 +1,30 @@
-#include <linux/rcuwait.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_RCUWAIT_TYPES_H_
+#define _LINUX_RCUWAIT_TYPES_H_
+
+#include <linux/stddef.h>
+#include <linux/compiler_types.h>
+
+struct task_struct;
+
+/*
+ * rcuwait provides a way of blocking and waking up a single
+ * task in an rcu-safe manner.
+ *
+ * The only time @task is non-nil is when a user is blocked (or
+ * checking if it needs to) on a condition, and reset as soon as we
+ * know that the condition has succeeded and are awoken.
+ */
+struct rcuwait {
+	struct task_struct __rcu *task;
+};
+
+#define __RCUWAIT_INITIALIZER(name)		\
+	{ .task = NULL, }
+
+static inline void rcuwait_init(struct rcuwait *w)
+{
+	w->task = NULL;
+}
+
+#endif /* _LINUX_RCUWAIT_TYPES_H_ */
