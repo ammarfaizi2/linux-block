36832a1142374cda505f1f590b6e64f678e6c846
Ingo Molnar <mingo@kernel.org>
2021-08-06 05:40:55 +0200

    headers/deps: mm: Optimize <linux/mm_api.h> dependencies, remove <linux/percpu-refcount.h> inclusion
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
      before: | #include <linux/mm_api.h>               | LOC: 11,557 | headers:  312
       after: | #include <linux/mm_api.h>               | LOC: 10,910 | headers:  298
    
    Fix up dangling dependencies.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/mm_api.h
+++ include/linux/mm_api.h
@@ -38,7 +38,6 @@
 #include <linux/mmap_lock.h>
 #include <linux/range.h>
 #include <linux/pfn.h>
-#include <linux/percpu-refcount.h>
 #include <linux/bit_spinlock.h>
 #include <linux/shrinker.h>
 #include <linux/resource.h>
