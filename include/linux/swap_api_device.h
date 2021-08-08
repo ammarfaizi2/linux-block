8e037d31f5606909a4b2cdd6d7733d223e6d5a1a
Ingo Molnar <mingo@kernel.org>
2021-08-08 13:16:54 +0200

    headers/deps: mm: Move put_swap_device() into its separate header in <linux/swap_api_device.h>
    This decouples <linux/swap.h> from <linux/percpu-refcount-api.h>
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/swap_api_device.h
+++ include/linux/swap_api_device.h
@@ -1,1 +1,24 @@
-#include <linux/skbuff_api.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_SWAP_API_DEVICE_H
+#define _LINUX_SWAP_API_DEVICE_H
+
+#include <linux/swap.h>
+
+#include <linux/percpu-refcount-api.h>
+
+#ifdef CONFIG_SWAP
+
+static inline void put_swap_device(struct swap_info_struct *si)
+{
+	percpu_ref_put(&si->users);
+}
+
+#else /* CONFIG_SWAP */
+
+static inline void put_swap_device(struct swap_info_struct *si)
+{
+}
+
+#endif /* CONFIG_SWAP */
+
+#endif /* _LINUX_SWAP_API_DEVICE_H */
