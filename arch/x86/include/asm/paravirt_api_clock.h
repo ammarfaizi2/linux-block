fd2f75c68c5e75f15d4786215c079819c8708148
Ingo Molnar <mingo@kernel.org>
2021-08-08 13:17:16 +0200

    headers/deps: x86/paravirt: Split sched_clock/steal_clock methods from <asm/paravirt.h>, into <asm/paravirt_api_clock.h>
    This decouples <asm/paravirt.h> from <linux/static_call_types.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/paravirt_api_clock.h
+++ arch/x86/include/asm/paravirt_api_clock.h
@@ -1,1 +1,27 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_X86_PARAVIRT_API_CLOCK_H
+#define _ASM_X86_PARAVIRT_API_CLOCK_H
+
 #include <asm/paravirt.h>
+
+#include <linux/static_call_types.h>
+
+u64 dummy_steal_clock(int cpu);
+u64 dummy_sched_clock(void);
+
+DECLARE_STATIC_CALL(pv_steal_clock, dummy_steal_clock);
+DECLARE_STATIC_CALL(pv_sched_clock, dummy_sched_clock);
+
+void paravirt_set_sched_clock(u64 (*func)(void));
+
+static inline u64 paravirt_sched_clock(void)
+{
+	return static_call(pv_sched_clock)();
+}
+
+static inline u64 paravirt_steal_clock(int cpu)
+{
+	return static_call(pv_steal_clock)(cpu);
+}
+
+#endif /* _ASM_X86_PARAVIRT_API_CLOCK_H */
