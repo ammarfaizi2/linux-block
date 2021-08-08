d08d6c6495c32e048b98f60cafb27feb58c76e01
Ingo Molnar <mingo@kernel.org>
2021-08-08 13:11:03 +0200

    headers/prep: arm64: Move the ARCH_LOW_ADDRESS_LIMIT definition from <asm/processor.h> to <asm/dma.h>
    <linux/memblock.h> expects it in <asm/dma.h>.
    
    Arm64 only defined this value by accident through the old <linux/mm.h> dependencies
    that <linux/memblock.h> included - but these will go away soon.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/arm64/include/asm/dma.h
+++ arch/arm64/include/asm/dma.h
@@ -1,1 +1,19 @@
-#include <asm/processor.h>
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __ASM_DMA_H
+#define __ASM_DMA_H
+
+#ifndef __ASSEMBLY__
+
+#include <linux/types.h>
+
+extern phys_addr_t arm64_dma_phys_limit;
+#define ARCH_LOW_ADDRESS_LIMIT	(arm64_dma_phys_limit - 1)
+
+#define MAX_DMA_ADDRESS PAGE_OFFSET
+
+extern int request_dma(unsigned int dmanr, const char *device_id);
+extern void free_dma(unsigned int dmanr);
+
+#endif /* __ASSEMBLY__ */
+
+#endif /* __ASM_DMA_H */
