2bb04fdf3746ac005ab1f8e9828caf07eb119796
Ingo Molnar <mingo@kernel.org>
2021-08-11 11:15:25 +0200

    headers/deps: pci/x86: Optimize <asm/pci.h> dependencies
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
      before: | #include <linux/pci.h>                  | LOC: 10,120 | headers:  298
       after: | #include <linux/pci.h>                  | LOC:  9,163 | headers:  274
    
    Fix up dangling dependencies.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/pci.h
+++ arch/x86/include/asm/pci.h
@@ -2,16 +2,7 @@
 #ifndef _ASM_X86_PCI_H
 #define _ASM_X86_PCI_H
 
-#include <linux/topology.h>
-#include <linux/mm.h> /* for struct page */
-#include <linux/types.h>
-#include <linux/slab.h>
-#include <linux/string.h>
-#include <linux/scatterlist.h>
-#include <linux/numa.h>
-#include <asm/io.h>
 #include <asm/memtype.h>
-#include <asm/x86_init.h>
 
 struct pci_sysdata {
 	int		domain;		/* PCI domain */
