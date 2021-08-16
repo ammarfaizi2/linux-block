0d6dc8f1c8a0fced48ab0576c7b280df2b084042
Ingo Molnar <mingo@kernel.org>
2021-08-17 00:11:45 +0200

    headers/deps: locking/local64, x86: Split <asm/local64.h> into <asm/local64_types.h> and <asm/local64_api.h>
    <asm/local64.h> is a medium size header, included in a lot of other headers.
    
    In order to reduce this impact, we are splitting it into three parts:
    
     - <asm/local64_types.h>: lightweight header with only type definitions
                              and initializers.
    
     - <asm/local64_api.h>: API declarations and inline functions. This header
                            is inevitably more heavyweight.
    
     - <asm/local64.h>: Simple default header that right now includes both
                        the types and the API headers, but will only
                        use the types header once all affected code
                        is converted.
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              |
              |
    
    Also add an asm-generic placeholder for other architectures.
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/asm-generic/local64_types.h
+++ include/asm-generic/local64_types.h
@@ -1,1 +1,30 @@
-#include <asm-generic/local64.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_GENERIC_LOCAL64_TYPES_H
+#define _ASM_GENERIC_LOCAL64_TYPES_H
+
+#include <asm/bitsperlong.h>
+
+#if BITS_PER_LONG == 64
+
+#include <asm/local_types.h>
+
+typedef struct {
+	local_t a;
+} local64_t;
+
+#define LOCAL64_INIT(i)	{ LOCAL_INIT(i) }
+
+#else /* BITS_PER_LONG != 64 */
+
+#include <linux/atomic_api.h>
+
+/* Don't use typedef: don't want them to be mixed with atomic_t's. */
+typedef struct {
+	atomic64_t a;
+} local64_t;
+
+#define LOCAL64_INIT(i)	{ ATOMIC_LONG_INIT(i) }
+
+#endif /* BITS_PER_LONG != 64 */
+
+#endif /* _ASM_GENERIC_LOCAL64_TYPES_H */
