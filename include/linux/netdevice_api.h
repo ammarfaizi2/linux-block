39ce65d104383fa9af06fbbb40d97c553129171d
Ingo Molnar <mingo@kernel.org>
2021-08-20 13:59:17 +0200

    headers/deps: net: Optimize <linux/netdevice_api.h> dependencies, remove <linux/prefetch.h> inclusion
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
      before: | #include <linux/netdevice_api.h>        | LOC: 21,897 | headers:  416
       after: | #include <linux/netdevice_api.h>        | LOC: 21,604 | headers:  412
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/netdevice_api.h
+++ include/linux/netdevice_api.h
@@ -25,7 +25,6 @@
 
 #include <linux/dev_printk.h>
 #include <linux/pm_wakeup.h>
-#include <linux/prefetch.h>
 #include <linux/cpumask_types.h>
 #include <linux/spinlock_api.h>
 #include <linux/lockdep_api.h>
