748b24740d4a12cc7184822df26b3ee7adeffb17
Ingo Molnar <mingo@kernel.org>
2021-08-23 11:53:21 +0200

    headers/deps: locking: Split <linux/osq_lock.h> into <linux/osq_lock_types.h> and <linux/osq_lock_api.h>
    <linux/osq_lock.h> is included in a lot of other headers.
    
    In order to reduce this impact, we are splitting it into three parts:
    
     - <linux/osq_lock_types.h>: lightweight header with only type definitions
                                 and initializers.
    
     - <linux/osq_lock_api.h>: API declarations and inline functions. This header
                               is a bit larger.
    
     - <linux/osq_lock.h>: Simple default header that right now includes both
                           the types and the API headers, but will only
                           use the types header once all affected code
                           is converted.
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              | #include <linux/osq_lock_api.h>         | LOC: 2,306 | headers:   53
    	  | #include <linux/osq_lock_types.h>       | LOC:   184 | headers:   25
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/osq_lock_api.h
+++ include/linux/osq_lock_api.h
@@ -1,1 +1,22 @@
-#include <linux/osq_lock.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __LINUX_OSQ_LOCK_API_H
+#define __LINUX_OSQ_LOCK_API_H
+
+#include <linux/osq_lock_types.h>
+
+#include <linux/atomic_api.h>
+
+static inline void osq_lock_init(struct optimistic_spin_queue *lock)
+{
+	atomic_set(&lock->tail, OSQ_UNLOCKED_VAL);
+}
+
+extern bool osq_lock(struct optimistic_spin_queue *lock);
+extern void osq_unlock(struct optimistic_spin_queue *lock);
+
+static inline bool osq_is_locked(struct optimistic_spin_queue *lock)
+{
+	return atomic_read(&lock->tail) != OSQ_UNLOCKED_VAL;
+}
+
+#endif /* __LINUX_OSQ_LOCK_API_H */
