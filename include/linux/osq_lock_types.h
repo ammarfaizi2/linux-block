58c16b7eede6c1e34ecd97518f60e4893d0408af
Ingo Molnar <mingo@kernel.org>
2021-08-23 11:53:21 +0200

    headers/deps: locking: Split <linux/osq_lock.h> into <linux/osq_lock_types.h> and <linux/osq_lock_api.h>
    <linux/osq_lock.h> is included in a lot of other headers.
    
    In order to reduce this impact, we are splitting it into three parts:
    
     - <linux/osq_lock_types.h>: lightweight header with only type definitions
                                 and initializers.
    
     - <linux/osq_lock_api.h>: API declarations and inline functions. This header
                               is a bit larger.
    
     - <linux/osq_lock.h>: Simple default header that right now includes both
                           the types and the API headers, but will only
                           use the types header once all affected code
                           is converted.
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              | #include <linux/osq_lock_api.h>         | LOC: 2,306 | headers:   53
    	  | #include <linux/osq_lock_types.h>       | LOC:   184 | headers:   25
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/osq_lock_types.h
+++ include/linux/osq_lock_types.h
@@ -1,1 +1,30 @@
-#include <linux/osq_lock.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __LINUX_OSQ_LOCK_TYPES_H
+#define __LINUX_OSQ_LOCK_TYPES_H
+
+#include <linux/types.h>
+
+/*
+ * An MCS like lock especially tailored for optimistic spinning for sleeping
+ * lock implementations (mutex, rwsem, etc).
+ */
+struct optimistic_spin_node {
+	struct optimistic_spin_node *next, *prev;
+	int locked; /* 1 if lock acquired */
+	int cpu; /* encoded CPU # + 1 value */
+};
+
+struct optimistic_spin_queue {
+	/*
+	 * Stores an encoded value of the CPU # of the tail node in the queue.
+	 * If the queue is empty, then it's set to OSQ_UNLOCKED_VAL.
+	 */
+	atomic_t tail;
+};
+
+#define OSQ_UNLOCKED_VAL (0)
+
+/* Init macro and function. */
+#define OSQ_LOCK_UNLOCKED { ATOMIC_INIT(OSQ_UNLOCKED_VAL) }
+
+#endif /* __LINUX_OSQ_LOCK_TYPES_H */
