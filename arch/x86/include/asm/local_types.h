4c2a55015f2f54d5815be8be5733047d5f26b55b
Ingo Molnar <mingo@kernel.org>
2021-08-26 14:17:26 +0200

    headers/deps: locking/local, x86: Split <asm/local.h> into <asm/local_types.h> and <asm/local_api.h>
    <asm/local.h> is a medium size header, included in a lot of other headers.
    
    In order to reduce this impact, we are splitting it into three parts:
    
     - <asm/local_types.h>: lightweight header with only type definitions
                            and initializers.
    
     - <asm/local_api.h>: API declarations and inline functions. This header
                          is inevitably more heavyweight.
    
     - <asm/local.h>: Simple default header that right now includes both
                      the types and the API headers, but will only
                      use the types header once all affected code
                      is converted.
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              | #include <asm/local_api.h>              | LOC:  2,837 | headers:   53
              | #include <asm/local_types.h>            | LOC:    179 | headers:   25
    
    Also add an asm-generic placeholder for other architectures.
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/local_types.h
+++ arch/x86/include/asm/local_types.h
@@ -1,1 +1,13 @@
-#include <asm/local.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_X86_LOCAL_TYPES_H
+#define _ASM_X86_LOCAL_TYPES_H
+
+#include <linux/types.h>
+
+typedef struct {
+	atomic_long_t a;
+} local_t;
+
+#define LOCAL_INIT(i)	{ ATOMIC_LONG_INIT(i) }
+
+#endif /* _ASM_X86_LOCAL_TYPES_H */
