7df2181426600304fd994aff686a60982c1e90c7
Ingo Molnar <mingo@kernel.org>
2021-08-26 13:58:11 +0200

    headers/deps: irq: Split out <linux/interrupt_types.h> out of <linux/interrupt.h>
    Create a much smaller header, to be used by widely included headers such
    as <linux/kernel_stat.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -560,30 +560,6 @@ DECLARE_STATIC_KEY_FALSE(force_irqthreads_key);
 #define hard_irq_disable()	do { } while(0)
 #endif
 
-/* PLEASE, avoid to allocate new softirqs, if you need not _really_ high
-   frequency threaded job scheduling. For almost all the purposes
-   tasklets are more than enough. F.e. all serial device BHs et
-   al. should be converted to tasklets, not to softirqs.
- */
-
-enum
-{
-	HI_SOFTIRQ=0,
-	TIMER_SOFTIRQ,
-	NET_TX_SOFTIRQ,
-	NET_RX_SOFTIRQ,
-	BLOCK_SOFTIRQ,
-	IRQ_POLL_SOFTIRQ,
-	TASKLET_SOFTIRQ,
-	SCHED_SOFTIRQ,
-	HRTIMER_SOFTIRQ,
-	RCU_SOFTIRQ,    /* Preferable RCU should always be the last softirq */
-
-	NR_SOFTIRQS
-};
-
-#define SOFTIRQ_STOP_IDLE_MASK (~(1 << RCU_SOFTIRQ))
-
 /* map softirq index to softirq name. update 'softirq_to_name' in
  * kernel/softirq.c when adding a new softirq.
  */
--- include/linux/interrupt_types.h
+++ include/linux/interrupt_types.h
@@ -1,1 +1,30 @@
-#include <linux/interrupt.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+/* interrupt.h */
+#ifndef _LINUX_INTERRUPT_TYPES_H
+#define _LINUX_INTERRUPT_TYPES_H
+
+/* PLEASE, avoid to allocate new softirqs, if you need not _really_ high
+   frequency threaded job scheduling. For almost all the purposes
+   tasklets are more than enough. F.e. all serial device BHs et
+   al. should be converted to tasklets, not to softirqs.
+ */
+
+enum
+{
+	HI_SOFTIRQ=0,
+	TIMER_SOFTIRQ,
+	NET_TX_SOFTIRQ,
+	NET_RX_SOFTIRQ,
+	BLOCK_SOFTIRQ,
+	IRQ_POLL_SOFTIRQ,
+	TASKLET_SOFTIRQ,
+	SCHED_SOFTIRQ,
+	HRTIMER_SOFTIRQ,
+	RCU_SOFTIRQ,    /* Preferable RCU should always be the last softirq */
+
+	NR_SOFTIRQS
+};
+
+#define SOFTIRQ_STOP_IDLE_MASK (~(1 << RCU_SOFTIRQ))
+
+#endif
