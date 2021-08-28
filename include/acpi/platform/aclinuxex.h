ae06382b48bd1b6b0e36f3edeadd8c34a5c4cba0
Ingo Molnar <mingo@kernel.org>
2021-08-28 05:20:01 +0200

    headers/deps: ACPICA: Split locking definitions into <acpi/platform/aclinuxex_locking.h>
    <acpi/acpi.h> leaked <linux/spinlock_api.h> into over 2,000 files in
    the kernel.
    
    Split out <acpi/platform/aclinuxex_locking.h> for those files that need
    it. Fix up dangling dependencies.
    
    On a distro build this reduces the number of <linux/spinlock_api.h> inclusions
    by 2,246 times:
    
      before: # <linux/spinlock_api.h>                inclusions:      12473 ( 78.2%)
       after: # <linux/spinlock_api.h>                inclusions:      10227 ( 64.1%)
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/acpi/platform/aclinuxex.h
+++ include/acpi/platform/aclinuxex.h
@@ -10,8 +10,9 @@
 #ifndef __ACLINUXEX_H__
 #define __ACLINUXEX_H__
 
-#include <linux/spinlock_api.h>
-#include <linux/types.h>
+#include <linux/irqflags.h>
+
+#include <asm/current.h>
 
 #ifdef __KERNEL__
 
@@ -74,53 +75,6 @@ static inline void *acpi_os_acquire_object(acpi_cache_t * cache)
 static inline acpi_thread_id acpi_os_get_thread_id(void)
 {
 	return (acpi_thread_id) (unsigned long)current;
-}
-
-/*
- * When lockdep is enabled, the spin_lock_init() macro stringifies it's
- * argument and uses that as a name for the lock in debugging.
- * By executing spin_lock_init() in a macro the key changes from "lock" for
- * all locks to the name of the argument of acpi_os_create_lock(), which
- * prevents lockdep from reporting false positives for ACPICA locks.
- */
-#define acpi_os_create_lock(__handle) \
-	({ \
-		spinlock_t *lock = ACPI_ALLOCATE(sizeof(*lock)); \
-		if (lock) { \
-			*(__handle) = lock; \
-			spin_lock_init(*(__handle)); \
-		} \
-		lock ? AE_OK : AE_NO_MEMORY; \
-	})
-
-
-#define acpi_os_create_raw_lock(__handle) \
-	({ \
-		raw_spinlock_t *lock = ACPI_ALLOCATE(sizeof(*lock)); \
-		if (lock) { \
-			*(__handle) = lock; \
-			raw_spin_lock_init(*(__handle)); \
-		} \
-		lock ? AE_OK : AE_NO_MEMORY; \
-	})
-
-static inline acpi_cpu_flags acpi_os_acquire_raw_lock(acpi_raw_spinlock lockp)
-{
-	acpi_cpu_flags flags;
-
-	raw_spin_lock_irqsave(lockp, flags);
-	return flags;
-}
-
-static inline void acpi_os_release_raw_lock(acpi_raw_spinlock lockp,
-					    acpi_cpu_flags flags)
-{
-	raw_spin_unlock_irqrestore(lockp, flags);
-}
-
-static inline void acpi_os_delete_raw_lock(acpi_raw_spinlock handle)
-{
-	ACPI_FREE(handle);
 }
 
 static inline u8 acpi_os_readable(void *pointer, acpi_size length)
--- include/acpi/platform/aclinuxex_locking.h
+++ include/acpi/platform/aclinuxex_locking.h
@@ -1,2 +1,72 @@
+/* SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0 */
+/******************************************************************************
+ *
+ * Name: aclinuxex.h - Extra OS specific defines, etc. for Linux
+ *
+ * Copyright (C) 2000 - 2021, Intel Corp.
+ *
+ *****************************************************************************/
+
+#ifndef __ACLINUXEX_LOCKING_H__
+#define __ACLINUXEX_LOCKING_H__
+
+#ifdef __KERNEL__
+
 #include <acpi/acpi.h>
+#include <linux/spinlock_api.h>
 #include <acpi/platform/aclinuxex.h>
+
+/*
+ * When lockdep is enabled, the spin_lock_init() macro stringifies it's
+ * argument and uses that as a name for the lock in debugging.
+ * By executing spin_lock_init() in a macro the key changes from "lock" for
+ * all locks to the name of the argument of acpi_os_create_lock(), which
+ * prevents lockdep from reporting false positives for ACPICA locks.
+ */
+#define acpi_os_create_lock(__handle) \
+	({ \
+		spinlock_t *lock = ACPI_ALLOCATE(sizeof(*lock)); \
+		if (lock) { \
+			*(__handle) = lock; \
+			spin_lock_init(*(__handle)); \
+		} \
+		lock ? AE_OK : AE_NO_MEMORY; \
+	})
+
+
+#define acpi_os_create_raw_lock(__handle) \
+	({ \
+		raw_spinlock_t *lock = ACPI_ALLOCATE(sizeof(*lock)); \
+		if (lock) { \
+			*(__handle) = lock; \
+			raw_spin_lock_init(*(__handle)); \
+		} \
+		lock ? AE_OK : AE_NO_MEMORY; \
+	})
+
+static inline acpi_cpu_flags acpi_os_acquire_raw_lock(acpi_raw_spinlock lockp)
+{
+	acpi_cpu_flags flags;
+
+	raw_spin_lock_irqsave(lockp, flags);
+	return flags;
+}
+
+static inline void acpi_os_release_raw_lock(acpi_raw_spinlock lockp,
+					    acpi_cpu_flags flags)
+{
+	raw_spin_unlock_irqrestore(lockp, flags);
+}
+
+static inline void acpi_os_delete_raw_lock(acpi_raw_spinlock handle)
+{
+	ACPI_FREE(handle);
+}
+
+/*
+ * OSL interfaces added by Linux
+ */
+
+#endif				/* __KERNEL__ */
+
+#endif				/* __ACLINUXEX_LOCKING_H__ */
