27a1d77b457e806f1590bd89783a73df7894df3d
Ingo Molnar <mingo@kernel.org>
2021-08-28 15:26:12 +0200

    headers/deps: kernel_stat: Split out <linux/kernel_stat_types.h> from <linux/kernel_stat.h>
    Much smaller header, to be used by <linux/cgroup_types.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/kernel_stat_types.h
+++ include/linux/kernel_stat_types.h
@@ -1,1 +1,24 @@
-#include <linux/kernel_stat.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_KERNEL_STAT_TYPES_H
+#define _LINUX_KERNEL_STAT_TYPES_H
+
+#include <linux/spinlock_types.h>
+
+/**
+ * struct prev_cputime - snapshot of system and user cputime
+ * @utime: time spent in user mode
+ * @stime: time spent in system mode
+ * @lock: protects the above two fields
+ *
+ * Stores previous user/system time values such that we can guarantee
+ * monotonicity.
+ */
+struct prev_cputime {
+#ifndef CONFIG_VIRT_CPU_ACCOUNTING_NATIVE
+	u64				utime;
+	u64				stime;
+	raw_spinlock_t			lock;
+#endif
+};
+
+#endif /* _LINUX_KERNEL_STAT_TYPES_H */
