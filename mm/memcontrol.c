25933e9390019a438269193ab9f9b2642027a3b1
Ingo Molnar <mingo@kernel.org>
2021-08-29 09:23:39 +0200

    headers/uninline: Uninline single-use function: wb_domain_size_changed()
    This also decouples <linux/writeback_api.h> from <linux/jiffies.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4436,6 +4436,26 @@ static void memcg_wb_domain_exit(struct mem_cgroup *memcg)
 	wb_domain_exit(&memcg->cgwb_domain);
 }
 
+/**
+ * wb_domain_size_changed - memory available to a wb_domain has changed
+ * @dom: wb_domain of interest
+ *
+ * This function should be called when the amount of memory available to
+ * @dom has changed.  It resets @dom's dirty limit parameters to prevent
+ * the past values which don't match the current configuration from skewing
+ * dirty throttling.  Without this, when memory size of a wb_domain is
+ * greatly reduced, the dirty throttling logic may allow too many pages to
+ * be dirtied leading to consecutive unnecessary OOMs and may get stuck in
+ * that situation.
+ */
+static inline void wb_domain_size_changed(struct wb_domain *dom)
+{
+	spin_lock(&dom->lock);
+	dom->dirty_limit_tstamp = jiffies;
+	dom->dirty_limit = 0;
+	spin_unlock(&dom->lock);
+}
+
 static void memcg_wb_domain_size_changed(struct mem_cgroup *memcg)
 {
 	wb_domain_size_changed(&memcg->cgwb_domain);
