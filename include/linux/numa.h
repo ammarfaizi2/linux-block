cc005d4badfb9dc6971e418599ad65c4f5db0409
Ingo Molnar <mingo@kernel.org>
2021-09-06 20:07:52 +0200

    headers/deps: numa: Factor out <linux/numa_types.h> from <linux/numa.h>
    This is a low level header used in almost every other header, so
    introduce a much more lightweight <linux/numa_types.h> variant as well:
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              | <linux/numa.h>         LOC:   459,  headers used: 44
    	  | <linux/numa_types.h>   LOC:    20,  headers used: 16
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/numa.h
+++ include/linux/numa.h
@@ -1,24 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #ifndef _LINUX_NUMA_H
 #define _LINUX_NUMA_H
+
 #include <linux/types.h>
-
-#ifdef CONFIG_NODES_SHIFT
-#define NODES_SHIFT     CONFIG_NODES_SHIFT
-#else
-#define NODES_SHIFT     0
-#endif
-
-#define MAX_NUMNODES    (1 << NODES_SHIFT)
-
-#define	NUMA_NO_NODE	(-1)
-
-/* optionally keep NUMA memory info available post init */
-#ifdef CONFIG_NUMA_KEEP_MEMINFO
-#define __initdata_or_meminfo
-#else
-#define __initdata_or_meminfo __initdata
-#endif
+#include <linux/numa_types.h>
 
 #ifdef CONFIG_NUMA
 #include <linux/printk.h>
--- include/linux/numa_types.h
+++ include/linux/numa_types.h
@@ -1,1 +1,22 @@
-#include <linux/numa.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_NUMA_TYPES_H
+#define _LINUX_NUMA_TYPES_H
+
+#ifdef CONFIG_NODES_SHIFT
+#define NODES_SHIFT     CONFIG_NODES_SHIFT
+#else
+#define NODES_SHIFT     0
+#endif
+
+#define MAX_NUMNODES    (1 << NODES_SHIFT)
+
+#define	NUMA_NO_NODE	(-1)
+
+/* optionally keep NUMA memory info available post init */
+#ifdef CONFIG_NUMA_KEEP_MEMINFO
+#define __initdata_or_meminfo
+#else
+#define __initdata_or_meminfo __initdata
+#endif
+
+#endif /* _LINUX_NUMA_TYPES_H */
