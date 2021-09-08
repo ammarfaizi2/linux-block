822bcc69e41a3b757ac49985aeae45e3fdbad8c3
Ingo Molnar <mingo@kernel.org>
2021-09-08 22:18:29 +0200

    headers/deps: net: Split <linux/tasklet_types.h> and <linux/tasklet_api.h> out of <linux/interrupt.h>
    <linux/interrupt.h> is a large header, included in a lot of other headers:
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              | #include <linux/interrupt.h>            | LOC:  6,497 | headers:  207
    
    In order to reduce this impact, we are splitting out one of the more rarely
    used pieces of functionality - tasklets.
    
    Create two new headers:
    
     - <linux/tasklet_types.h>: lightweight header with only type definitions
                                and initializers.
    
     - <linux/tasklet_api.h>: API declarations and inline functions. This header
                              is inevitably more heavyweight.
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              | #include <linux/tasklet_api.h>          | LOC:  3,067 | headers:   79
              | #include <linux/tasklet_types.h>        | LOC:    188 | headers:   26
    
    No change in functionality in this first step, include <linux/tasklet_api.h>
    in <linux/interrupt.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/tasklet_types.h
+++ include/linux/tasklet_types.h
@@ -1,1 +1,20 @@
-#include <linux/interrupt.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_TASKLET_TYPES_H
+#define _LINUX_TASKLET_TYPES_H
+
+#include <linux/atomic_types.h>
+
+struct tasklet_struct
+{
+	struct tasklet_struct *next;
+	unsigned long state;
+	atomic_t count;
+	bool use_callback;
+	union {
+		void (*func)(unsigned long data);
+		void (*callback)(struct tasklet_struct *t);
+	};
+	unsigned long data;
+};
+
+#endif /* _LINUX_TASKLET_TYPES_H */
