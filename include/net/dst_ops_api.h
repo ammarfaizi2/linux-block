0dd7974b3d2632f74b1c3b34cf69dba61f6bee9b
Ingo Molnar <mingo@kernel.org>
2021-09-08 19:37:04 +0200

    headers/deps: net/dst: Split out the <net/dst_ops_api.h> header from <net/dst_ops.h>
    The goal is to have a much simpler <linux/dst_ops.h> that can be
    included in headers that don't need the APIs.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/net/dst_ops_api.h
+++ include/net/dst_ops_api.h
@@ -1,1 +1,38 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _NET_DST_OPS_API_H
+#define _NET_DST_OPS_API_H
+
 #include <net/dst_ops.h>
+
+#include <linux/types.h>
+#include <linux/percpu_counter_api.h>
+#include <linux/cache.h>
+
+static inline int dst_entries_get_fast(struct dst_ops *dst)
+{
+	return percpu_counter_read_positive(&dst->pcpuc_entries);
+}
+
+static inline int dst_entries_get_slow(struct dst_ops *dst)
+{
+	return percpu_counter_sum_positive(&dst->pcpuc_entries);
+}
+
+#define DST_PERCPU_COUNTER_BATCH 32
+static inline void dst_entries_add(struct dst_ops *dst, int val)
+{
+	percpu_counter_add_batch(&dst->pcpuc_entries, val,
+				 DST_PERCPU_COUNTER_BATCH);
+}
+
+static inline int dst_entries_init(struct dst_ops *dst)
+{
+	return percpu_counter_init(&dst->pcpuc_entries, 0, GFP_KERNEL);
+}
+
+static inline void dst_entries_destroy(struct dst_ops *dst)
+{
+	percpu_counter_destroy(&dst->pcpuc_entries);
+}
+
+#endif
