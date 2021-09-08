4ac18e91fc11383703a2d4626263c3020905cbff
Ingo Molnar <mingo@kernel.org>
2021-09-08 22:39:39 +0200

    headers/deps: resource: Split <linux/resource_ext.h> into <linux/resource_ext_types.h> and <linux/resource_ext_api.h>
    <linux/resource_ext.h> is a resonably large header, included in a lot of
    other headers.
    
    In order to reduce this impact, we are splitting it into three parts:
    
     - <linux/resource_ext_types.h>: lightweight header with only type definitions
                                     and initializers.
    
     - <linux/resource_ext_api.h>: API declarations and inline functions. This header
                                   is inevitably more heavyweight.
    
     - <linux/resource_ext.h>: Simple default header that right now includes both
                               the types and the API headers, but will only
                               use the types header once all affected code
                               is converted.
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
    	  | #include <linux/resource_ext_api.h>     | LOC:  2,628 | headers:  125
              | #include <linux/resource_ext_types.h>   | LOC:    370 | headers:   38
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/resource_ext_api.h
+++ include/linux/resource_ext_api.h
@@ -1,1 +1,67 @@
-#include <linux/resource_ext.h>
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2015, Intel Corporation
+ * Author: Jiang Liu <jiang.liu@linux.intel.com>
+ */
+#ifndef _LINUX_RESOURCE_EXT_API_H
+#define _LINUX_RESOURCE_EXT_API_H
+
+#include <linux/resource_ext_types.h>
+
+#include <linux/types.h>
+#include <linux/list.h>
+#include <linux/slab.h>
+#include <linux/kernel.h>
+
+extern struct resource_entry *
+resource_list_create_entry(struct resource *res, size_t extra_size);
+extern void resource_list_free(struct list_head *head);
+
+static inline void resource_list_add(struct resource_entry *entry,
+				     struct list_head *head)
+{
+	list_add(&entry->node, head);
+}
+
+static inline void resource_list_add_tail(struct resource_entry *entry,
+					  struct list_head *head)
+{
+	list_add_tail(&entry->node, head);
+}
+
+static inline void resource_list_del(struct resource_entry *entry)
+{
+	list_del(&entry->node);
+}
+
+static inline void resource_list_free_entry(struct resource_entry *entry)
+{
+	kfree(entry);
+}
+
+static inline void
+resource_list_destroy_entry(struct resource_entry *entry)
+{
+	resource_list_del(entry);
+	resource_list_free_entry(entry);
+}
+
+#define resource_list_for_each_entry(entry, list)	\
+	list_for_each_entry((entry), (list), node)
+
+#define resource_list_for_each_entry_safe(entry, tmp, list)	\
+	list_for_each_entry_safe((entry), (tmp), (list), node)
+
+static inline struct resource_entry *
+resource_list_first_type(struct list_head *list, unsigned long type)
+{
+	struct resource_entry *entry;
+
+	resource_list_for_each_entry(entry, list) {
+		if (resource_type(entry->res) == type)
+			return entry;
+	}
+	return NULL;
+}
+
+#endif /* _LINUX_RESOURCE_EXT_API_H */
--- include/linux/resource_ext_types.h
+++ include/linux/resource_ext_types.h
@@ -1,1 +1,28 @@
-#include <linux/resource_ext.h>
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2015, Intel Corporation
+ * Author: Jiang Liu <jiang.liu@linux.intel.com>
+ */
+#ifndef _LINUX_RESOURCE_EXT_TYPES_H
+#define _LINUX_RESOURCE_EXT_TYPES_H
+
+#include <linux/ioport.h>
+
+/* Represent resource window for bridge devices */
+struct resource_win {
+	struct resource res;		/* In master (CPU) address space */
+	resource_size_t offset;		/* Translation offset for bridge */
+};
+
+/*
+ * Common resource list management data structure and interfaces to support
+ * ACPI, PNP and PCI host bridge etc.
+ */
+struct resource_entry {
+	struct list_head	node;
+	struct resource		*res;	/* In master (CPU) address space */
+	resource_size_t		offset;	/* Translation offset for bridge */
+	struct resource		__res;	/* Default storage for res */
+};
+
+#endif /* _LINUX_RESOURCE_EXT_TYPES_H */
