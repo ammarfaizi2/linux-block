ca70417596b43cb1038524bb9a23bb536dd7a643
Al Viro <viro@zeniv.linux.org.uk>
2021-09-16 16:48:18 -0400

    um: stop polluting the namespace with registers.h contents
    Only one extern in there is needed in processor-generic.h, and it's
    not needed anywhere else.  So move it over there and get rid of
    the include in processor-generic.h, adding includes of registers.h
    to the few files that need the declarations in it.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/x86/um/syscalls_64.c
+++ arch/x86/um/syscalls_64.c
@@ -10,6 +10,7 @@
 #include <linux/syscalls.h>
 #include <linux/uaccess.h>
 #include <asm/prctl.h> /* XXX This should get the constants from libc */
+#include <registers.h>
 #include <os.h>
 
 long arch_prctl(struct task_struct *task, int option,
