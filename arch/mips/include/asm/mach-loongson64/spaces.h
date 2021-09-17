128dace6e27ece8e9fb66d6093bd122163799a4a
Ingo Molnar <mingo@kernel.org>
2021-09-17 11:14:22 +0200

    headers/prep: Fix header to build standalone: MIPS: <asm/mach-loongson64/spaces.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/mips/include/asm/mach-loongson64/spaces.h
+++ arch/mips/include/asm/mach-loongson64/spaces.h
@@ -2,6 +2,8 @@
 #ifndef __ASM_MACH_LOONGSON64_SPACES_H_
 #define __ASM_MACH_LOONGSON64_SPACES_H_
 
+#include <linux/sizes.h>
+
 #if defined(CONFIG_64BIT)
 #define CAC_BASE        _AC(0x9800000000000000, UL)
 #endif /* CONFIG_64BIT */
