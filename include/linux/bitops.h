d9f67dc60242bf95607d9caf1e2b4ed1d87af90d
Ingo Molnar <mingo@kernel.org>
2021-10-13 22:04:09 +0200

    headers/deps: bitops: Split out <linux/bitops_types.h> header
    This is a smaller header that suffices for many other headers.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/bitops.h
+++ include/linux/bitops.h
@@ -2,24 +2,9 @@
 #ifndef _LINUX_BITOPS_H
 #define _LINUX_BITOPS_H
 
-#include <asm/types.h>
-#include <linux/bits.h>
+#include <linux/bitops_types.h>
+
 #include <linux/typecheck.h>
-
-#include <uapi/linux/kernel.h>
-
-/* Set bits in the first 'n' bytes when loaded from memory */
-#ifdef __LITTLE_ENDIAN
-#  define aligned_byte_mask(n) ((1UL << 8*(n))-1)
-#else
-#  define aligned_byte_mask(n) (~0xffUL << (BITS_PER_LONG - 8 - 8*(n)))
-#endif
-
-#define BITS_PER_TYPE(type)	(sizeof(type) * BITS_PER_BYTE)
-#define BITS_TO_LONGS(nr)	__KERNEL_DIV_ROUND_UP(nr, BITS_PER_TYPE(long))
-#define BITS_TO_U64(nr)		__KERNEL_DIV_ROUND_UP(nr, BITS_PER_TYPE(u64))
-#define BITS_TO_U32(nr)		__KERNEL_DIV_ROUND_UP(nr, BITS_PER_TYPE(u32))
-#define BITS_TO_BYTES(nr)	__KERNEL_DIV_ROUND_UP(nr, BITS_PER_TYPE(char))
 
 /*
  * Include this here because some architectures need generic_ffs/fls in
--- include/linux/bitops_types.h
+++ include/linux/bitops_types.h
@@ -1,1 +1,23 @@
-#include <linux/bitops.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_BITOPS_TYPES_H
+#define _LINUX_BITOPS_TYPES_H
+
+#include <asm/types.h>
+#include <linux/bits.h>
+
+#include <uapi/linux/kernel.h>
+
+/* Set bits in the first 'n' bytes when loaded from memory */
+#ifdef __LITTLE_ENDIAN
+#  define aligned_byte_mask(n) ((1UL << 8*(n))-1)
+#else
+#  define aligned_byte_mask(n) (~0xffUL << (BITS_PER_LONG - 8 - 8*(n)))
+#endif
+
+#define BITS_PER_TYPE(type)	(sizeof(type) * BITS_PER_BYTE)
+#define BITS_TO_LONGS(nr)	__KERNEL_DIV_ROUND_UP(nr, BITS_PER_TYPE(long))
+#define BITS_TO_U64(nr)		__KERNEL_DIV_ROUND_UP(nr, BITS_PER_TYPE(u64))
+#define BITS_TO_U32(nr)		__KERNEL_DIV_ROUND_UP(nr, BITS_PER_TYPE(u32))
+#define BITS_TO_BYTES(nr)	__KERNEL_DIV_ROUND_UP(nr, BITS_PER_TYPE(char))
+
+#endif
