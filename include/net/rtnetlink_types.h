3b214c0cda5facce29cd9a446ff36da538c5b96c
Ingo Molnar <mingo@kernel.org>
2021-10-14 12:19:26 +0200

    headers/deps: net: Split <net/rtnetlink.h> into <net/rtnetlink_types.h> and <net/rtnetlink_api.h>
    <net/rtnetlink.h> is a large header, included in a lot of other headers.
    
    In order to reduce this impact, we are splitting it into three parts:
    
     - <net/rtnetlink_types.h>: lightweight header with only type definitions
                                and initializers.
    
     - <net/rtnetlink_api.h>: API declarations and inline functions. This header
                              is inevitably more heavyweight.
    
     - <net/rtnetlink.h>: Simple default header that right now includes both
                          the types and the API headers, but will only
                          use the types header once all affected code
                          is converted.
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              | #include <net/rtnetlink_api.h>          | LOC: 15,601 | headers:  323
    	  | #include <net/rtnetlink_types.h>        | LOC:    244 | headers:   26
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/net/rtnetlink_types.h
+++ include/net/rtnetlink_types.h
@@ -1,1 +1,11 @@
-#include <net/rtnetlink.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __NET_RTNETLINK_TYPES_H
+#define __NET_RTNETLINK_TYPES_H
+
+#include <net/netlink_types.h>
+
+enum rtnl_link_flags {
+	RTNL_FLAG_DOIT_UNLOCKED = 1,
+};
+
+#endif
