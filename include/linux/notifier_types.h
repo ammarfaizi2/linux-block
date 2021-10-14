ed226e8b569ce39c91b5c2f84a5f95438a98073d
Ingo Molnar <mingo@kernel.org>
2021-10-14 10:42:17 +0200

    headers/deps: notifiers: Split <linux/notifier.h> into <linux/notifier_types.h> and <linux/notifier_api.h>
    <linux/notifier.h> is a pretty large header, included in a lot of other headers:
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              | #include <linux/notifier.h>                    | LOC:  6824 | headers: 215 |
    
    In order to reduce this impact, we are going to split the header into
    three parts:
    
     - <linux/notifier_types.h>: lightweight header with only type definitions
                                 and initializers.
    
     - <linux/notifier_api.h>: API declarations and inline functions. This header
                               is inevitably more heavyweight.
    
     - <linux/notifier.h>: Simple default header that right now includes both
                           the types and the API headers, but will only
                           use the types header once all affected code
                           is converted.
    
    The size savings will be very substantial:
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              | #include <linux/notifier_api.h>                | LOC:  6824 | headers: 214 |
              | #include <linux/notifier_types.h>              | LOC:    29 | headers:  16 |
    
    But no change in functionality or header dependencies in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/notifier_types.h
+++ include/linux/notifier_types.h
@@ -1,1 +1,16 @@
-#include <linux/notifier.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_NOTIFIER_TYPES_H
+#define _LINUX_NOTIFIER_TYPES_H
+
+struct notifier_block;
+
+typedef	int (*notifier_fn_t)(struct notifier_block *nb,
+			unsigned long action, void *data);
+
+struct notifier_block {
+	notifier_fn_t notifier_call;
+	struct notifier_block __rcu *next;
+	int priority;
+};
+
+#endif /* _LINUX_NOTIFIER_TYPES_H */
