7e4d0fc8dd3fac8187d86f994100fc6481018275
David Howells <dhowells@redhat.com>
2021-10-20 15:53:34 +0100

    fscache: Implement cookie registration
    Add functions to the fscache API to allow data file cookies to be acquired
    and relinquished by the network filesystem.  It is intended that the
    filesystem will create such cookies per-inode under a volume.
    
    To request a cookie, it uses:
    
    	struct fscache_cookie *
    	fscache_acquire_cookie(struct fscache_volume *volume,
    			       u8 advice,
    			       const void *index_key,
    			       size_t index_key_len,
    			       const void *aux_data,
    			       size_t aux_data_len,
    			       loff_t object_size)
    
    
    The filesystem must first create a volume cookie, which is passed in here.
    If it passes in NULL then the function will do nothing other than just
    return a NULL cookie.
    
    A binary key should be passed in index_key and is of size index_key_len.
    This is saved in the cookie and is used to locate the associated data in
    the cache.
    
    A coherency data buffer of size aux_data_len will be allocated and
    initialised from the buffer pointed to by aux_data.  This is used to
    validate cache objects when they're opened and is stored on disk with them
    when they're committed.  The data is stored in the cookie and will be
    updateable by various functions in later patches.
    
    The object_size must also be given.  This is also used to perform a
    coherency check and to size the backing storage appropriately.
    
    This function disallows a cookie from being acquired twice in parallel,
    though it will cause the second user to wait if the first is busy
    relinquishing its cookie.
    
    
    When a network filesystem has finished with a cookie, it should call:
    
    	void
    	fscache_relinquish_cookie(struct fscache_volume *volume,
    				  bool retire)
    
    If retire is true, any backing data will be discarded immediately.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: linux-cachefs@redhat.com

--- fs/fscache/stats.c
+++ fs/fscache/stats.c
@@ -16,6 +16,20 @@
 atomic_t fscache_n_volumes;
 atomic_t fscache_n_volumes_collision;
 atomic_t fscache_n_volumes_nomem;
+atomic_t fscache_n_cookies;
+
+atomic_t fscache_n_acquires;
+atomic_t fscache_n_acquires_null;
+atomic_t fscache_n_acquires_no_cache;
+atomic_t fscache_n_acquires_ok;
+atomic_t fscache_n_acquires_oom;
+
+atomic_t fscache_n_updates;
+EXPORT_SYMBOL(fscache_n_updates);
+
+atomic_t fscache_n_relinquishes;
+atomic_t fscache_n_relinquishes_retire;
+atomic_t fscache_n_relinquishes_dropped;
 
 /*
  * display the general statistics
@@ -23,11 +37,27 @@ atomic_t fscache_n_volumes_nomem;
 int fscache_stats_show(struct seq_file *m, void *v)
 {
 	seq_puts(m, "FS-Cache statistics\n");
-	seq_printf(m, "Cookies: v=%d vcol=%u voom=%u\n",
+	seq_printf(m, "Cookies: n=%d v=%d vcol=%u voom=%u\n",
+		   atomic_read(&fscache_n_cookies),
 		   atomic_read(&fscache_n_volumes),
 		   atomic_read(&fscache_n_volumes_collision),
 		   atomic_read(&fscache_n_volumes_nomem)
 		   );
+
+	seq_printf(m, "Acquire: n=%u nul=%u noc=%u ok=%u oom=%u\n",
+		   atomic_read(&fscache_n_acquires),
+		   atomic_read(&fscache_n_acquires_null),
+		   atomic_read(&fscache_n_acquires_no_cache),
+		   atomic_read(&fscache_n_acquires_ok),
+		   atomic_read(&fscache_n_acquires_oom));
+
+	seq_printf(m, "Updates: n=%u\n",
+		   atomic_read(&fscache_n_updates));
+
+	seq_printf(m, "Relinqs: n=%u rtr=%u drop=%u\n",
+		   atomic_read(&fscache_n_relinquishes),
+		   atomic_read(&fscache_n_relinquishes_retire),
+		   atomic_read(&fscache_n_relinquishes_dropped));
 
 	netfs_stats_show(m);
 	return 0;
--- include/linux/fscache-cache.h
+++ include/linux/fscache-cache.h
@@ -18,6 +18,7 @@
 
 struct fscache_cache;
 enum fscache_cache_trace;
+enum fscache_cookie_trace;
 enum fscache_access_trace;
 
 enum fscache_cache_state {
@@ -75,6 +76,24 @@ extern struct rw_semaphore fscache_addremove_sem;
 extern struct fscache_cache *fscache_acquire_cache(const char *name);
 extern void fscache_put_cache(struct fscache_cache *cache,
 			      enum fscache_cache_trace where);
+
+extern struct fscache_cookie *fscache_get_cookie(struct fscache_cookie *cookie,
+						 enum fscache_cookie_trace where);
+extern void fscache_put_cookie(struct fscache_cookie *cookie,
+			       enum fscache_cookie_trace where);
+extern void fscache_set_cookie_stage(struct fscache_cookie *cookie,
+				     enum fscache_cookie_stage stage);
+
+/*
+ * Find the key on a cookie.
+ */
+static inline void *fscache_get_key(struct fscache_cookie *cookie)
+{
+	if (cookie->key_len <= sizeof(cookie->inline_key))
+		return cookie->inline_key;
+	else
+		return cookie->key;
+}
 
 extern struct workqueue_struct *fscache_wq;
 
