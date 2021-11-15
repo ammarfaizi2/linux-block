8cacaa91c592814cc3d93999298c0e9fb1614440
Ingo Molnar <mingo@kernel.org>
2021-11-15 18:56:35 +0100

    headers/uninline: Uninline multi-use function: io_mapping_map_atomic_wc() and io_mapping_unmap_atomic()
    These are rather large functions, but doing this also decouples
    <linux/io-mapping.h> from task_struct.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/io-mapping.h
+++ include/linux/io-mapping.h
@@ -60,28 +60,6 @@ io_mapping_fini(struct io_mapping *mapping)
 	iomap_free(mapping->base, mapping->size);
 }
 
-/* Atomic map/unmap */
-static inline void __iomem *
-io_mapping_map_atomic_wc(struct io_mapping *mapping,
-			 unsigned long offset)
-{
-	resource_size_t phys_addr;
-
-	BUG_ON(offset >= mapping->size);
-	phys_addr = mapping->base + offset;
-	preempt_disable();
-	pagefault_disable();
-	return __iomap_local_pfn_prot(PHYS_PFN(phys_addr), mapping->prot);
-}
-
-static inline void
-io_mapping_unmap_atomic(void __iomem *vaddr)
-{
-	kunmap_local_indexed((void __force *)vaddr);
-	pagefault_enable();
-	preempt_enable();
-}
-
 static inline void __iomem *
 io_mapping_map_local_wc(struct io_mapping *mapping, unsigned long offset)
 {
@@ -157,24 +135,6 @@ io_mapping_unmap(void __iomem *vaddr)
 {
 }
 
-/* Atomic map/unmap */
-static inline void __iomem *
-io_mapping_map_atomic_wc(struct io_mapping *mapping,
-			 unsigned long offset)
-{
-	preempt_disable();
-	pagefault_disable();
-	return io_mapping_map_wc(mapping, offset, PAGE_SIZE);
-}
-
-static inline void
-io_mapping_unmap_atomic(void __iomem *vaddr)
-{
-	io_mapping_unmap(vaddr);
-	pagefault_enable();
-	preempt_enable();
-}
-
 static inline void __iomem *
 io_mapping_map_local_wc(struct io_mapping *mapping, unsigned long offset)
 {
@@ -187,6 +147,10 @@ static inline void io_mapping_unmap_local(void __iomem *vaddr)
 }
 
 #endif /* !HAVE_ATOMIC_IOMAP */
+
+/* Atomic map/unmap */
+extern void __iomem *io_mapping_map_atomic_wc(struct io_mapping *mapping, unsigned long offset);
+extern void io_mapping_unmap_atomic(void __iomem *vaddr);
 
 static inline struct io_mapping *
 io_mapping_create_wc(resource_size_t base,
