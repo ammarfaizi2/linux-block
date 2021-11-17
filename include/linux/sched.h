f3579da4482f82001a4960d59590487a446e7335
Ingo Molnar <mingo@kernel.org>
2021-11-17 13:10:23 +0100

    headers/deps: Move task_struct::last_wakee to per_task()
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -695,7 +695,6 @@ struct task_struct {
 
 #ifdef CONFIG_SMP
 	struct __call_single_node	wake_entry;
-	struct task_struct		*last_wakee;
 
 	/*
 	 * recent_used_cpu is initially set as the last CPU used by a task
