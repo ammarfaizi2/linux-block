3572b1f3eccf7c6df7d9cdbc673bbe00ea159836
Ingo Molnar <mingo@kernel.org>
2021-11-20 10:52:15 +0100

    headers/uninline: Uninline single-use function: set_page_links()
    The CONFIG_SPARSEMEM definitions are full of layering violations,
    with include/asm-generic/memory_model.h being a prime example.
    
    Start cleaning this up by moving some of its definitions to less
    dependency-fragile places, such as the set_page_links() method,
    which can move from linux/mm_api.h into mm/page_alloc.c.
    
    This also reduces <linux/mm_api.h>'s complexity a tiny bit.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1606,16 +1606,6 @@ static inline void set_page_node(struct page *page, unsigned long node)
 	page->flags |= (node & NODES_MASK) << NODES_PGSHIFT;
 }
 
-static inline void set_page_links(struct page *page, enum zone_type zone,
-	unsigned long node, unsigned long pfn)
-{
-	set_page_zone(page, zone);
-	set_page_node(page, node);
-#ifdef SECTION_IN_PAGE_FLAGS
-	set_page_section(page, pfn_to_section_nr(pfn));
-#endif
-}
-
 /**
  * folio_nr_pages - The number of pages in the folio.
  * @folio: The folio.
--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1567,6 +1567,16 @@ static void free_one_page(struct zone *zone,
 	spin_unlock_irqrestore(&zone->lock, flags);
 }
 
+static inline void set_page_links(struct page *page, enum zone_type zone,
+	unsigned long node, unsigned long pfn)
+{
+	set_page_zone(page, zone);
+	set_page_node(page, node);
+#ifdef SECTION_IN_PAGE_FLAGS
+	set_page_section(page, pfn_to_section_nr(pfn));
+#endif
+}
+
 static void __meminit __init_single_page(struct page *page, unsigned long pfn,
 				unsigned long zone, int nid)
 {
