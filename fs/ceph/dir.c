60566b4b4c1f7dd9b5f4a76ba2d66a67e2629d84
Christian Brauner <christian.brauner@ubuntu.com>
2021-11-24 17:22:44 +0100

    ceph: allow idmapped mknod inode op
    Enable ceph_mknod() to handle idmapped mounts. This is just a matter of
    passing down the mount's idmapping.
    
    Cc: Jeff Layton <jlayton@kernel.org>
    Cc: Ilya Dryomov <idryomov@gmail.com>
    Cc: ceph-devel@vger.kernel.org
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- fs/ceph/dir.c
+++ fs/ceph/dir.c
@@ -871,6 +871,7 @@ static int ceph_mknod(struct user_namespace *mnt_userns, struct inode *dir,
 	req->r_parent = dir;
 	ihold(dir);
 	set_bit(CEPH_MDS_R_PARENT_LOCKED, &req->r_req_flags);
+	req->mnt_userns = mnt_userns;
 	req->r_args.mknod.mode = cpu_to_le32(mode);
 	req->r_args.mknod.rdev = cpu_to_le32(rdev);
 	req->r_dentry_drop = CEPH_CAP_FILE_SHARED | CEPH_CAP_AUTH_EXCL;
