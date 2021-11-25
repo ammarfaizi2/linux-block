78c072b241b0facf2ff00384dda4bdd9ceb46e55
Ingo Molnar <mingo@kernel.org>
2021-11-25 10:05:23 +0100

    headers/deps: mm: Split <linux/mm_api_kvmalloc.h> methods out of <linux/slab.h>
    No need for them to be in such a frequently used and heavy header.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/slab.h
+++ include/linux/slab.h
@@ -12,6 +12,7 @@
 #ifndef _LINUX_SLAB_H
 #define	_LINUX_SLAB_H
 
+#include <linux/mm_api_kvmalloc.h>
 #include <linux/numa_types.h>
 #include <linux/gfp_types.h>
 #include <linux/overflow.h>
@@ -724,40 +725,6 @@ static inline __alloc_size(1) void *kzalloc_node(size_t size, gfp_t flags, int n
 {
 	return kmalloc_node(size, flags | __GFP_ZERO, node);
 }
-
-extern void *kvmalloc_node(size_t size, gfp_t flags, int node) __alloc_size(1);
-static inline __alloc_size(1) void *kvmalloc(size_t size, gfp_t flags)
-{
-	return kvmalloc_node(size, flags, NUMA_NO_NODE);
-}
-static inline __alloc_size(1) void *kvzalloc_node(size_t size, gfp_t flags, int node)
-{
-	return kvmalloc_node(size, flags | __GFP_ZERO, node);
-}
-static inline __alloc_size(1) void *kvzalloc(size_t size, gfp_t flags)
-{
-	return kvmalloc(size, flags | __GFP_ZERO);
-}
-
-static inline __alloc_size(1, 2) void *kvmalloc_array(size_t n, size_t size, gfp_t flags)
-{
-	size_t bytes;
-
-	if (unlikely(check_mul_overflow(n, size, &bytes)))
-		return NULL;
-
-	return kvmalloc(bytes, flags);
-}
-
-static inline __alloc_size(1, 2) void *kvcalloc(size_t n, size_t size, gfp_t flags)
-{
-	return kvmalloc_array(n, size, flags | __GFP_ZERO);
-}
-
-extern void *kvrealloc(const void *p, size_t oldsize, size_t newsize, gfp_t flags)
-		      __alloc_size(3);
-extern void kvfree(const void *addr);
-extern void kvfree_sensitive(const void *addr, size_t len);
 
 unsigned int kmem_cache_size(struct kmem_cache *s);
 void __init kmem_cache_init_late(void);
