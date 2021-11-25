09fe94870e7214594572ea25f762204cf47577b2
Ingo Molnar <mingo@kernel.org>
2021-11-25 14:22:31 +0100

    headers/deps: llist: Split <linux/llist.h> into <linux/llist_types.h> and <linux/llist_api.h>
    <linux/llist.h> is a medium header, included in a lot of other headers:
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              |  #include <linux/llist.h>                | LOC:  2,770 | headers:   67
    
    In order to reduce this impact, we are splitting it into three parts:
    
     - <linux/llist_types.h>: lightweight header with only type definitions
                              and initializers.
    
     - <linux/llist_api.h>: API declarations and inline functions. This header
                            is inevitably more heavyweight.
    
     - <linux/llist.h>: Simple default header that right now includes both
                        the types and the API headers, but will only
                        use the types header once all affected code
                        is converted.
    
    The size reduction is substantial:
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
    	  | #include <linux/llist_api.h>            | LOC:  2,770 | headers:   66
              | #include <linux/llist_types.h>          | LOC:     34 | headers:   19
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/llist_types.h
+++ include/linux/llist_types.h
@@ -1,1 +1,27 @@
-#include <linux/llist.h>
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef LLIST_TYPES_H
+#define LLIST_TYPES_H
+
+#include <linux/stddef.h>
+
+struct llist_head {
+	struct llist_node *first;
+};
+
+struct llist_node {
+	struct llist_node *next;
+};
+
+#define LLIST_HEAD_INIT(name)	{ NULL }
+#define LLIST_HEAD(name)	struct llist_head name = LLIST_HEAD_INIT(name)
+
+/**
+ * init_llist_head - initialize lock-less list head
+ * @head:	the head for your lock-less list
+ */
+static inline void init_llist_head(struct llist_head *list)
+{
+	list->first = NULL;
+}
+
+#endif /* LLIST_TYPES_H */
