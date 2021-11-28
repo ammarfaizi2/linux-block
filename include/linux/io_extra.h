0e5e3a6198a83f44f8c349b2540c0463ae122183
Ingo Molnar <mingo@kernel.org>
2021-11-28 18:44:30 +0100

    headers/deps: io: Introduce <linux/io.h> for rarely used APIs
    Move the memset/memcpy using APIs over there, because they
    are relatively infrequently used: only ~300 times, vs. the
    over 15,000 uses of the <asm/io.h> facilities.
    
    This decouples <asm-generic/io.h> from <linux/string.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/io_extra.h
+++ include/linux/io_extra.h
@@ -1,1 +1,64 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef _LINUX_IO_EXTRA_H
+#define _LINUX_IO_EXTRA_H
+
 #include <linux/io.h>
+#include <linux/string.h>
+
+#ifndef memset_io
+#define memset_io memset_io
+/**
+ * memset_io	Set a range of I/O memory to a constant value
+ * @addr:	The beginning of the I/O-memory range to set
+ * @val:	The value to set the memory to
+ * @count:	The number of bytes to set
+ *
+ * Set a range of I/O memory to a given value.
+ */
+static inline void memset_io(volatile void __iomem *addr, int value,
+			     size_t size)
+{
+	memset(__io_virt(addr), value, size);
+}
+#endif
+
+#ifndef memcpy_fromio
+#define memcpy_fromio memcpy_fromio
+/**
+ * memcpy_fromio	Copy a block of data from I/O memory
+ * @dst:		The (RAM) destination for the copy
+ * @src:		The (I/O memory) source for the data
+ * @count:		The number of bytes to copy
+ *
+ * Copy a block of data from I/O memory.
+ */
+static inline void memcpy_fromio(void *buffer,
+				 const volatile void __iomem *addr,
+				 size_t size)
+{
+	memcpy(buffer, __io_virt(addr), size);
+}
+#endif
+
+#ifndef memcpy_toio
+#define memcpy_toio memcpy_toio
+/**
+ * memcpy_toio		Copy a block of data into I/O memory
+ * @dst:		The (I/O memory) destination for the copy
+ * @src:		The (RAM) source for the data
+ * @count:		The number of bytes to copy
+ *
+ * Copy a block of data to I/O memory.
+ */
+static inline void memcpy_toio(volatile void __iomem *addr, const void *buffer,
+			       size_t size)
+{
+	memcpy(__io_virt(addr), buffer, size);
+}
+#endif
+
+#ifndef CONFIG_GENERIC_DEVMEM_IS_ALLOWED
+extern int devmem_is_allowed(unsigned long pfn);
+#endif
+
+#endif /* _LINUX_IO_EXTRA_H */
