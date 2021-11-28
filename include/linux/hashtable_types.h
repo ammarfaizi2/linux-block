80c4496c7f6fe9380008050a7667a009b54738b3
Ingo Molnar <mingo@kernel.org>
2021-11-28 11:00:34 +0100

    headers/deps: hashtable_types: Use ilog2_const()
    Make HASH_BITS() use ilog2_const().
    
    Since it uses ARRAY_SIZE(), which is not supposed to be used on dynamic arrays,
    this shouldn't be a limitation in practice.
    
    If some code uses it that way, a compiler error or warning should alert us,
    due to HASH_BITS() returning -1. I hope. :-/
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/hashtable_types.h
+++ include/linux/hashtable_types.h
@@ -22,7 +22,7 @@
 	struct hlist_head name[1 << (bits)]
 
 #define HASH_SIZE(name) (ARRAY_SIZE(name))
-#define HASH_BITS(name) ilog2(HASH_SIZE(name))
+#define HASH_BITS(name) const_ilog2(HASH_SIZE(name))
 
 /* Use hash_32 when possible to allow for fast 32bit hashing in 64bit kernels. */
 #define hash_min(val, bits)							\
