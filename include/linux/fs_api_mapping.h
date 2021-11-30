a90b3171450547143d58e15a3efad8324ad5a3b8
Ingo Molnar <mingo@kernel.org>
2021-11-30 17:23:52 +0100

    headers/deps: fs: Introduce <linux/fs_api_mapping.h> for address space mapping related APIs
    <linux/fs_api.h> is used in over 4,100 files, while the address space
    mapping related functions are only used by ~8 files.
    
    So move them over into their new <linux/fs_api_mapping.h> header.
    
    The motivation is to allow the reduction of <linux/fs_api.h>
    header dependencies.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/fs_api_mapping.h
+++ include/linux/fs_api_mapping.h
@@ -1,1 +1,43 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_FS_API_MAPPING_H
+#define _LINUX_FS_API_MAPPING_H
+
 #include <linux/fs_api.h>
+
+/*
+ * Might pages of this file have been modified in userspace?
+ * Note that i_mmap_writable counts all VM_SHARED vmas: do_mmap
+ * marks vma as VM_SHARED if it is shared, and the file was opened for
+ * writing i.e. vma may be mprotected writable even if now readonly.
+ *
+ * If i_mmap_writable is negative, no new writable mappings are allowed. You
+ * can only deny writable mappings, if none exists right now.
+ */
+static inline int mapping_writably_mapped(struct address_space *mapping)
+{
+	return atomic_read(&mapping->i_mmap_writable) > 0;
+}
+
+static inline int mapping_map_writable(struct address_space *mapping)
+{
+	return atomic_inc_unless_negative(&mapping->i_mmap_writable) ?
+		0 : -EPERM;
+}
+
+static inline void mapping_unmap_writable(struct address_space *mapping)
+{
+	atomic_dec(&mapping->i_mmap_writable);
+}
+
+static inline int mapping_deny_writable(struct address_space *mapping)
+{
+	return atomic_dec_unless_positive(&mapping->i_mmap_writable) ?
+		0 : -EBUSY;
+}
+
+static inline void mapping_allow_writable(struct address_space *mapping)
+{
+	atomic_inc(&mapping->i_mmap_writable);
+}
+
+#endif /* _LINUX_FS_API_MAPPING_H */
