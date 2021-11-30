8615e15e10bd935551d9404269d72aabbda7df07
Ingo Molnar <mingo@kernel.org>
2021-11-30 20:45:22 +0100

    headers/deps: poll: Introduce <linux/poll_time.h> for time related APIs
    <linux/poll.h.h> is used in over 3,800 files, while the time
    related functions are only used in ~4 files.
    
    So move them over into their new <linux/poll_time.h> header.
    
    The motivation is to allow the reduction of <linux/poll.h.h>
    header dependencies.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/poll_time.h
+++ include/linux/poll_time.h
@@ -1,1 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_POLL_TIME_H
+#define _LINUX_POLL_TIME_H
+
 #include <linux/poll.h>
+
+#include <linux/time64_types.h>
+#include <linux/ktime.h>
+
+
+extern u64 select_estimate_accuracy(struct timespec64 *tv);
+
+#define MAX_INT64_SECONDS (((s64)(~((u64)0)>>1)/HZ)-1)
+
+extern int core_sys_select(int n, fd_set __user *inp, fd_set __user *outp,
+			   fd_set __user *exp, struct timespec64 *end_time);
+
+extern int poll_select_set_timeout(struct timespec64 *to, time64_t sec,
+				   long nsec);
+#endif /* _LINUX_POLL_TIME_H */
