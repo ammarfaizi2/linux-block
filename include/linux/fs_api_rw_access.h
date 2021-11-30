7a3763ba4c9288f09c056638063ffef6fe1509ff
Ingo Molnar <mingo@kernel.org>
2021-11-30 17:24:00 +0100

    headers/deps: fs: Introduce <linux/fs_api_rw_access.h> for inode RW access related APIs
    <linux/fs_api.h> is used in over 4,100 files, while the RW access
    related functions are only used by ~25 files.
    
    So move them over into their new <linux/fs_api_rw_access.h> header.
    
    The motivation is to allow the reduction of <linux/fs_api.h>
    header dependencies.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/fs_api_rw_access.h
+++ include/linux/fs_api_rw_access.h
@@ -1,1 +1,72 @@
-#include <linux/fs.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_FS_API_RW_H
+#define _LINUX_FS_API_RW_H
+
+#include <linux/fs_api.h>
+
+#if defined(CONFIG_IMA) || defined(CONFIG_FILE_LOCKING)
+static inline void i_readcount_dec(struct inode *inode)
+{
+	BUG_ON(!atomic_read(&inode->i_readcount));
+	atomic_dec(&inode->i_readcount);
+}
+static inline void i_readcount_inc(struct inode *inode)
+{
+	atomic_inc(&inode->i_readcount);
+}
+#else
+static inline void i_readcount_dec(struct inode *inode)
+{
+	return;
+}
+static inline void i_readcount_inc(struct inode *inode)
+{
+	return;
+}
+#endif
+
+/*
+ * This is used for regular files where some users -- especially the
+ * currently executed binary in a process, previously handled via
+ * VM_DENYWRITE -- cannot handle concurrent write (and maybe mmap
+ * read-write shared) accesses.
+ *
+ * get_write_access() gets write permission for a file.
+ * put_write_access() releases this write permission.
+ * deny_write_access() denies write access to a file.
+ * allow_write_access() re-enables write access to a file.
+ *
+ * The i_writecount field of an inode can have the following values:
+ * 0: no write access, no denied write access
+ * < 0: (-i_writecount) users that denied write access to the file.
+ * > 0: (i_writecount) users that have write access to the file.
+ *
+ * Normally we operate on that counter with atomic_{inc,dec} and it's safe
+ * except for the cases where we don't hold i_writecount yet. Then we need to
+ * use {get,deny}_write_access() - these functions check the sign and refuse
+ * to do the change if sign is wrong.
+ */
+static inline int get_write_access(struct inode *inode)
+{
+	return atomic_inc_unless_negative(&inode->i_writecount) ? 0 : -ETXTBSY;
+}
+static inline int deny_write_access(struct file *file)
+{
+	struct inode *inode = file_inode(file);
+	return atomic_dec_unless_positive(&inode->i_writecount) ? 0 : -ETXTBSY;
+}
+static inline void put_write_access(struct inode * inode)
+{
+	atomic_dec(&inode->i_writecount);
+}
+static inline void allow_write_access(struct file *file)
+{
+	if (file)
+		atomic_inc(&file_inode(file)->i_writecount);
+}
+static inline bool inode_is_open_for_write(const struct inode *inode)
+{
+	return atomic_read(&inode->i_writecount) > 0;
+}
+
+#endif /* _LINUX_FS_API_RW_H */
