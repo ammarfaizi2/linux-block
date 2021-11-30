143abac6fd2d10c5f3386118289ef376a4d429c8
Ingo Molnar <mingo@kernel.org>
2021-11-30 17:23:31 +0100

    headers/deps: x86: Move native_safe_halt() and native_halt() from <asm/irqflags.h> to <asm/halt.h>
    These two functions are rarely used, and they bring in extra header dependencies.
    
    Move them to their separate <asm/halt.h> header.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/halt.h
+++ arch/x86/include/asm/halt.h
@@ -1,1 +1,45 @@
-#include <asm/processor.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _X86_HALT_H_
+#define _X86_HALT_H_
+
+#include <linux/cpu.h>
+
+#include <asm/nospec-branch.h>
+
+static inline __cpuidle void native_safe_halt(void)
+{
+	mds_idle_clear_cpu_buffers();
+	asm volatile("sti; hlt": : :"memory");
+}
+
+static inline __cpuidle void native_halt(void)
+{
+	mds_idle_clear_cpu_buffers();
+	asm volatile("hlt": : :"memory");
+}
+
+#ifdef CONFIG_PARAVIRT_XXL
+# include <asm/paravirt.h>
+#else
+
+/*
+ * Used in the idle loop; sti takes one instruction cycle
+ * to complete:
+ */
+static inline __cpuidle void arch_safe_halt(void)
+{
+	native_safe_halt();
+}
+
+/*
+ * Used when interrupts are already enabled or to
+ * shutdown the processor:
+ */
+static inline __cpuidle void halt(void)
+{
+	native_halt();
+}
+
+#endif /* CONFIG_PARAVIRT_XXL */
+
+#endif /* _X86_HALT_H_ */
--- arch/x86/include/asm/irqflags.h
+++ arch/x86/include/asm/irqflags.h
@@ -43,18 +43,6 @@ static __always_inline void native_irq_enable(void)
 	asm volatile("sti": : :"memory");
 }
 
-static inline __cpuidle void native_safe_halt(void)
-{
-	mds_idle_clear_cpu_buffers();
-	asm volatile("sti; hlt": : :"memory");
-}
-
-static inline __cpuidle void native_halt(void)
-{
-	mds_idle_clear_cpu_buffers();
-	asm volatile("hlt": : :"memory");
-}
-
 #endif
 
 #ifdef CONFIG_PARAVIRT_XXL
@@ -76,24 +64,6 @@ static __always_inline void arch_local_irq_disable(void)
 static __always_inline void arch_local_irq_enable(void)
 {
 	native_irq_enable();
-}
-
-/*
- * Used in the idle loop; sti takes one instruction cycle
- * to complete:
- */
-static inline __cpuidle void arch_safe_halt(void)
-{
-	native_safe_halt();
-}
-
-/*
- * Used when interrupts are already enabled or to
- * shutdown the processor:
- */
-static inline __cpuidle void halt(void)
-{
-	native_halt();
 }
 
 /*
