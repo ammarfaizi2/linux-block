baf2457615ad7b554eeb011dc2ca1004a9e1a7a7
Ingo Molnar <mingo@kernel.org>
2021-11-30 17:24:07 +0100

    headers/deps: fs: Introduce <linux/fs_api_fb_count.h> for f_count related APIs
    <linux/fs_api.h> is used in over 4,100 files, while the f_count
    related functions are only used by ~37 files.
    
    So move them over into their new <linux/fs_api_fb_count.h> header.
    
    The motivation is to allow the reduction of <linux/fs_api.h>
    header dependencies.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/fs_api_f_count.h
+++ include/linux/fs_api_f_count.h
@@ -1,1 +1,19 @@
-#include <linux/fs.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_FS_API_F_COUNT_H
+#define _LINUX_FS_API_F_COUNT_H
+
+#include <linux/fs_api.h>
+
+static inline struct file *get_file(struct file *f)
+{
+	atomic_long_inc(&f->f_count);
+	return f;
+}
+#define get_file_rcu_many(x, cnt)	\
+	atomic_long_add_unless(&(x)->f_count, (cnt), 0)
+
+#define get_file_rcu(x) get_file_rcu_many((x), 1)
+
+#define file_count(x)	atomic_long_read(&(x)->f_count)
+
+#endif /* _LINUX_FS_API_F_COUNT_H */
