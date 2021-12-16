0f37cb0a171eccbc967dbdd340fedc122575d6a3
Jens Axboe <axboe@kernel.dk>
2021-12-16 18:16:19 +0530

    io_uring: add support for IORING_OP_URING_CMD
    This is a file private kind of request. io_uring doesn't know what's
    in this command type, it's for the file_operations->async_cmd()
    handler to deal with.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/linux/io_uring.h
+++ include/linux/io_uring.h
@@ -5,7 +5,20 @@
 #include <linux/sched.h>
 #include <linux/xarray.h>
 
+/*
+ * Note that the first member here must be a struct file, as the
+ * io_uring command layout depends on that.
+ */
+struct io_uring_cmd {
+	struct file	*file;
+	__u16		op;
+	__u16		unused;
+	__u32		len;
+	__u64		pdu[5];	/* 40 bytes available inline for free use */
+};
+
 #if defined(CONFIG_IO_URING)
+void io_uring_cmd_done(struct io_uring_cmd *cmd, ssize_t ret);
 struct sock *io_uring_get_socket(struct file *file);
 void __io_uring_cancel(bool cancel_all);
 void __io_uring_free(struct task_struct *tsk);
@@ -26,6 +39,9 @@ static inline void io_uring_free(struct task_struct *tsk)
 		__io_uring_free(tsk);
 }
 #else
+static inline void io_uring_cmd_done(struct io_uring_cmd *cmd, ssize_t ret)
+{
+}
 static inline struct sock *io_uring_get_socket(struct file *file)
 {
 	return NULL;
