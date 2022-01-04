bd7300988b268a5d2dec71ab31289998b43f737b
Damien Le Moal <damien.lemoal@opensource.wdc.com>
2022-01-04 14:55:41 +0900

    ata: ahci_st: add compile test support
    Add Kconfig dependendy on COMPILE_TEST to allow compile tests with
    configs that do not enable ARCH_STI.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>
    Reviewed-by: Hannes Reinecke <hare@suse.de>

--- drivers/ata/ahci_st.c
+++ drivers/ata/ahci_st.c
@@ -230,7 +230,7 @@ static int st_ahci_resume(struct device *dev)
 
 static SIMPLE_DEV_PM_OPS(st_ahci_pm_ops, st_ahci_suspend, st_ahci_resume);
 
-static const struct of_device_id st_ahci_match[] = {
+static const struct of_device_id __maybe_unused st_ahci_match[] = {
 	{ .compatible = "st,ahci", },
 	{},
 };
