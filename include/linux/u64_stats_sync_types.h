269bfeeef9b0e814f8aafb3c917b83a976a36f20
Ingo Molnar <mingo@kernel.org>
2022-02-14 12:43:38 +0100

    headers/deps: locking: Split <linux/u64_stats_sync.h> into <linux/u64_stats_sync_types.h> and <linux/u64_stats_sync_api.h>
    <linux/u64_stats_sync.h> is included in a lot of other headers.
    
    In order to reduce this impact, we are splitting it into three parts:
    
     - <linux/u64_stats_sync_types.h>: lightweight header with only type definitions
                                       and initializers.
    
     - <linux/u64_stats_sync_api.h>: API declarations and inline functions. This header
                                     is inevitably more heavyweight.
    
     - <linux/u64_stats_sync.h>: Simple default header that right now includes both
                                 the types and the API headers, but will only
                                 use the types header once all affected code
                                 is converted.
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/u64_stats_sync_types.h
+++ include/linux/u64_stats_sync_types.h
@@ -1,1 +1,17 @@
-#include <linux/u64_stats_sync.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_U64_STATS_SYNC_TYPES_H
+#define _LINUX_U64_STATS_SYNC_TYPES_H
+
+#include <asm/bitsperlong.h>
+
+#if BITS_PER_LONG==32 && defined(CONFIG_SMP)
+#include <linux/seqlock_types.h>
+#endif
+
+struct u64_stats_sync {
+#if BITS_PER_LONG == 32 && (defined(CONFIG_SMP) || defined(CONFIG_PREEMPT_RT))
+	seqcount_t	seq;
+#endif
+};
+
+#endif /* _LINUX_U64_STATS_SYNC_TYPES_H */
