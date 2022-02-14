929709a94562f82862e01db7450887f1056a813e
Ingo Molnar <mingo@kernel.org>
2022-02-14 11:19:13 +0100

    headers/deps: mm: Split out the <linux/mm_api_truncate.h> APIs from <linux/mm_api.h>
    These are the APIs defined in mm/truncate.c, and used the filesystem code.
    
    Users often don't need the full-blown <linux/mm_api.h> header to
    call these methods.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/mm_api.h
+++ include/linux/mm_api.h
@@ -6,6 +6,7 @@
 
 #include <linux/mm_types.h>
 #include <linux/mm_api_gate_area.h>
+#include <linux/mm_api_truncate.h>
 
 #include <linux/cpumask_api.h>
 #include <linux/topology.h>
@@ -1461,13 +1462,6 @@ int follow_phys(struct vm_area_struct *vma, unsigned long address,
 int generic_access_phys(struct vm_area_struct *vma, unsigned long addr,
 			void *buf, int len, int write);
 
-extern void truncate_pagecache(struct inode *inode, loff_t new);
-extern void truncate_setsize(struct inode *inode, loff_t newsize);
-void pagecache_isize_extended(struct inode *inode, loff_t from, loff_t to);
-void truncate_pagecache_range(struct inode *inode, loff_t offset, loff_t end);
-int generic_error_remove_page(struct address_space *mapping, struct page *page);
-int invalidate_inode_page(struct page *page);
-
 #ifdef CONFIG_MMU
 extern vm_fault_t handle_mm_fault(struct vm_area_struct *vma,
 				  unsigned long address, unsigned int flags,
@@ -2131,12 +2125,6 @@ struct vm_unmapped_area_info {
 };
 
 extern unsigned long vm_unmapped_area(struct vm_unmapped_area_info *info);
-
-/* truncate.c */
-extern void truncate_inode_pages(struct address_space *, loff_t);
-extern void truncate_inode_pages_range(struct address_space *,
-				       loff_t lstart, loff_t lend);
-extern void truncate_inode_pages_final(struct address_space *);
 
 /* generic vm_area_ops exported for stackable file systems */
 extern vm_fault_t filemap_fault(struct vm_fault *vmf);
--- include/linux/mm_api_truncate.h
+++ include/linux/mm_api_truncate.h
@@ -1,1 +1,24 @@
-#include <linux/mm.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_MM_API_TRUNCATE_H
+#define _LINUX_MM_API_TRUNCATE_H
+
+#include <linux/types.h>
+
+struct address_space;
+struct inode;
+struct page;
+
+/* truncate.c */
+extern void truncate_inode_pages(struct address_space *, loff_t);
+extern void truncate_inode_pages_range(struct address_space *,
+				       loff_t lstart, loff_t lend);
+extern void truncate_inode_pages_final(struct address_space *);
+
+extern void truncate_pagecache(struct inode *inode, loff_t new);
+extern void truncate_setsize(struct inode *inode, loff_t newsize);
+void pagecache_isize_extended(struct inode *inode, loff_t from, loff_t to);
+void truncate_pagecache_range(struct inode *inode, loff_t offset, loff_t end);
+int generic_error_remove_page(struct address_space *mapping, struct page *page);
+int invalidate_inode_page(struct page *page);
+
+#endif /* _LINUX_MM_API_TRUNCATE_H */
