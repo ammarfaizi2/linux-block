ca2c27ddf94cd39297279964d9483458aa650977
Ingo Molnar <mingo@kernel.org>
2022-02-14 12:19:26 +0100

    headers/deps: iov: Split <linux/uio.h> into <linux/uio_types.h> and <linux/uio_api.h>
    <linux/uio.h> is included in a lot of other headers.
    
    In order to reduce this impact, we are splitting it into three parts:
    
     - <linux/uio_types.h>: lightweight header with only type definitions
                            and initializers.
    
     - <linux/uio_api.h>: API declarations and inline functions. This header
                          is a bit larger.
    
     - <linux/uio.h>: Simple default header that right now includes both
                      the types and the API headers, but will only
                      use the types header once all affected code
                      is converted.
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              | #include <linux/uio_api.h>              | LOC:   972 | headers:   57
              | #include <linux/uio_types.h>            | LOC:   266 | headers:   31
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/uio_types.h
+++ include/linux/uio_types.h
@@ -1,1 +1,57 @@
-#include <linux/uio.h>
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ *	Berkeley style UIO structures	-	Alan Cox 1994.
+ */
+#ifndef __LINUX_UIO_TYPES_H
+#define __LINUX_UIO_TYPES_H
+
+#include <uapi/linux/uio.h>
+
+struct page;
+struct pipe_inode_info;
+
+struct kvec {
+	void *iov_base; /* and that should *never* hold a userland pointer */
+	size_t iov_len;
+};
+
+enum iter_type {
+	/* iter types */
+	ITER_IOVEC,
+	ITER_KVEC,
+	ITER_BVEC,
+	ITER_PIPE,
+	ITER_XARRAY,
+	ITER_DISCARD,
+};
+
+struct iov_iter_state {
+	size_t iov_offset;
+	size_t count;
+	unsigned long nr_segs;
+};
+
+struct iov_iter {
+	u8 iter_type;
+	bool nofault;
+	bool data_source;
+	size_t iov_offset;
+	size_t count;
+	union {
+		const struct iovec *iov;
+		const struct kvec *kvec;
+		const struct bio_vec *bvec;
+		struct xarray *xarray;
+		struct pipe_inode_info *pipe;
+	};
+	union {
+		unsigned long nr_segs;
+		struct {
+			unsigned int head;
+			unsigned int start_head;
+		};
+		loff_t xarray_start;
+	};
+};
+
+#endif
