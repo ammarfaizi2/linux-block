e063a17a53dcc7063b71c5f8d700232b9dcf73b3
Ingo Molnar <mingo@kernel.org>
2022-02-14 12:27:44 +0100

    headers/deps: PM/core: Optimize <linux/pm.h> dependencies
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
      before: | #include <linux/pm.h>                   | LOC: 2,968 | headers:   93
       after: | #include <linux/pm.h>                   | LOC:   881 | headers:   67
    
    This header is part of the <linux/tcp.h> and <linux/device*.h> superheader
    dependency chains.
    
    Fix up dangling dependencies.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/pm.h
+++ include/linux/pm.h
@@ -8,11 +8,11 @@
 #ifndef _LINUX_PM_H
 #define _LINUX_PM_H
 
-#include <linux/mutex.h>
 #include <linux/export.h>
-#include <linux/workqueue.h>
+#include <linux/mutex_types.h>
+#include <linux/workqueue_types.h>
 #include <linux/wait_types.h>
-#include <linux/hrtimer.h>
+#include <linux/hrtimer_types.h>
 #include <linux/completion.h>
 
 /*
