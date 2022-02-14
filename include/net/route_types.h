80c63ad350df75c32183a588a3702cd81d6198de
Ingo Molnar <mingo@kernel.org>
2022-02-14 12:10:08 +0100

    headers/deps: net: Split <net/route.h> into <net/route_types.h> and <net/route_api.h>
    <net/route.h> is included in almost 2,000 .c files indirectly.
    
    In order to reduce this impact, we are splitting it into three parts:
    
     - <net/route_types.h>: lightweight header with only type definitions
                            and initializers.
    
     - <net/route_api.h>: API declarations and inline functions. This header
                          is inevitably more heavyweight.
    
     - <net/route.h>: Simple default header that right now includes both
                      the types and the API headers, but will only
                      use the types header once all affected code
                      is converted.
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/net/route_types.h
+++ include/net/route_types.h
@@ -1,1 +1,90 @@
-#include <net/route.h>
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * INET		An implementation of the TCP/IP protocol suite for the LINUX
+ *		operating system.  INET  is implemented using the  BSD Socket
+ *		interface as the means of communication with the user level.
+ *
+ *		Definitions for the IP router.
+ *
+ * Version:	@(#)route.h	1.0.4	05/27/93
+ *
+ * Authors:	Ross Biro
+ *		Fred N. van Kempen, <waltje@uWalt.NL.Mugnet.ORG>
+ * Fixes:
+ *		Alan Cox	:	Reformatted. Added ip_rt_local()
+ *		Alan Cox	:	Support for TCP parameters.
+ *		Alexey Kuznetsov:	Major changes for new routing code.
+ *		Mike McLagan    :	Routing by source
+ *		Robert Olsson   :	Added rt_cache statistics
+ */
+#ifndef _ROUTE_TYPES_H
+#define _ROUTE_TYPES_H
+
+#include <net/dst.h>
+#include <net/inetpeer.h>
+#include <net/flow.h>
+#include <net/inet_sock.h>
+#include <net/ip_fib.h>
+#include <net/arp.h>
+#include <net/ndisc.h>
+#include <linux/in_route.h>
+#include <linux/rtnetlink.h>
+#include <linux/rcupdate.h>
+#include <linux/route.h>
+#include <linux/ip.h>
+#include <linux/cache.h>
+#include <linux/security.h>
+
+/* IPv4 datagram length is stored into 16bit field (tot_len) */
+#define IP_MAX_MTU	0xFFFFU
+
+#define RTO_ONLINK	0x01
+
+struct fib_nh;
+struct fib_info;
+struct uncached_list;
+struct rtable {
+	struct dst_entry	dst;
+
+	int			rt_genid;
+	unsigned int		rt_flags;
+	__u16			rt_type;
+	__u8			rt_is_input;
+	__u8			rt_uses_gateway;
+
+	int			rt_iif;
+
+	u8			rt_gw_family;
+	/* Info on neighbour */
+	union {
+		__be32		rt_gw4;
+		struct in6_addr	rt_gw6;
+	};
+
+	/* Miscellaneous cached information */
+	u32			rt_mtu_locked:1,
+				rt_pmtu:31;
+
+	struct list_head	rt_uncached;
+	struct uncached_list	*rt_uncached_list;
+};
+
+struct ip_rt_acct {
+	__u32 	o_bytes;
+	__u32 	o_packets;
+	__u32 	i_bytes;
+	__u32 	i_packets;
+};
+
+struct rt_cache_stat {
+        unsigned int in_slow_tot;
+        unsigned int in_slow_mc;
+        unsigned int in_no_route;
+        unsigned int in_brd;
+        unsigned int in_martian_dst;
+        unsigned int in_martian_src;
+        unsigned int out_slow_tot;
+        unsigned int out_slow_mc;
+};
+
+#endif	/* _ROUTE_TYPES_H */
