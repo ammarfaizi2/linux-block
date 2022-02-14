f2e7f832ee090cbbba9b17ba45e1c8f52940ed92
Ingo Molnar <mingo@kernel.org>
2022-02-14 12:48:16 +0100

    headers/deps: mm: Move the __pa_symbol() and lm_alias() defaults to <linux/mm_api.h>
    <linux/mm_types.h>, <linux/mm_types_task.h> and <linux/mm_page_address.h> wants
    to avoid including <asm/page.h>, but cannot do this if <linux/mm_page_address.h>
    provides defaults for these two macros.
    
    Move it to <linux/mm_api.h> instead - which is more appropriate anyway, as these
    are rarely used API.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/mm_page_address.h
+++ include/linux/mm_page_address.h
@@ -24,16 +24,8 @@ extern struct page *mem_map;
 #define untagged_addr(addr) (addr)
 #endif
 
-#ifndef __pa_symbol
-#define __pa_symbol(x)  __pa(RELOC_HIDE((unsigned long)(x), 0))
-#endif
-
 #ifndef page_to_virt
 #define page_to_virt(x)	__va(PFN_PHYS(page_to_pfn(x)))
-#endif
-
-#ifndef lm_alias
-#define lm_alias(x)	__va(__pa_symbol(x))
 #endif
 
 #define lowmem_page_address(page) page_to_virt(page)
