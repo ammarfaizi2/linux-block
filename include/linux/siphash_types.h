3afa96ff94ba10f00eea78deb17150e59e5c6475
Ingo Molnar <mingo@kernel.org>
2022-02-14 12:17:08 +0100

    headers/deps: net: Split <linux/siphash.h> into <linux/siphash_types.h> and <linux/siphash_api.h>
    <linux/siphash.h> is included in a lot of other headers.
    
    In order to reduce this impact, we are splitting it into three parts:
    
     - <linux/siphash_types.h>: lightweight header with only type definitions
                                and initializers.
    
     - <linux/siphash_api.h>: API declarations and inline functions. This header
                              is inevitably more heavyweight.
    
     - <linux/siphash.h>: Simple default header that right now includes both
                          the types and the API headers, but will only
                          use the types header once all affected code
                          is converted.
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/siphash_types.h
+++ include/linux/siphash_types.h
@@ -1,1 +1,26 @@
-#include <linux/siphash.h>
+/* Copyright (C) 2016 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
+ *
+ * This file is provided under a dual BSD/GPLv2 license.
+ *
+ * SipHash: a fast short-input PRF
+ * https://131002.net/siphash/
+ *
+ * This implementation is specifically for SipHash2-4 for a secure PRF
+ * and HalfSipHash1-3/SipHash1-3 for an insecure PRF only suitable for
+ * hashtables.
+ */
+
+#ifndef _LINUX_SIPHASH_TYPES_H
+#define _LINUX_SIPHASH_TYPES_H
+
+#include <linux/types.h>
+
+#define SIPHASH_ALIGNMENT __alignof__(u64)
+
+typedef struct {
+	u64 key[2];
+} siphash_key_t;
+
+#define siphash_aligned_key_t siphash_key_t __aligned(16)
+
+#endif /* _LINUX_SIPHASH_TYPES_H */
