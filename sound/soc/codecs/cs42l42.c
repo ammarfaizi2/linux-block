29026548245b5d0bd1776d4d50155f366006801b
Martin Povišer <povik+lin@cutebit.org>
2022-02-19 09:49:48 +0100

    HACK: ASoC: cs42l42: Disable regcache
    There's some issue that has yet to be pinned down.

--- sound/soc/codecs/cs42l42.c
+++ sound/soc/codecs/cs42l42.c
@@ -380,7 +380,7 @@ static const struct regmap_config cs42l42_regmap = {
 	.max_register = CS42L42_MAX_REGISTER,
 	.reg_defaults = cs42l42_reg_defaults,
 	.num_reg_defaults = ARRAY_SIZE(cs42l42_reg_defaults),
-	.cache_type = REGCACHE_RBTREE,
+	//.cache_type = REGCACHE_RBTREE,
 
 	.use_single_read = true,
 	.use_single_write = true,
