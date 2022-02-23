ff4f2b31c4fd9ea1f11f1c072f57611d01dcc7c4
Ingo Molnar <mingo@kernel.org>
2022-02-23 10:40:46 +0100

    headers/deps: net: Move seqlock related methods from <net/neighbour.h> to <net/neighbour_api_seqlock.h>
    Decouple <net/neighbour.h> from <linux/seqlock_api.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/net/neighbour_api_seqlock.h
+++ include/net/neighbour_api_seqlock.h
@@ -1,1 +1,32 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _NET_NEIGHBOUR_API_SEQLOCK_H
+#define _NET_NEIGHBOUR_API_SEQLOCK_H
+
 #include <net/neighbour.h>
+
+#if IS_ENABLED(CONFIG_BRIDGE_NETFILTER)
+static inline int neigh_hh_bridge(struct hh_cache *hh, struct sk_buff *skb)
+{
+	unsigned int seq, hh_alen;
+
+	do {
+		seq = read_seqbegin(&hh->hh_lock);
+		hh_alen = HH_DATA_ALIGN(ETH_HLEN);
+		memcpy(skb->data - hh_alen, hh->hh_data, ETH_ALEN + hh_alen - ETH_HLEN);
+	} while (read_seqretry(&hh->hh_lock, seq));
+	return 0;
+}
+#endif
+
+static inline void neigh_ha_snapshot(char *dst, const struct neighbour *n,
+				     const struct net_device *dev)
+{
+	unsigned int seq;
+
+	do {
+		seq = read_seqbegin(&n->ha_lock);
+		memcpy(dst, n->ha, dev->addr_len);
+	} while (read_seqretry(&n->ha_lock, seq));
+}
+
+#endif /* _NET_NEIGHBOUR_API_SEQLOCK_H */
