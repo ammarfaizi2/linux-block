570dea6aebdf631be0af67756ea2cd5ce3ac87f1
Ingo Molnar <mingo@kernel.org>
2022-02-23 10:32:55 +0100

    headers/deps: mm: Split <linux/pgtable.h> into <linux/pgtable_types.h> and <linux/pgtable_api.h>
    <linux/pgtable.h> is a large header, included in a lot of other headers
    in a spurious fashion:
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              | #include <linux/pgtable.h>                     | LOC: 10831 | headers: 281 |
    
    In order to reduce this impact, we are going to split the header into
    three parts:
    
     - <linux/pgtable_types.h>: lightweight header with only type definitions
                                and initializers.
    
     - <linux/pgtable_api.h>: API declarations and inline functions. This header
                              is inevitably more heavyweight.
    
     - <linux/pgtable.h>: Simple default header that right now includes both
                          the types and the API headers, but will only
                          use the types header once all affected code
                          is converted.
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/pgtable_types.h
+++ include/linux/pgtable_types.h
@@ -1,1 +1,32 @@
-#include <linux/pgtable.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_PGTABLE_TYPES_H
+#define _LINUX_PGTABLE_TYPES_H
+
+
+#include <asm/pgtable_types.h>
+
+#ifndef __ASSEMBLY__
+
+#include <linux/pfn.h>
+
+/*
+ * Architecture PAGE_KERNEL_* fallbacks
+ *
+ * Some architectures don't define certain PAGE_KERNEL_* flags. This is either
+ * because they really don't support them, or the port needs to be updated to
+ * reflect the required functionality. Below are a set of relatively safe
+ * fallbacks, as best effort, which we can count on in lieu of the architectures
+ * not defining them on their own yet.
+ */
+
+#ifndef PAGE_KERNEL_RO
+# define PAGE_KERNEL_RO PAGE_KERNEL
+#endif
+
+#ifndef PAGE_KERNEL_EXEC
+# define PAGE_KERNEL_EXEC PAGE_KERNEL
+#endif
+
+#endif /* !__ASSEMBLY__ */
+
+#endif /* _LINUX_PGTABLE_TYPES_H */
