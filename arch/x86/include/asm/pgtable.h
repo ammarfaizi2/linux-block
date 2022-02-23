c1f2cb587d0f5f33cc62f20e0da7d8a7a309c6ca
Ingo Molnar <mingo@kernel.org>
2022-02-23 08:00:39 +0100

    headers/prep: x86/mm: Move arch_memory_failure() and arch_is_platform_page() definitions from <asm/processor.h> to <asm/pgtable.h>
    <linux/mm.h> relies on these definitions being included first,
    but <asm/processor.h> is not guaranteed to be included by the MM code.
    
    Move the definitions over into a suitable MM header.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/pgtable.h
+++ arch/x86/include/asm/pgtable.h
@@ -1429,6 +1429,14 @@ static inline bool arch_faults_on_old_pte(void)
 	return false;
 }
 
+#ifdef CONFIG_X86_SGX
+int arch_memory_failure(unsigned long pfn, int flags);
+#define arch_memory_failure arch_memory_failure
+
+bool arch_is_platform_page(u64 paddr);
+#define arch_is_platform_page arch_is_platform_page
+#endif
+
 #endif	/* __ASSEMBLY__ */
 
 #endif /* _ASM_X86_PGTABLE_H */
