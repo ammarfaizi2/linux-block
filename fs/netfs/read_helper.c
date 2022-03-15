4c2dfa311a0ca2def1107c3e48f976fdc615311d
Ingo Molnar <mingo@kernel.org>
2022-03-15 11:45:23 +0100

    headers/deps: Add header dependencies to .c files: <linux/cacheflush.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- fs/netfs/read_helper.c
+++ fs/netfs/read_helper.c
@@ -5,6 +5,7 @@
  * Written by David Howells (dhowells@redhat.com)
  */
 
+#include <linux/cacheflush.h>
 #include <linux/atomic_api.h>
 #include <asm/cacheflush.h>
 #include <linux/sched/cond_resched.h>
