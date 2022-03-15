c35322576ec6d5460904918e4f8a493ddcaa83c6
Ingo Molnar <mingo@kernel.org>
2022-03-15 11:45:22 +0100

    headers/deps: Add header dependencies to .c files: <linux/bpf.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- drivers/net/ethernet/intel/igc/igc_xdp.c
+++ drivers/net/ethernet/intel/igc/igc_xdp.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /* Copyright (c) 2020, Intel Corporation. */
 
+#include <linux/bpf.h>
 #include <linux/atomic_api.h>
 #include <linux/if_vlan.h>
 #include <net/xdp_sock_drv.h>
