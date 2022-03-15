9968680e8fdbdd2a6bcbd60465bae3feecb2df1c
Ingo Molnar <mingo@kernel.org>
2022-03-15 11:40:34 +0100

    headers/deps: Add header dependencies to .h files: <asm/percpu.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/cpu_entry_area.h
+++ arch/x86/include/asm/cpu_entry_area.h
@@ -3,6 +3,7 @@
 #ifndef _ASM_X86_CPU_ENTRY_AREA_H
 #define _ASM_X86_CPU_ENTRY_AREA_H
 
+#include <asm/percpu.h>
 #include <linux/percpu-defs.h>
 #include <asm/processor.h>
 #include <asm/intel_ds.h>
