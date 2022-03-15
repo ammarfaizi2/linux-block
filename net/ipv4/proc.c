0306839620a9fc9408dd4ce964c5bac3dc7d3e38
Ingo Molnar <mingo@kernel.org>
2022-03-15 11:54:42 +0100

    headers/deps: Add header dependencies to .c files: <net/inet_frag.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- net/ipv4/proc.c
+++ net/ipv4/proc.c
@@ -27,6 +27,7 @@
  *	Andi Kleen		:	Add support for /proc/net/netstat
  *	Arnaldo C. Melo		:	Convert to seq_file
  */
+#include <net/inet_frag.h>
 #include <linux/slab.h>
 #include <linux/preempt.h>
 #include <linux/percpu_counter_api.h>
