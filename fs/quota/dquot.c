24e9ebffa64206238184fa441063c03d4aaba9fa
Ingo Molnar <mingo@kernel.org>
2022-03-15 11:51:26 +0100

    headers/deps: Add header dependencies to .c files: <linux/quota.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- fs/quota/dquot.c
+++ fs/quota/dquot.c
@@ -54,6 +54,7 @@
  * (C) Copyright 1994 - 1997 Marco van Wieringen
  */
 
+#include <linux/quota.h>
 #include <linux/percpu_counter_api.h>
 #include <linux/notifier_api.h>
 #include <linux/mutex_api.h>
