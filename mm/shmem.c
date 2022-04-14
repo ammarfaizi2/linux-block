382cbc6bcfed5f3427a721f82745df5ba82968c7
Ingo Molnar <mingo@kernel.org>
2022-04-14 13:26:59 +0200

    headers/deps: Add header dependencies to .c files: <linux/swap_api_device.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- mm/shmem.c
+++ mm/shmem.c
@@ -21,6 +21,7 @@
  * This file is released under the GPL.
  */
 
+#include <linux/swap_api_device.h>
 #include <linux/spinlock_api.h>
 #include <linux/smp_api.h>
 #include <linux/sched/thread_info_api.h>
--- mm/swapfile.c
+++ mm/swapfile.c
@@ -6,6 +6,7 @@
  *  Swap reorganised 29.12.95, Stephen Tweedie
  */
 
+#include <linux/swap_api_device.h>
 #include <linux/spinlock_api.h>
 #include <linux/smp_api.h>
 #include <linux/sched/signal.h>
