872e939839847916e0318dc7ee61bdcf5d72a564
Ingo Molnar <mingo@kernel.org>
2022-04-14 13:28:34 +0200

    headers/deps: Add header dependencies to .c files: <uapi/linux/magic.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- fs/ksmbd/smb2pdu.c
+++ fs/ksmbd/smb2pdu.c
@@ -4,6 +4,7 @@
  *   Copyright (C) 2018 Samsung Electronics Co., Ltd.
  */
 
+#include <uapi/linux/magic.h>
 #include <uapi/linux/if_arp.h>
 #include <linux/wait_api.h>
 #include <linux/spinlock_api.h>
