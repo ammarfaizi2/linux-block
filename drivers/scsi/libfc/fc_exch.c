b458e24d4f331a1ca980ecddc6537088ae8c3819
Ingo Molnar <mingo@kernel.org>
2022-04-14 13:18:19 +0200

    headers/deps: Add header dependencies to .c files: <linux/cache.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- drivers/scsi/libfc/fc_exch.c
+++ drivers/scsi/libfc/fc_exch.c
@@ -11,6 +11,7 @@
  * Fibre Channel exchange and sequence handling.
  */
 
+#include <linux/cache.h>
 #include <linux/atomic_api.h>
 #include <linux/timer.h>
 #include <linux/slab.h>
--- drivers/scsi/virtio_scsi.c
+++ drivers/scsi/virtio_scsi.c
@@ -12,6 +12,7 @@
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
+#include <linux/cache.h>
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/mempool.h>
