d9fe1df49e5016d8f585a8ab7e9b0512e2abbadf
Ingo Molnar <mingo@kernel.org>
2022-04-14 13:13:57 +0200

    headers/deps: Add header dependencies to .h files: <linux/numa_types.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/kthread.h
+++ include/linux/kthread.h
@@ -2,6 +2,7 @@
 #ifndef _LINUX_KTHREAD_H
 #define _LINUX_KTHREAD_H
 /* Simple interface for creating and stopping kernel threads without mess. */
+#include <linux/numa_types.h>
 #include <linux/timer.h>
 #include <linux/err.h>
 #include <linux/sched.h>
