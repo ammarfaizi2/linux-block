c79e762b7a77a5554f8e195e388236d0a18d14c7
Ingo Molnar <mingo@kernel.org>
2022-04-14 13:26:52 +0200

    headers/deps: Add header dependencies to .c files: <linux/stackprotector.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/kernel/smpboot.c
+++ arch/x86/kernel/smpboot.c
@@ -39,6 +39,7 @@
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
+#include <linux/stackprotector.h>
 #include <linux/spinlock_api.h>
 #include <linux/sched/thread_info_api.h>
 #include <linux/sched/thread.h>
