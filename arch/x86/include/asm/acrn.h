a15b1f1062bd38435528fad6ca712f3ef363d154
Ingo Molnar <mingo@kernel.org>
2022-04-14 13:13:11 +0200

    headers/deps: Add header dependencies to .h files: <asm/processor_api.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/acrn.h
+++ arch/x86/include/asm/acrn.h
@@ -2,6 +2,7 @@
 #ifndef _ASM_X86_ACRN_H
 #define _ASM_X86_ACRN_H
 
+#include <asm/processor_api.h>
 #include <asm/cpufeature.h>
 #include <linux/types.h>
 
--- arch/x86/include/asm/microcode.h
+++ arch/x86/include/asm/microcode.h
@@ -2,6 +2,7 @@
 #ifndef _ASM_X86_MICROCODE_H
 #define _ASM_X86_MICROCODE_H
 
+#include <asm/processor_api.h>
 #include <asm/cpu.h>
 #include <linux/earlycpio.h>
 #include <linux/initrd.h>
