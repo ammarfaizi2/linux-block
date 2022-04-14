74ce1053c56eb05af29d72ee65c127f36d4dc4b2
Ingo Molnar <mingo@kernel.org>
2022-04-14 13:13:35 +0200

    headers/deps: Add header dependencies to .h files: <linux/indirect_call_wrapper.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/net/ip_vs.h
+++ include/net/ip_vs.h
@@ -6,6 +6,7 @@
 #ifndef _NET_IP_VS_H
 #define _NET_IP_VS_H
 
+#include <linux/indirect_call_wrapper.h>
 #include <linux/ip_vs.h>                /* definitions shared with userland */
 
 #include <asm/types.h>                  /* for __uXX types */
