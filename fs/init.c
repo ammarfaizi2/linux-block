4add01a9cf8b6d6d135ce2bba49a45b84485710c
Ingo Molnar <mingo@kernel.org>
2022-04-14 12:20:31 +0200

    headers/uninline: Uninline multi-use function: elf_core_*() methods
    Decouple <linux/elfcore.h> from <linux/ptrace.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- fs/init.c
+++ fs/init.c
@@ -3,6 +3,7 @@
  * Routines that mimic syscalls, but don't use the user address space or file
  * descriptors.  Only for init/ and related early init code.
  */
+#include <linux/elfcore.h>
 #include <linux/init.h>
 #include <linux/mount.h>
 #include <linux/namei.h>
@@ -267,3 +268,46 @@ int __init init_dup(struct file *file)
 	fd_install(fd, get_file(file));
 	return 0;
 }
+
+void elf_core_copy_regs(elf_gregset_t *elfregs, struct pt_regs *regs)
+{
+#ifdef ELF_CORE_COPY_REGS
+	ELF_CORE_COPY_REGS((*elfregs), regs)
+#else
+	BUG_ON(sizeof(*elfregs) != sizeof(*regs));
+	*(struct pt_regs *)elfregs = *regs;
+#endif
+}
+
+void elf_core_copy_kernel_regs(elf_gregset_t *elfregs, struct pt_regs *regs)
+{
+#ifdef ELF_CORE_COPY_KERNEL_REGS
+	ELF_CORE_COPY_KERNEL_REGS((*elfregs), regs);
+#else
+	elf_core_copy_regs(elfregs, regs);
+#endif
+}
+
+int elf_core_copy_task_regs(struct task_struct *t, elf_gregset_t* elfregs)
+{
+#if defined (ELF_CORE_COPY_TASK_REGS)
+	return ELF_CORE_COPY_TASK_REGS(t, elfregs);
+#elif defined (task_pt_regs)
+	elf_core_copy_regs(elfregs, task_pt_regs(t));
+#endif
+	return 0;
+}
+
+#ifndef CORE_DUMP_USE_REGSET
+
+extern int dump_fpu (struct pt_regs *, elf_fpregset_t *);
+
+int elf_core_copy_task_fpregs(struct task_struct *t, struct pt_regs *regs, elf_fpregset_t *fpu)
+{
+#ifdef ELF_CORE_COPY_FPREGS
+	return ELF_CORE_COPY_FPREGS(t, fpu);
+#else
+	return dump_fpu(regs, fpu);
+#endif
+}
+#endif /* CORE_DUMP_USE_REGSET */
