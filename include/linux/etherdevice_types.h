516b1cae676b72995d98aaa264c5989ba0a2c00a
Ingo Molnar <mingo@kernel.org>
2022-04-15 11:34:47 +0200

    headers/deps: net: Split <linux/etherdevice.h> into <linux/etherdevice_types.h> and <linux/etherdevice_api.h>
    Create a more lightweight <linux/etherdevice_types.h> header, to which
    we'll migrate more and more code.
    
    No change in functionality yet.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/etherdevice_types.h
+++ include/linux/etherdevice_types.h
@@ -1,1 +1,27 @@
-#include <linux/etherdevice.h>
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * INET		An implementation of the TCP/IP protocol suite for the LINUX
+ *		operating system.  NET  is implemented using the  BSD Socket
+ *		interface as the means of communication with the user level.
+ *
+ *		Definitions for the Ethernet handlers.
+ *
+ * Version:	@(#)eth.h	1.0.4	05/13/93
+ *
+ * Authors:	Ross Biro
+ *		Fred N. van Kempen, <waltje@uWalt.NL.Mugnet.ORG>
+ *
+ *		Relocated to include/linux where it belongs by Alan Cox
+ *							<gw4pts@gw4pts.ampr.org>
+ */
+#ifndef _LINUX_ETHERDEVICE_TYPES_H
+#define _LINUX_ETHERDEVICE_TYPES_H
+
+#include <linux/if_ether.h>
+#include <linux/netdevice_api.h>
+#include <linux/random.h>
+#include <linux/crc32.h>
+#include <asm/unaligned.h>
+#include <asm/bitsperlong.h>
+
+#endif	/* _LINUX_ETHERDEVICE_TYPES_H */
