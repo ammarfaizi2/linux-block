104346db362fa6a1753e720fa6d00ad6e3abec13
Ingo Molnar <mingo@kernel.org>
2022-04-18 08:56:35 +0200

    headers/deps: net: Split <net/ndisc.h> into <net/ndisc_types.h> and <net/ndisc_api.h>
    <net/ndisc.h> is included over 2,300 times in other headers.
    
    In order to reduce its impact, we are splitting it into three parts:
    
     - <net/ndisc_types.h>: lightweight header with only type definitions
                            and initializers.
    
     - <net/ndisc_api.h>: API declarations and inline functions. This header
                          is inevitably more heavyweight.
    
     - <net/ndisc.h>: Simple default header that right now includes both
                      the types and the API headers, but will only
                      use the types header once all affected code
                      is converted.
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/net/ndisc_types.h
+++ include/net/ndisc_types.h
@@ -1,1 +1,18 @@
-#include <net/ndisc.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _NDISC_TYPES_H
+#define _NDISC_TYPES_H
+
+#include <net/ipv6_stubs.h>
+
+#include <linux/net.h>
+#include <linux/compiler.h>
+#include <linux/icmpv6.h>
+#include <linux/in6.h>
+#include <linux/types.h>
+#include <linux/if_arp.h>
+#include <linux/netdevice.h>
+#include <linux/hash.h>
+
+#include <net/neighbour.h>
+
+#endif
