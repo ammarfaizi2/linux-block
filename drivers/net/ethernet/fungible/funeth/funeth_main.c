d7037234e5354225d8326e6b131c7280dc0ab014
Ingo Molnar <mingo@kernel.org>
2022-04-19 12:59:35 +0200

    headers/deps: Add header dependencies to .c files: <linux/etherdevice_api_addr.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- drivers/net/ethernet/fungible/funeth/funeth_main.c
+++ drivers/net/ethernet/fungible/funeth/funeth_main.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: (GPL-2.0-only OR BSD-3-Clause)
 
+#include <linux/etherdevice_api_addr.h>
 #include <linux/netdevice_api_lock.h>
 #include <linux/uaccess.h>
 #include <linux/topology.h>
