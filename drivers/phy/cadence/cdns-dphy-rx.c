55c1ea72f69d15bbbf6cdba59fe5ddadc57e3745
Ingo Molnar <mingo@kernel.org>
2022-04-19 12:54:16 +0200

    headers/deps: Add header dependencies to .c files: <linux/mod_devicetable.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- drivers/phy/cadence/cdns-dphy-rx.c
+++ drivers/phy/cadence/cdns-dphy-rx.c
@@ -3,6 +3,7 @@
  * Copyright (C) 2022 Texas Instruments Incorporated - https://www.ti.com/
  */
 
+#include <linux/mod_devicetable.h>
 #include <linux/string.h>
 #include <linux/device_api_lock.h>
 #include <linux/bitfield.h>
