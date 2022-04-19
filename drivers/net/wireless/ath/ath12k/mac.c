366dce11a06429654136e6350d6aff341e100633
P Praneesh <quic_ppranees@quicinc.com>
2022-04-19 13:00:50 +0530

    ath12k: update signal filled flag during sta_stats drv op
    Peer RSSI information is updated to station dump from driver
    sta_statistics mac op. Since the NL80211_STA_INFO_SIGNAL filled
    flag is not set in station, signal related information is not updated
    properly. Hence update this flag while filling the RSSI info.
    
    Signed-off-by: P Praneesh <quic_ppranees@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>

--- drivers/net/wireless/ath/ath12k/mac.c
+++ drivers/net/wireless/ath/ath12k/mac.c
@@ -6343,6 +6343,7 @@ static void ath12k_mac_op_sta_statistics(struct ieee80211_hw *hw,
 
 	/* TODO: Use real NF instead of default one. */
 	sinfo->signal = arsta->rssi_comb + ATH12K_DEFAULT_NOISE_FLOOR;
+	sinfo->filled |= BIT_ULL(NL80211_STA_INFO_SIGNAL);
 }
 
 static const struct ieee80211_ops ath12k_ops = {
