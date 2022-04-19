vm03/msm8916-mainline-linux

headers/prep: net: Fix fs/compat_binfmt_elf.c vs. <linux/skbuff.h> type ordering assumption
If we include <net/skbuff.h> too late (after ELF_COMPAT gets defined), we can get into this build failure:

   fs/compat_binfmt_elf.c:58:34: error: incompatible types when assigning to type ‘struct __kernel_old_timeval’ from type ‘struct old_timeval32’

Make sure we define the skbuff methods before the ELF compat #ifdeffery.

Signed-off-by: Ingo Molnar <mingo@kernel.org>

fs/compat_binfmt_elf.c
@@ -16,6 +16,7 @@
 
 #include <linux/elfcore-compat.h>
 #include <linux/time.h>
+#include <linux/skbuff.h>
 
 #define ELF_COMPAT	1
 

ammarfaizi2/linux-block

fs/compat_binfmt_elf.c
@@ -14,6 +14,9 @@
  * functions used in binfmt_elf.c to compat versions.
  */
 
+#include <asm/elf_api.h>
+
+#include <linux/skbuff_api.h>
 #include <linux/elfcore-compat.h>
 #include <linux/time.h>
 