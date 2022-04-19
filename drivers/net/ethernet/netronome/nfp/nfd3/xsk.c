33a9fed1ec3a151825b96abbd317d6c2d3b9a175
Ingo Molnar <mingo@kernel.org>
2022-04-19 11:37:38 +0200

    headers/deps: Add header dependencies to .c files: <net/xdp_api.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- drivers/net/ethernet/netronome/nfp/nfd3/xsk.c
+++ drivers/net/ethernet/netronome/nfp/nfd3/xsk.c
@@ -2,6 +2,7 @@
 /* Copyright (C) 2018 Netronome Systems, Inc */
 /* Copyright (C) 2021 Corigine, Inc */
 
+#include <net/xdp_api.h>
 #include <linux/u64_stats_sync_api.h>
 #include <linux/prefetch.h>
 #include <linux/netdevice_api.h>
