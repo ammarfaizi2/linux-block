3ea1d4e002d457ec1cf55b10549a7413335c77cd
Zqiang <qiang1.zhang@intel.com>
2022-04-21 10:34:50 +1000

    kasan: prevent cpu_quarantine corruption when CPU offline and cache shrink occur at same time
    kasan_quarantine_remove_cache() is called in kmem_cache_shrink()/
    destroy().  The kasan_quarantine_remove_cache() call is protected by
    cpuslock in kmem_cache_destroy() to ensure serialization with
    kasan_cpu_offline().
    
    However the kasan_quarantine_remove_cache() call is not protected by
    cpuslock in kmem_cache_shrink().  When a CPU is going offline and cache
    shrink occurs at same time, the cpu_quarantine may be corrupted by
    interrupt (per_cpu_remove_cache operation).
    
    So add a cpu_quarantine offline flags check in per_cpu_remove_cache().
    
    Link: https://lkml.kernel.org/r/20220414025925.2423818-1-qiang1.zhang@intel.com
    Signed-off-by: Zqiang <qiang1.zhang@intel.com>
    Cc: Andrey Ryabinin <ryabinin.a.a@gmail.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Andrey Konovalov <andreyknvl@gmail.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/kasan/quarantine.c
+++ mm/kasan/quarantine.c
@@ -315,6 +315,8 @@ static void per_cpu_remove_cache(void *arg)
 	struct qlist_head *q;
 
 	q = this_cpu_ptr(&cpu_quarantine);
+	if (READ_ONCE(q->offline))
+		return;
 	qlist_move_cache(q, &to_free, cache);
 	qlist_free_all(&to_free, cache);
 }
