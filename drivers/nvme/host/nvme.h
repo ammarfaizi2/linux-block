86116c2f6fee81a0be72a71cc2f1f7c087df8efa
Kanchan Joshi <joshi.k@samsung.com>
2022-05-05 11:36:15 +0530

    nvme: wire-up uring-cmd support for io-passthru on char-device.
    Introduce handler for fops->uring_cmd(), implementing async passthru
    on char device (/dev/ngX). The handler supports newly introduced
    operation NVME_URING_CMD_IO. This operates on a new structure
    nvme_uring_cmd, which is similar to struct nvme_passthru_cmd64 but
    without the embedded 8b result field. This field is not needed since
    uring-cmd allows to return additional result via big-CQE.
    
    Signed-off-by: Kanchan Joshi <joshi.k@samsung.com>
    Signed-off-by: Anuj Gupta <anuj20.g@samsung.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20220505060616.803816-5-joshi.k@samsung.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/nvme/host/nvme.h
+++ drivers/nvme/host/nvme.h
@@ -16,6 +16,7 @@
 #include <linux/rcupdate.h>
 #include <linux/wait.h>
 #include <linux/t10-pi.h>
+#include <linux/io_uring.h>
 
 #include <trace/events/block.h>
 
@@ -782,6 +783,10 @@ long nvme_ns_head_chr_ioctl(struct file *file, unsigned int cmd,
 		unsigned long arg);
 long nvme_dev_ioctl(struct file *file, unsigned int cmd,
 		unsigned long arg);
+int nvme_ns_chr_uring_cmd(struct io_uring_cmd *ioucmd,
+		unsigned int issue_flags);
+int nvme_ns_head_chr_uring_cmd(struct io_uring_cmd *ioucmd,
+		unsigned int issue_flags);
 int nvme_getgeo(struct block_device *bdev, struct hd_geometry *geo);
 
 extern const struct attribute_group *nvme_ns_id_attr_groups[];
