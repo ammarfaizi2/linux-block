f459498b60d0db861290c48c88e970cd480ff314
Baolin Wang <baolin.wang@linux.alibaba.com>
2022-05-12 21:15:52 -0700

    mm-change-huge_ptep_clear_flush-to-return-the-original-pte-fix-2
    fix build in several more architectures
    
    Link: https://lkml.kernel.org/r/0009a4cd-2826-e8be-e671-f050d4f18d5d@linux.alibaba.com
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/sh/include/asm/hugetlb.h
+++ arch/sh/include/asm/hugetlb.h
@@ -24,6 +24,7 @@ static inline int prepare_hugepage_range(struct file *file,
 static inline pte_t huge_ptep_clear_flush(struct vm_area_struct *vma,
 					  unsigned long addr, pte_t *ptep)
 {
+	return *ptep;
 }
 
 static inline void arch_clear_hugepage_flags(struct page *page)
