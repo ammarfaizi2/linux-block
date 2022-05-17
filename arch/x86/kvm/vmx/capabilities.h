97cb1c273ea66c49fce4affff9780a29e8f29a57
Yang Weijiang <weijiang.yang@intel.com>
2022-05-17 11:40:54 -0400

    KVM: x86/vmx: Check Arch LBR config when return perf capabilities
    Two new bit fields(VM_EXIT_CLEAR_IA32_LBR_CTL, VM_ENTRY_LOAD_IA32_LBR_CTL)
    are added to support guest Arch LBR. These two bits should be set in order
    to make Arch LBR workable in both guest and host.
    
    Co-developed-by: Like Xu <like.xu@linux.intel.com>
    Signed-off-by: Like Xu <like.xu@linux.intel.com>
    Signed-off-by: Yang Weijiang <weijiang.yang@intel.com>
    Message-Id: <20220517154100.29983-11-weijiang.yang@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx/capabilities.h
+++ arch/x86/kvm/vmx/capabilities.h
@@ -404,6 +404,12 @@ static inline bool vmx_pebs_supported(void)
 	return boot_cpu_has(X86_FEATURE_PEBS) && kvm_pmu_cap.pebs_ept;
 }
 
+static inline bool cpu_has_vmx_arch_lbr(void)
+{
+	return (vmcs_config.vmexit_ctrl & VM_EXIT_CLEAR_IA32_LBR_CTL) &&
+		(vmcs_config.vmentry_ctrl & VM_ENTRY_LOAD_IA32_LBR_CTL);
+}
+
 static inline u64 vmx_get_perf_capabilities(void)
 {
 	u64 perf_cap = PMU_CAP_FW_WRITES;
@@ -419,6 +425,9 @@ static inline u64 vmx_get_perf_capabilities(void)
 		if ((perf_cap & PERF_CAP_PEBS_FORMAT) < 4)
 			perf_cap &= ~PERF_CAP_PEBS_BASELINE;
 	}
+
+	if (boot_cpu_has(X86_FEATURE_ARCH_LBR) && !cpu_has_vmx_arch_lbr())
+		perf_cap &= ~PMU_CAP_LBR_FMT;
 
 	return perf_cap;
 }
