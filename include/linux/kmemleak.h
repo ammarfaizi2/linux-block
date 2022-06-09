b93824d0828d3988087e76f4222962e4643fc944
Patrick Wang <patrick.wang.shcn@gmail.com>
2022-06-09 20:49:48 +0800

    mm: kmemleak: add OBJECT_PHYS flag for objects allocated with physical address
    Patch series "mm: kmemleak: store objects allocated with physical address
    separately and check when scan", v3.
    
    The kmemleak_*_phys() interface uses "min_low_pfn" and "max_low_pfn" to
    check address.  But on some architectures, kmemleak_*_phys() is called
    before those two variables initialized.  The following steps will be
    taken:
    
    1) Add OBJECT_PHYS flag and rbtree for the objects allocated
       with physical address
    2) Store physical address in objects if allocated with OBJECT_PHYS
    3) Check the boundary when scan instead of in kmemleak_*_phys()
    
    This patch set will solve:
    https://lore.kernel.org/r/20220527032504.30341-1-yee.lee@mediatek.com
    https://lore.kernel.org/r/9dd08bb5-f39e-53d8-f88d-bec598a08c93@gmail.com
    
    
    This patch (of 3):
    
    Add OBJECT_PHYS flag for object.  This flag is used to identify the
    objects allocated with physical addresses.  create_object_phys() is added
    as well to set that flag.  And remove the min_count argument to
    kmemleak_alloc_phys() function, assume it's 0.
    
    Link: https://lkml.kernel.org/r/20220609124950.1694394-1-patrick.wang.shcn@gmail.com
    Link: https://lkml.kernel.org/r/20220609124950.1694394-2-patrick.wang.shcn@gmail.com
    Signed-off-by: Patrick Wang <patrick.wang.shcn@gmail.com>
    Suggested-by: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Yee Lee <yee.lee@mediatek.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/kmemleak.h
+++ include/linux/kmemleak.h
@@ -29,7 +29,7 @@ extern void kmemleak_not_leak(const void *ptr) __ref;
 extern void kmemleak_ignore(const void *ptr) __ref;
 extern void kmemleak_scan_area(const void *ptr, size_t size, gfp_t gfp) __ref;
 extern void kmemleak_no_scan(const void *ptr) __ref;
-extern void kmemleak_alloc_phys(phys_addr_t phys, size_t size, int min_count,
+extern void kmemleak_alloc_phys(phys_addr_t phys, size_t size,
 				gfp_t gfp) __ref;
 extern void kmemleak_free_part_phys(phys_addr_t phys, size_t size) __ref;
 extern void kmemleak_not_leak_phys(phys_addr_t phys) __ref;
@@ -107,7 +107,7 @@ static inline void kmemleak_no_scan(const void *ptr)
 {
 }
 static inline void kmemleak_alloc_phys(phys_addr_t phys, size_t size,
-				       int min_count, gfp_t gfp)
+				       gfp_t gfp)
 {
 }
 static inline void kmemleak_free_part_phys(phys_addr_t phys, size_t size)
