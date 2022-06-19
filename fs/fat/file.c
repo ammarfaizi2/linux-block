8e3409bccb1de13d0cdfb2e3935c182659575238
Christian Brauner <brauner@kernel.org>
2022-06-19 13:20:12 +0200

    attr: port attribute changes to new types
    Now that we introduced new infrastructure to increase the type safety
    for filesystems supporting idmapped mounts port the first part of the
    vfs over to them.
    
    This ports the attribute changes codepaths to rely on the new better
    helpers using a dedicated type.
    
    Before this change we used to take a shortcut and place the actual
    values that would be written to inode->i_{g,u}id into struct iattr. This
    had the advantage that we moved idmappings mostly out of the picture
    early on but it made reasoning about changes more difficult than it
    should be.
    
    The filesystem was never explicitly told that it dealt with an idmapped
    mount. The transition to the value that needed to be stored in
    inode->i_{g,u}id appeared way too early and increased the probability of
    bugs in various codepaths.
    
    We know place the same value in struct iattr no matter if this is an
    idmapped mount or not. The vfs will only deal with type safe
    vfs{g,u}id_t. This makes it massively safer to perform permission checks
    as the type will tell us what checks we need to perform and what helpers
    we need to use.
    
    Fileystems raising FS_ALLOW_IDMAP can't simply write ia_vfs{g,u}id to
    inode->i_{g,u}id since they are different types. Instead they need to
    use the dedicated vfs{g,u}id_to_k{g,u}id() helpers that map the
    vfs{g,u}id into the filesystem.
    
    The other nice effect is that filesystems like overlayfs don't need to
    care about idmappings explicitly anymore and can simply set up struct
    iattr accordingly directly.
    
    Link: https://lore.kernel.org/lkml/CAHk-=win6+ahs1EwLkcq8apqLi_1wXFWbrPf340zYEhObpz4jA@mail.gmail.com [1]
    Cc: Seth Forshee <sforshee@digitalocean.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Aleksa Sarai <cyphar@cyphar.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    CC: linux-fsdevel@vger.kernel.org
    Signed-off-by: Christian Brauner (Microsoft) <brauner@kernel.org>
    ---
    /* v2 */
    - Linus Torvalds <torvalds@linux-foundation.org>:
      - Rename s/kmnt{g,u}id_t/vfs{g,u}id_t/g
    
    - Christian Brauner (Microsoft) <brauner@kernel.org>:
      - Rename and simplify setattr_vfs{g,u}id().

--- fs/fat/file.c
+++ fs/fat/file.c
@@ -90,7 +90,7 @@ static int fat_ioctl_set_attributes(struct file *file, u32 __user *user_attr)
 	 * out the RO attribute for checking by the security
 	 * module, just because it maps to a file mode.
 	 */
-	err = security_inode_setattr(&init_user_ns,
+	err = security_inode_setattr(file_mnt_user_ns(file),
 				     file->f_path.dentry, &ia);
 	if (err)
 		goto out_unlock_inode;
@@ -517,9 +517,13 @@ int fat_setattr(struct user_namespace *mnt_userns, struct dentry *dentry,
 	}
 
 	if (((attr->ia_valid & ATTR_UID) &&
-	     (!uid_eq(attr->ia_uid, sbi->options.fs_uid))) ||
+	     (!uid_eq(vfsuid_to_kuid(mnt_userns, i_user_ns(inode),
+				     attr->ia_vfsuid),
+		      sbi->options.fs_uid))) ||
 	    ((attr->ia_valid & ATTR_GID) &&
-	     (!gid_eq(attr->ia_gid, sbi->options.fs_gid))) ||
+	     (!gid_eq(vfsgid_to_kgid(mnt_userns, i_user_ns(inode),
+				     attr->ia_vfsgid),
+		      sbi->options.fs_gid))) ||
 	    ((attr->ia_valid & ATTR_MODE) &&
 	     (attr->ia_mode & ~FAT_VALID_MODE)))
 		error = -EPERM;
