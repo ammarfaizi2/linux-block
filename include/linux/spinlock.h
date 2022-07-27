2265fa821ec90634df6c2e62c3c01fd2da4a099f
Wedson Almeida Filho <wedsonaf@google.com>
2022-07-27 22:13:46 +0200

    locking/spinlock: introduce `__spin_lock_init`
    A Rust helper (introduced in a later patch) needs to call
    `spin_lock_init` with a passed key, rather than define
    one in place.
    
    In order to do that, this changes the `spin_lock_init` macro
    to call a new `__spin_lock_init` function which takes the key
    as an extra parameter.
    
    Co-developed-by: Alex Gaynor <alex.gaynor@gmail.com>
    Signed-off-by: Alex Gaynor <alex.gaynor@gmail.com>
    Signed-off-by: Wedson Almeida Filho <wedsonaf@google.com>
    Co-developed-by: Miguel Ojeda <ojeda@kernel.org>
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- include/linux/spinlock.h
+++ include/linux/spinlock.h
@@ -326,12 +326,17 @@ static __always_inline raw_spinlock_t *spinlock_check(spinlock_t *lock)
 
 #ifdef CONFIG_DEBUG_SPINLOCK
 
+static inline void __spin_lock_init(spinlock_t *lock, const char *name,
+				    struct lock_class_key *key)
+{
+	__raw_spin_lock_init(spinlock_check(lock), name, key, LD_WAIT_CONFIG);
+}
+
 # define spin_lock_init(lock)					\
 do {								\
 	static struct lock_class_key __key;			\
 								\
-	__raw_spin_lock_init(spinlock_check(lock),		\
-			     #lock, &__key, LD_WAIT_CONFIG);	\
+	__spin_lock_init(lock, #lock, &__key);			\
 } while (0)
 
 #else
