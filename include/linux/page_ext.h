495f40e410f46e5deacb5d2bcac6f785ddfd1e8f
Li Zhe <lizhe.67@bytedance.com>
2022-08-15 20:09:54 +0800

    page_ext: move up page_ext_init() to catch early page allocation if DEFERRED_STRUCT_PAGE_INIT is n
    In commit 2f1ee0913ce5 ("Revert "mm: use early_pfn_to_nid in
    page_ext_init"") we call page_ext_init() after page_alloc_init_late() to
    avoid some panic problem.  It seems that we cannot track early page
    allocations in current kernel even if page structure has been initialized
    early.
    
    This patch moves up page_ext_init() to catch early page allocations when
    DEFERRED_STRUCT_PAGE_INIT is n.  After this patch, we only need to turn
    DEFERRED_STRUCT_PAGE_INIT to n then we are able to analyze the early page
    allocations.  This is useful especially when we find that the free memory
    value is not the same right after different kernel booting.
    
    Link: https://lkml.kernel.org/r/20220815120954.65957-1-lizhe.67@bytedance.com
    Signed-off-by: Li Zhe <lizhe.67@bytedance.com>
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Jason A. Donenfeld <Jason@zx2c4.com>
    Cc: Mark-PK Tsai <mark-pk.tsai@mediatek.com>
    Cc: Steven Rostedt (Google) <rostedt@goodmis.org>
    Cc: Zefan Li <lizefan.x@bytedance.com>
    Cc: Qian Cai <cai@lca.pw>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Pasha Tatashin <Pavel.Tatashin@microsoft.com>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Yang Shi <yang.shi@linaro.org>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/page_ext.h
+++ include/linux/page_ext.h
@@ -43,14 +43,34 @@ extern void pgdat_page_ext_init(struct pglist_data *pgdat);
 static inline void page_ext_init_flatmem(void)
 {
 }
-extern void page_ext_init(void);
 static inline void page_ext_init_flatmem_late(void)
 {
 }
+extern void _page_ext_init(void);
+#ifdef CONFIG_DEFERRED_STRUCT_PAGE_INIT
+static inline void page_ext_init_early(void)
+{
+}
+static inline void page_ext_init_late(void)
+{
+	_page_ext_init();
+}
+#else
+static inline void page_ext_init_early(void)
+{
+	_page_ext_init();
+}
+static inline void page_ext_init_late(void)
+{
+}
+#endif /* CONFIG_DEFERRED_STRUCT_PAGE_INIT */
 #else
 extern void page_ext_init_flatmem(void);
 extern void page_ext_init_flatmem_late(void);
-static inline void page_ext_init(void)
+static inline void page_ext_init_early(void)
+{
+}
+static inline void page_ext_init_late(void)
 {
 }
 #endif
@@ -76,7 +96,11 @@ static inline struct page_ext *lookup_page_ext(const struct page *page)
 	return NULL;
 }
 
-static inline void page_ext_init(void)
+static inline void page_ext_init_early(void)
+{
+}
+
+static inline void page_ext_init_late(void)
 {
 }
 
