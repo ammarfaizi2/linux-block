b11354d38cff08453fab3d154c295b382c6a9173
Shigeru Yoshida <yshigeru@gmail.com>
2022-08-28 08:00:37 +0900

    mm/gup.c: Fix uninitialized return value on __gup_longterm_locked()
    In __gup_longterm_locked(), it returns uninitialized value if
    __get_user_pages_locked() fails on the first iteration of the loop since
    rc is not initialized.
    
    This patch fixes this issue by properly returning error code if
    __get_user_pages_locked() fails.
    
    Link: https://lkml.kernel.org/r/20220827230037.78876-1-syoshida@redhat.com
    Fixes: 11147539df44 ("mm/gup.c: Fix return value for __gup_longterm_locked()")
    Signed-off-by: Shigeru Yoshida <yshigeru@gmail.com>
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Cc: Alistair Popple <apopple@nvidia.com>
    
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/gup.c
+++ mm/gup.c
@@ -2090,8 +2090,10 @@ static long __gup_longterm_locked(struct mm_struct *mm,
 		nr_pinned_pages = __get_user_pages_locked(mm, start, nr_pages,
 							  pages, vmas, NULL,
 							  gup_flags);
-		if (nr_pinned_pages <= 0)
+		if (nr_pinned_pages <= 0) {
+			rc = nr_pinned_pages;
 			break;
+		}
 		rc = check_and_migrate_movable_pages(nr_pinned_pages, pages,
 						     gup_flags);
 	} while (rc == -EAGAIN);
