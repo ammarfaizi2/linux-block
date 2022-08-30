5cff4a97c737f6eead46105887eb318dd25f5be6
Michal Hocko <mhocko@suse.com>
2022-08-30 09:34:06 +0200

    mm-reduce-noise-in-show_mem-for-lowmem-allocations-update
    Dan has brought up[1] that the use of gfp mask has confused his static
    analyzer which assumes that GFP_HIGHUSER_MOVABLE implies a sleeping
    allocation and that wouldn't be a great idea from the panic path. I
    would add that most callers of this function would be really bad to
    allocate.
    
    The report itself is a false positive but it made me think a bit about
    this. Even if the check is too simplistic I guess it resembles how many
    developers are thinking (including me). If I see GFP_HIGHUSER_MOVABLE or
    GF_KERNEL I automatically assume a sleeping allocation down the road.
    And who know somebody might add one in the future even into show_mem
    because the gfp parameter would be too tempting to not (ab)use.
    
    My original intention was to use a natural allocation speak but this can
    backfire so maybe it would be better to give the argument its real
    meaning and that is the high_zone_idx. This is cryptic for code outside
    of MM but that is not all that many callers and we can hide this fact
    from them. In other words does the thing below looks better (incremental
    for illustration, I will make it a proper patch if yes)?
    
    [1] https://lore.kernel.org/all/Yw2ugrlZ8bwE5/hh@kili/?q=http%3A%2F%2Flkml.kernel.org%2Fr%2FYw2ugrlZ8bwE5%2Fhh%40kili
    Link: https://lkml.kernel.org/r/Yw29bmJTIkKogTiW@dhcp22.suse.cz
    Cc: Dan Carpenter <dan.carpenter@oracle.com>
    Cc: Christoph Hellwig <hch@infradead.org>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- lib/show_mem.c
+++ lib/show_mem.c
@@ -8,13 +8,13 @@
 #include <linux/mm.h>
 #include <linux/cma.h>
 
-void show_mem(unsigned int filter, nodemask_t *nodemask, gfp_t gfp_mask)
+void __show_mem(unsigned int filter, nodemask_t *nodemask, int max_zone_idx)
 {
 	pg_data_t *pgdat;
 	unsigned long total = 0, reserved = 0, highmem = 0;
 
 	printk("Mem-Info:\n");
-	show_free_areas(filter, nodemask, gfp_mask);
+	show_free_areas(filter, nodemask, max_zone_idx);
 
 	for_each_online_pgdat(pgdat) {
 		int zoneid;
