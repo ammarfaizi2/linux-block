f82de740606d3f83b368c7efacf8923d2156dd6a
Michal Hocko <mhocko@suse.com>
2022-08-31 09:58:35 +0200

    pc-mm-reduce-noise-in-show_mem-for-lowmem-allocations-update-fix-2
    fix build
    
    
    lib/show_mem.c: In function '__show_mem':
    lib/show_mem.c:17:9: error: too many arguments to function 'show_free_areas'
       17 |         show_free_areas(filter, nodemask, max_zone_idx);
          |         ^~~~~~~~~~~~~~~
    In file included from lib/show_mem.c:8:
    ./include/linux/mm.h:1842:28: note: declared here
     1842 | static void __maybe_unused show_free_areas(unsigned int flags, nodemask_t *nodemask)
    
    Link: https://lkml.kernel.org/r/Yw8Uq5rF19qVUQH+@dhcp22.suse.cz
    
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- lib/show_mem.c
+++ lib/show_mem.c
@@ -14,7 +14,7 @@ void __show_mem(unsigned int filter, nodemask_t *nodemask, int max_zone_idx)
 	unsigned long total = 0, reserved = 0, highmem = 0;
 
 	printk("Mem-Info:\n");
-	show_free_areas(filter, nodemask, max_zone_idx);
+	__show_free_areas(filter, nodemask, max_zone_idx);
 
 	for_each_online_pgdat(pgdat) {
 		int zoneid;
