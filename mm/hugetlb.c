d0d4d2be1e39d8cfacbbd47df5939e38d09aeb54
Miaohe Lin <linmiaohe@huawei.com>
2022-09-01 20:00:21 +0800

    hugetlb: make hugetlb_cma_check() static
    Patch series "A few cleanup patches for hugetlb", v2.
    
    This series contains a few cleanup patches to use helper functions to
    simplify the codes, remove unneeded nid parameter and so on. More
    details can be found in the respective changelogs.
    
    
    This patch (of 10):
    
    Make hugetlb_cma_check() static as it's only used inside mm/hugetlb.c.
    
    Link: https://lkml.kernel.org/r/20220901120030.63318-1-linmiaohe@huawei.com
    Link: https://lkml.kernel.org/r/20220901120030.63318-2-linmiaohe@huawei.com
    Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
    Reviewed-by: Muchun Song <songmuchun@bytedance.com>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -4043,6 +4043,14 @@ static void hugetlb_register_all_nodes(void) { }
 
 #endif
 
+#ifdef CONFIG_CMA
+static void __init hugetlb_cma_check(void);
+#else
+static inline __init void hugetlb_cma_check(void)
+{
+}
+#endif
+
 static int __init hugetlb_init(void)
 {
 	int i;
@@ -7373,7 +7381,7 @@ void __init hugetlb_cma_reserve(int order)
 		hugetlb_cma_size = 0;
 }
 
-void __init hugetlb_cma_check(void)
+static void __init hugetlb_cma_check(void)
 {
 	if (!hugetlb_cma_size || cma_reserve_called)
 		return;
