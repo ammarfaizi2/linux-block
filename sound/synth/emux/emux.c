8f8e2c8d1980810aa810634c706ffb490ce422ed
Steven Rostedt (Google) <rostedt@goodmis.org>
2022-10-27 10:13:06 -0400

    timers: ALSA: Use del_timer_shutdown() before freeing timer
    Before a timer is freed, del_timer_shutdown() must be called.
    
    Link: https://lore.kernel.org/all/20220407161745.7d6754b3@gandalf.local.home/
    
    Cc: Jaroslav Kysela <perex@perex.cz>
    Cc: Takashi Iwai <tiwai@suse.com>
    Cc: Austin Kim <austin.kim@lge.com>
    Cc: alsa-devel@alsa-project.org
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- sound/synth/emux/emux.c
+++ sound/synth/emux/emux.c
@@ -129,7 +129,7 @@ int snd_emux_free(struct snd_emux *emu)
 	if (! emu)
 		return -EINVAL;
 
-	del_timer_sync(&emu->tlist);
+	del_timer_shutdown(&emu->tlist);
 
 	snd_emux_proc_free(emu);
 	snd_emux_delete_virmidi(emu);
