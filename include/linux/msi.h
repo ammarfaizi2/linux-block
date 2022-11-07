8456337a37bbd9273c3c517054a30beb123019f3
Thomas Gleixner <tglx@linutronix.de>
2022-11-07 16:57:56 +0100

    genirq/msi: Extend msi_parent_ops
    Supporting per device MSI domains on ARM64 and the zoo of interrupt
    mechanisms needs a bit more information than what the initial x86
    implementation provides.
    
    Add the following fields:
    
      - required_flags: 	The flags which a parent domain requires to be set
      - bus_select_token:	The bus token of the parent domain for select()
      - bus_select_mask:	A bitmask of supported child domain bus types
    
    This allows to provide library functions which can be shared between
    various interrupt chip implementations and avoids replicating mostly
    similar code all over the place.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/msi.h
+++ include/linux/msi.h
@@ -537,6 +537,14 @@ enum {
  * struct msi_parent_ops - MSI parent domain callbacks and configuration info
  *
  * @supported_flags:	Required: The supported MSI flags of the parent domain
+ * @required_flags:	Optional: The required MSI flags of the parent MSI domain
+ *			Mandatory for ARM_GIC_MSI_LIB users
+ * @bus_select_token:	Optional: The bus token of the real parent domain for
+ *			irq_domain::select()
+ *			Mandatory for ARM_GIC_MSI_LIB users
+ * @bus_select_mask:	Optional: A mask of supported BUS_DOMAINs for
+ *			irq_domain::select()
+ *			Mandatory for ARM_GIC_MSI_LIB users
  * @prefix:		Optional: Prefix for the domain and chip name
  * @init_dev_msi_info:	Required: Callback for MSI parent domains to setup parent
  *			domain specific domain flags, domain ops and interrupt chip
@@ -544,6 +552,9 @@ enum {
  */
 struct msi_parent_ops {
 	u32		supported_flags;
+	u32		required_flags;
+	u32		bus_select_token;
+	u32		bus_select_mask;
 	const char	*prefix;
 	bool		(*init_dev_msi_info)(struct device *dev, struct irq_domain *domain,
 					     struct irq_domain *real_parent,
