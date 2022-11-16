e329a79c13ea6bc7484fbc207c55f22b194f13bc
Thomas Gleixner <tglx@linutronix.de>
2022-11-16 11:00:08 +0100

    genirq/msi: Optionally use dev->fwnode for device domain
    To support wire to MSI domains via the MSI infrastructure it is required to
    use the firmware node of the device which implements this for creating the
    MSI domain. Otherwise the existing firmware match mechanisms to find the
    correct irqdomain for a wired interrupt which is connected to a wire to MSI
    bridge would fail.
    
    This cannot be used for the general case because not all devices provide
    firmware nodes and all regular per device MSI domains are directly
    associated to the device and have not be searched for.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/msi.h
+++ include/linux/msi.h
@@ -517,6 +517,8 @@ enum {
 	 * handled in the core MSI code.
 	 */
 	MSI_FLAG_NOMASK_QUIRK		= (1 << 7),
+	/* Use dev->fwnode for MSI device domain creation */
+	MSI_FLAG_USE_DEV_FWNODE		= (1 << 8),
 
 	/* Mask for the generic functionality */
 	MSI_GENERIC_FLAGS_MASK		= GENMASK(15, 0),
