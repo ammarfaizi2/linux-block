b442aa42285568dccffe9bc0690512db2f6a238f
Thomas Gleixner <tglx@linutronix.de>
2022-11-20 20:24:44 +0100

    genirq/msi: Add pointers for per device irq domains
    With the upcoming per device MSI interrupt domain support it is necessary
    to store the domain pointers per device.
    
    Instead of delegating that storage to device drivers or subsystems create a
    storage array in struct msi_device_data which will also take care of
    tearing down the irq domains when msi_device_data is cleaned up via devres.
    
    The interfaces into the MSI core will be changed from irqdomain pointer
    based interfaces to domain id based interfaces to support multiple MSI
    domains on a single device (e.g. PCI/MSI[-X] and PCI/IMS.
    
    Once the per device domain support is complete the irq domain pointer in
    struct device::msi.domain will not longer contain a pointer to the "global"
    MSI domain. It will contain a pointer to the MSI parent domain instead.
    
    It would be a horrible maze of conditionals to evaluate all over the place
    which domain pointer should be used, i.e. the "global" one in
    device::msi::domain or one from the internal pointer array.
    
    To avoid this evaluate in msi_setup_device_data() whether the irq domain
    which is associated to a device is a "global" or a parent MSI domain. If it
    is global then copy the pointer into the first entry in the irqdomain
    pointer array.
    
    This allows to convert interfaces and implementation to domain ids while
    keeping everything existing working.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/msi.h
+++ include/linux/msi.h
@@ -77,6 +77,7 @@ struct msi_desc;
 struct pci_dev;
 struct platform_msi_priv_data;
 struct device_attribute;
+struct irq_domain;
 
 void __get_cached_msi_msg(struct msi_desc *entry, struct msi_msg *msg);
 #ifdef CONFIG_GENERIC_MSI_IRQ
@@ -180,6 +181,7 @@ enum msi_desc_filter {
  * @mutex:		Mutex protecting the MSI descriptor store
  * @__store:		Xarray for storing MSI descriptor pointers
  * @__iter_idx:		Index to search the next entry for iterators
+ * @__irqdomains:	Per device interrupt domains
  */
 struct msi_device_data {
 	unsigned long			properties;
@@ -187,6 +189,7 @@ struct msi_device_data {
 	struct mutex			mutex;
 	struct xarray			__store;
 	unsigned long			__iter_idx;
+	struct irq_domain		*__irqdomains[MSI_MAX_DEVICE_IRQDOMAINS];
 };
 
 int msi_setup_device_data(struct device *dev);
