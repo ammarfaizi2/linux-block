7facb25c785c1f69a51f981b20e338e45bedaa51
Peter Xu <peterx@redhat.com>
2022-11-29 14:35:20 -0500

    mm/hugetlb: move swap entry handling into vma lock when faulted
    In hugetlb_fault(), there used to have a special path to handle swap entry
    at the entrance using huge_pte_offset().  That's unsafe because
    huge_pte_offset() for a pmd sharable range can access freed pgtables if
    without any lock to protect the pgtable from being freed after pmd
    unshare.
    
    Here the simplest solution to make it safe is to move the swap handling to
    be after the vma lock being held.  We may need to take the fault mutex on
    either migration or hwpoison entries now (also the vma lock, but that's
    really needed), however neither of them is hot path.
    
    Note that the vma lock cannot be released in hugetlb_fault() when the
    migration entry is detected, because in migration_entry_wait_huge() the
    pgtable page will be used again (by taking the pgtable lock), so that also
    need to be protected by the vma lock.  Modify migration_entry_wait_huge()
    so that it must be called with vma read lock held, and properly release
    the lock in __migration_entry_wait_huge().
    
    Link: https://lkml.kernel.org/r/20221129193526.3588187-5-peterx@redhat.com
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: James Houghton <jthoughton@google.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Miaohe Lin <linmiaohe@huawei.com>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Muchun Song <songmuchun@bytedance.com>
    Cc: Nadav Amit <nadav.amit@gmail.com>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: David Hildenbrand <david@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -5826,22 +5826,6 @@ vm_fault_t hugetlb_fault(struct mm_struct *mm, struct vm_area_struct *vma,
 	int need_wait_lock = 0;
 	unsigned long haddr = address & huge_page_mask(h);
 
-	ptep = huge_pte_offset(mm, haddr, huge_page_size(h));
-	if (ptep) {
-		/*
-		 * Since we hold no locks, ptep could be stale.  That is
-		 * OK as we are only making decisions based on content and
-		 * not actually modifying content here.
-		 */
-		entry = huge_ptep_get(ptep);
-		if (unlikely(is_hugetlb_entry_migration(entry))) {
-			migration_entry_wait_huge(vma, ptep);
-			return 0;
-		} else if (unlikely(is_hugetlb_entry_hwpoisoned(entry)))
-			return VM_FAULT_HWPOISON_LARGE |
-				VM_FAULT_SET_HINDEX(hstate_index(h));
-	}
-
 	/*
 	 * Serialize hugepage allocation and instantiation, so that we don't
 	 * get spurious allocation failures if two CPUs race to instantiate
@@ -5888,8 +5872,22 @@ vm_fault_t hugetlb_fault(struct mm_struct *mm, struct vm_area_struct *vma,
 	 * fault, and is_hugetlb_entry_(migration|hwpoisoned) check will
 	 * properly handle it.
 	 */
-	if (!pte_present(entry))
+	if (!pte_present(entry)) {
+		if (unlikely(is_hugetlb_entry_migration(entry))) {
+			/*
+			 * Release fault lock first because the vma lock is
+			 * needed to guard the huge_pte_lockptr() later in
+			 * migration_entry_wait_huge().  The vma lock will
+			 * be released there.
+			 */
+			mutex_unlock(&hugetlb_fault_mutex_table[hash]);
+			migration_entry_wait_huge(vma, ptep);
+			return 0;
+		} else if (unlikely(is_hugetlb_entry_hwpoisoned(entry)))
+			ret = VM_FAULT_HWPOISON_LARGE |
+			    VM_FAULT_SET_HINDEX(hstate_index(h));
 		goto out_mutex;
+	}
 
 	/*
 	 * If we are going to COW/unshare the mapping later, we examine the
