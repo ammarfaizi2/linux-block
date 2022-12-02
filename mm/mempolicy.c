60dc61997481414cb130ed2ae85366de4b867906
tzm <tcm1030@163.com>
2022-12-02 22:16:30 +0800

    mm/mempolicy: failed to disable numa balancing
    The kernel fails to disable numa balancing policy permanently when the
    user passes <numa_balancing=disable> to the boot cmdline parameters.  The
    numabalancing_override variable is 1 for enable -1 for disable.  So,
    !numabalancing_override will always be true, which causes this bug.
    
    Link: https://lkml.kernel.org/r/20221202141630.41220-1-tcm1030@163.com
    Signed-off-by: tzm <tcm1030@163.com>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/mempolicy.c
+++ mm/mempolicy.c
@@ -2865,7 +2865,7 @@ static void __init check_numabalancing_enable(void)
 	if (numabalancing_override)
 		set_numabalancing_state(numabalancing_override == 1);
 
-	if (num_online_nodes() > 1 && !numabalancing_override) {
+	if (num_online_nodes() > 1 && (numabalancing_override == 1)) {
 		pr_info("%s automatic NUMA balancing. Configure with numa_balancing= or the kernel.numa_balancing sysctl\n",
 			numabalancing_default ? "Enabling" : "Disabling");
 		set_numabalancing_state(numabalancing_default);
