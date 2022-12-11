db093b851d300422328d8a0d42db2a2ab188fa46
Palmer Dabbelt <palmer@rivosinc.com>
2022-12-10 21:32:42 -0800

    sparc: Remove empty <uapi/asm/setup.h>
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>

--- arch/sparc/include/uapi/asm/setup.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
-/*
- *	Just a place holder. 
- */
-
-#ifndef _UAPI_SPARC_SETUP_H
-#define _UAPI_SPARC_SETUP_H
-
-#endif /* _UAPI_SPARC_SETUP_H */
