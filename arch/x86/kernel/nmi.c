5d4159ff8295addf11917c989a18dfdce1c77686
Paul E. McKenney <paulmck@kernel.org>
2022-12-19 13:31:47 -0800

    x86/nmi: Test commit for severely delayed NMI handlers
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- arch/x86/kernel/nmi.c
+++ arch/x86/kernel/nmi.c
@@ -530,9 +530,13 @@ nmi_restart:
 		WRITE_ONCE(nsp->idt_ignored, nsp->idt_ignored + 1);
 	} else if (IS_ENABLED(CONFIG_NMI_CHECK_CPU)) {
 	} else if (!ignore_nmis) {
+		int i;
+
 		if (IS_ENABLED(CONFIG_NMI_CHECK_CPU)) {
 			WRITE_ONCE(nsp->idt_nmi_seq, nsp->idt_nmi_seq + 1);
 			WARN_ON_ONCE(!(nsp->idt_nmi_seq & 0x1));
+			for (i = 0; i < 22000; i++)
+				udelay(1000);
 		}
 		default_do_nmi(regs);
 		if (IS_ENABLED(CONFIG_NMI_CHECK_CPU)) {
