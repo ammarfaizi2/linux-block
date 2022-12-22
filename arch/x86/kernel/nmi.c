79237a508a57aaec52d98013b232f04148bdb55f
Paul E. McKenney <paulmck@kernel.org>
2022-12-21 17:02:20 -0800

    x86/nmi: Test commit for exc_nmi handler deep-losing NMIs
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- arch/x86/kernel/nmi.c
+++ arch/x86/kernel/nmi.c
@@ -528,6 +528,7 @@ nmi_restart:
 
 	if (IS_ENABLED(CONFIG_NMI_CHECK_CPU) && ignore_nmis) {
 		WRITE_ONCE(nsp->idt_ignored, nsp->idt_ignored + 1);
+	} else if (IS_ENABLED(CONFIG_NMI_CHECK_CPU)) {
 	} else if (!ignore_nmis) {
 		WRITE_ONCE(nsp->idt_nmi_seq, nsp->idt_nmi_seq + 1);
 		WARN_ON_ONCE(!(nsp->idt_nmi_seq & 0x1));
