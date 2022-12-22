262fff156fdb6595c97b05dc30d4249c9f734e53
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2022-12-22 10:51:30 +0700

    selftests/nolibc: Add `-Wall` and `-Wno-unsed-function` to the CFLAGS
    Turn on more compiler warnings to help spot issues. Skip unused
    function warning as it's inevitable in the nolibc test suite.
    
    While adding these flags, remove unused variables in run_stdlib()
    warned by the `-Wall` flag.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- tools/testing/selftests/nolibc/nolibc-test.c
+++ tools/testing/selftests/nolibc/nolibc-test.c
@@ -572,9 +572,7 @@ int run_syscall(int min, int max)
 int run_stdlib(int min, int max)
 {
 	int test;
-	int tmp;
 	int ret = 0;
-	void *p1, *p2;
 
 	for (test = min; test >= 0 && test <= max; test++) {
 		int llen = 0; // line length
