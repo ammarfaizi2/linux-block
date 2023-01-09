5532926f7ae7a9cd5dcbce71464ca987d6242850
Borislav Petkov (AMD) <bp@alien8.de>
2023-01-09 13:57:14 +0100

    x86/cpufeatures: Add a new, separate CPUID leaf for 0x80000021_EAX
    Add a separate CPUID leaf as the majority of the bits in there are going
    to be used in Linux.
    
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -13,7 +13,7 @@
 /*
  * Defines x86 CPU feature bits
  */
-#define NCAPINTS			20	   /* N 32-bit words worth of info */
+#define NCAPINTS			21	   /* N 32-bit words worth of info */
 #define NBUGINTS			1	   /* N 32-bit bug flags */
 
 /*
@@ -425,6 +425,8 @@
 #define X86_FEATURE_SEV_ES		(19*32+ 3) /* AMD Secure Encrypted Virtualization - Encrypted State */
 #define X86_FEATURE_V_TSC_AUX		(19*32+ 9) /* "" Virtual TSC_AUX */
 #define X86_FEATURE_SME_COHERENT	(19*32+10) /* "" AMD hardware-enforced cache coherency */
+
+/* AMD-defined extended feature 2, CPUID level 0x80000021 (EAX), word 20 */
 
 /*
  * BUG word(s)
