4c6bf77061e872c190aa4128c63808b9a9d3f029
Liam R. Howlett <Liam.Howlett@Oracle.com>
2023-01-20 11:26:50 -0500

    vma_merge: set vma iterator to correct position.
    When merging the previous value, set the vma iterator to the previous
    slot.  Don't use the vma iterator to get the next/prev so that it is in
    the correct position for a write.
    
    Link: https://lkml.kernel.org/r/20230120162650.984577-50-Liam.Howlett@oracle.com
    Signed-off-by: Liam R. Howlett <Liam.Howlett@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/mmap.c
+++ mm/mmap.c
@@ -935,6 +935,7 @@ struct vm_area_struct *vma_merge(struct vma_iterator *vmi, struct mm_struct *mm,
 		    && can_vma_merge_after(prev, vm_flags, anon_vma, file,
 				   pgoff, vm_userfaultfd_ctx, anon_name)) {
 			merge_prev = true;
+			vma_prev(vmi);
 		}
 	}
 	/* Can we merge the successor? */
@@ -1025,9 +1026,6 @@ struct vm_area_struct *vma_merge(struct vma_iterator *vmi, struct mm_struct *mm,
 	vma_iter_free(vmi);
 	validate_mm(mm);
 	khugepaged_enter_vma(res, vm_flags);
-
-	if (res)
-		vma_iter_set(vmi, end);
 
 	return res;
 }
