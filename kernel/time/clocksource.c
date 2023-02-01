46131922472a2f73057fb95a2870be13dc90757e
Paul E. McKenney <paulmck@kernel.org>
2023-02-01 09:53:43 -0800

    Revert "clocksource: Permit limited-duration clocksource watchdogging"
    This reverts commit b71346a1e3139f9c457f104d95ab652d051857ca.
    
    Instead, provide a separate control from sysfs that allows userspace to
    do whatever it wants.

--- kernel/time/clocksource.c
+++ kernel/time/clocksource.c
@@ -213,9 +213,6 @@ module_param(max_cswd_read_retries, ulong, 0644);
 EXPORT_SYMBOL_GPL(max_cswd_read_retries);
 static int verify_n_cpus = 8;
 module_param(verify_n_cpus, int, 0644);
-static int watchdog_duration = CONFIG_CLOCKSOURCE_WATCHDOG_DURATION;
-module_param(watchdog_duration, int, 0444);
-static unsigned long watchdog_end_jiffies;
 
 enum wd_read_status {
 	WD_READ_SUCCESS,
@@ -552,9 +549,7 @@ static void clocksource_watchdog(struct timer_list *unused)
 	 * Arm timer if not already pending: could race with concurrent
 	 * pair clocksource_stop_watchdog() clocksource_start_watchdog().
 	 */
-	if (!timer_pending(&watchdog_timer) &&
-	    (watchdog_duration < 0 ||
-	     (watchdog_duration >= 0 && time_before(jiffies, watchdog_end_jiffies)))) {
+	if (!timer_pending(&watchdog_timer)) {
 		watchdog_timer.expires += WATCHDOG_INTERVAL + extra_wait;
 		add_timer_on(&watchdog_timer, next_cpu);
 	}
@@ -564,10 +559,8 @@ out:
 
 static inline void clocksource_start_watchdog(void)
 {
-	if (watchdog_running || !watchdog || list_empty(&watchdog_list) || !watchdog_duration)
+	if (watchdog_running || !watchdog || list_empty(&watchdog_list))
 		return;
-	if (watchdog_duration > 0)
-		watchdog_end_jiffies = jiffies + watchdog_duration * 60 * HZ;
 	timer_setup(&watchdog_timer, clocksource_watchdog, 0);
 	watchdog_timer.expires = jiffies + WATCHDOG_INTERVAL;
 	add_timer_on(&watchdog_timer, cpumask_first(cpu_online_mask));
