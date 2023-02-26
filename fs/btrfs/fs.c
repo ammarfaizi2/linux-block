0591b56b08edf8ae59d9697b97aa1cdd639ce4b0
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2023-02-26 23:02:56 +0700

    btrfs: Create btrfs CPU set struct and helpers
    It's a preparation to add Btrfs CPU affinity support. First, create a
    new struct named `btrfs_cpu_set` to contain CPU affinity information.
    Then, create helpers to allocate, parse, and free them.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>
    Link: https://lore.kernel.org/r/20230226160259.18354-4-ammarfaizi2@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- fs/btrfs/fs.c
+++ fs/btrfs/fs.c
@@ -96,3 +96,100 @@ void __btrfs_clear_fs_compat_ro(struct btrfs_fs_info *fs_info, u64 flag,
 		set_bit(BTRFS_FS_FEATURE_CHANGED, &fs_info->flags);
 	}
 }
+
+/*
+ * The user can't use the taskset pattern because ',' is used as
+ * the mount option delimiter. They can use the same taskset pattern,
+ * but replace the ',' with '.' and we will replace it back to
+ * ',', so the cpulist_parse() can recognize it.
+ *
+ * For example, in taskset cmd, they do:
+ * taskset -c 1,4,7 /bin/ls
+ *
+ * The equivalent CPU mask for the btrfs mount option will be:
+ * wq_cpu_set=1.4.7
+ *
+ * Mark these as __cold to avoid the code bloat from overoptimizing
+ * the loop.
+ */
+__cold static void cpulist_dot_to_comma(char *set)
+{
+	while (*set) {
+		if (*set == '.')
+			*set = ',';
+		set++;
+	}
+}
+
+__cold static void cpulist_comma_to_dot(char *set)
+{
+	while (*set) {
+		if (*set == ',')
+			*set = '.';
+		set++;
+	}
+}
+
+void btrfs_destroy_cpu_set(struct btrfs_cpu_set *cpu_set)
+{
+	if (!cpu_set)
+		return;
+
+	free_cpumask_var(cpu_set->mask);
+	kfree(cpu_set->mask_str);
+	kfree(cpu_set);
+}
+
+/*
+ * Only called from btrfs_parse_cpu_set().
+ */
+static struct btrfs_cpu_set *btrfs_alloc_cpu_set(void)
+{
+	struct btrfs_cpu_set *cpu_set;
+
+	cpu_set = kmalloc(sizeof(*cpu_set), GFP_KERNEL);
+	if (!cpu_set)
+		return NULL;
+
+	if (!alloc_cpumask_var(&cpu_set->mask, GFP_KERNEL)) {
+		kfree(cpu_set);
+		return NULL;
+	}
+
+	cpu_set->mask_str = NULL;
+	return cpu_set;
+}
+
+int btrfs_parse_cpu_set(struct btrfs_cpu_set **cpu_set_p, const char *mask_str)
+{
+	struct btrfs_cpu_set *cpu_set;
+	int ret;
+
+	cpu_set = btrfs_alloc_cpu_set();
+	if (!cpu_set)
+		return -ENOMEM;
+
+	cpu_set->mask_str = kstrdup(mask_str, GFP_KERNEL);
+	if (!cpu_set->mask_str) {
+		ret = -ENOMEM;
+		goto out_fail;
+	}
+
+	cpulist_dot_to_comma(cpu_set->mask_str);
+	ret = cpulist_parse(cpu_set->mask_str, cpu_set->mask);
+	if (ret)
+		goto out_fail;
+
+	if (cpumask_empty(cpu_set->mask)) {
+		ret = -EINVAL;
+		goto out_fail;
+	}
+
+	cpulist_comma_to_dot(cpu_set->mask_str);
+	*cpu_set_p = cpu_set;
+	return 0;
+
+out_fail:
+	btrfs_destroy_cpu_set(cpu_set);
+	return ret;
+}
