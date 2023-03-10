8c4e2a0a837f58ca93ca1af1adfc98873b9edfaf
Al Viro <viro@zeniv.linux.org.uk>
2023-03-09 19:31:26 -0500

    x86_64: move csum_ipv6_magic() from csum-wrappers_64.c to csum-partial_64.c
    ... and make uml/amd64 use it.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/x86/um/asm/checksum_64.h
+++ arch/x86/um/asm/checksum_64.h
@@ -7,4 +7,11 @@
 
 #define _HAVE_IP_COMPUTE_CSUM
 
+struct in6_addr;
+
+#define _HAVE_ARCH_IPV6_CSUM 1
+extern __sum16
+csum_ipv6_magic(const struct in6_addr *saddr, const struct in6_addr *daddr,
+		__u32 len, __u8 proto, __wsum sum);
+
 #endif
