99ad32fae2811f7576c902676fe7811dcddca4e1
Paul E. McKenney <paulmck@kernel.org>
2023-03-23 17:04:46 -0700

    fixup! srcu: Use static init for statically allocated in-module srcu_struct
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/srcutree.c
+++ kernel/rcu/srcutree.c
@@ -1936,7 +1936,7 @@ static void srcu_module_going(struct module *mod)
 		ssp = *(sspp++);
 		if (!rcu_seq_state(smp_load_acquire(&ssp->srcu_sup->srcu_gp_seq_needed)) &&
 		    !WARN_ON_ONCE(!ssp->srcu_sup->sda_is_static))
-				cleanup_srcu_struct(ssp);
+			cleanup_srcu_struct(ssp);
 		free_percpu(ssp->sda);
 	}
 }
