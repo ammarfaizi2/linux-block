641cf661fec3ff81b43b971a9075c6c57b263174
Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
2024-01-14 17:02:39 +0200

    wifi: ath12k: Refactor MAC un/register helper function
    Currently, the mac80211 hw registration procedure is tightly coupled with
    the handling of link/radio (ar). Define a new helper function to separate
    the link/radio handling from the mac80211 hw registration procedure for
    improved code readability. Also, it can be easy to scale these
    functionality to support single/multi link operation in the future.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.0.1-00029-QCAHKSWPL_SILICONZ-1
    
    Signed-off-by: Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
    Patchwork-Id: 13480999
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>

--- drivers/net/wireless/ath/ath12k/mac.h
+++ drivers/net/wireless/ath/ath12k/mac.h
@@ -49,8 +49,8 @@ enum ath12k_supported_bw {
 extern const struct htt_rx_ring_tlv_filter ath12k_mac_mon_status_filter_default;
 
 void ath12k_mac_hw_destroy(struct ath12k_base *ab);
-void ath12k_mac_unregister(struct ath12k_base *ab);
-int ath12k_mac_register(struct ath12k_base *ab);
+void ath12k_mac_hw_unregister(struct ath12k *ar);
+int ath12k_mac_hw_register(struct ath12k *ar);
 int ath12k_mac_hw_allocate(struct ath12k_base *ab);
 int ath12k_mac_hw_ratecode_to_legacy_rate(u8 hw_rc, u8 preamble, u8 *rateidx,
 					  u16 *rate);
