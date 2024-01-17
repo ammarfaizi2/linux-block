23c88b95ce6a67eb422ab858e22c90312028b69d
Tamizh Chelvam Raja <quic_tamizhr@quicinc.com>
2024-01-17 13:58:30 +0200

    wifi: ath12k: Remove unused tcl_*_ring configuration
    Currently memory allocation for multiple hardware rings
    done in host as these memory will be used by firmware/hardware.
    But in case of tcl_cmd_ring and tcl_status_ring neither firmware
    nor hardware uses host allocated memory and it uses memory allocated
    by firmware itself. So avoid tcl_cmd_ring and tcl_status_ring setup
    in host.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.0.1-00029-QCAHKSWPL_SILICONZ-1
    
    Signed-off-by: Tamizh Chelvam Raja <quic_tamizhr@quicinc.com>
    Acked-by: Jeff Johnson <quic_jjohnson@quicinc.com>
    Patchwork-Id: 13464103
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>

--- drivers/net/wireless/ath/ath12k/dp.h
+++ drivers/net/wireless/ath/ath12k/dp.h
@@ -321,8 +321,6 @@ struct ath12k_dp {
 	struct dp_link_desc_bank link_desc_banks[DP_LINK_DESC_BANKS_MAX];
 	struct dp_srng wbm_idle_ring;
 	struct dp_srng wbm_desc_rel_ring;
-	struct dp_srng tcl_cmd_ring;
-	struct dp_srng tcl_status_ring;
 	struct dp_srng reo_reinject_ring;
 	struct dp_srng rx_rel_ring;
 	struct dp_srng reo_except_ring;
